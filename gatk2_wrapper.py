31f865311a58e9758ef4d066ba372d5972dfaeff
Nicola Soranzo <soranzo@crs4.it>
2014-03-06 12:41:59 +0100

    Fix for missing .fai and .dict when using reference file from the history.
    When selecting the FASTA reference file from the history, GATK 2.8 and
    later (and maybe earlier versions too) does not create '.fai' and '.dict'
    anymore:
    
    ERROR MESSAGE: Fasta index file /opt/galaxy/tmp/tmp-gatk-Obym0N/gatk_input.fasta.fai for reference /opt/galaxy/tmp/tmp-gatk-Obym0N/gatk_input.fasta does not exist. Please see http://gatkforums.broadinstitute.org/discussion/1601/how-can-i-prepare-a-fasta-file-to-use-as-reference for help creating it.
    
    Similar error if only the .fai, but not the .dict is created:
    
    ERROR MESSAGE: Fasta dict file /opt/galaxy/tmp/tmp-gatk-vlrnAe/gatk_input.dict for reference /opt/galaxy/tmp/tmp-gatk-vlrnAe/gatk_input.fasta does not exist. Please see http://gatkforums.broadinstitute.org/discussion/1601/how-can-i-prepare-a-fasta-file-to-use-as-reference for help creating it.

--- gatk2_wrapper.py
+++ gatk2_wrapper.py
@@ -7,7 +7,6 @@ A wrapper script for running the GenomeAnalysisTK.jar commands.
 
 import sys, optparse, os, tempfile, subprocess, shutil
 from binascii import unhexlify
-from string import Template
 
 GALAXY_EXT_TO_GATK_EXT = { 'gatk_interval':'intervals', 'bam_index':'bam.bai', 'gatk_dbsnp':'dbSNP', 'picard_interval_list':'interval_list' } #items not listed here will use the galaxy extension as-is
 GALAXY_EXT_TO_GATK_FILE_TYPE = GALAXY_EXT_TO_GATK_EXT #for now, these are the same, but could be different if needed
@@ -19,6 +18,7 @@ def cleanup_before_exit( tmp_dir ):
     if tmp_dir and os.path.exists( tmp_dir ):
         shutil.rmtree( tmp_dir )
 
+
 def gatk_filename_from_galaxy( galaxy_filename, galaxy_ext, target_dir = None, prefix = None ):
     suffix = GALAXY_EXT_TO_GATK_EXT.get( galaxy_ext, galaxy_ext )
     if prefix is None:
@@ -29,13 +29,16 @@ def gatk_filename_from_galaxy( galaxy_filename, galaxy_ext, target_dir = None, p
     os.symlink( galaxy_filename, gatk_filename )
     return gatk_filename
 
+
 def gatk_filetype_argument_substitution( argument, galaxy_ext ):
     return argument % dict( file_type = GALAXY_EXT_TO_GATK_FILE_TYPE.get( galaxy_ext, galaxy_ext ) )
+
 
 def open_file_from_option( filename, mode = 'rb' ):
     if filename:
         return open( filename, mode = mode )
     return None
+
 
 def html_report_from_directory( html_out, dir ):
     html_out.write( '<html>\n<head>\n<title>Galaxy - GATK Output</title>\n</head>\n<body>\n<p/>\n<ul>\n' )
@@ -43,22 +46,22 @@ def html_report_from_directory( html_out, dir ):
         html_out.write(  '<li><a href="%s">%s</a></li>\n' % ( fname, fname ) )
     html_out.write( '</ul>\n</body>\n</html>\n' )
 
-def index_bam_files( bam_filenames, tmp_dir ):
+
+def index_bam_files( bam_filenames ):
     for bam_filename in bam_filenames:
         bam_index_filename = "%s.bai" % bam_filename
         if not os.path.exists( bam_index_filename ):
             #need to index this bam file
             stderr_name = tempfile.NamedTemporaryFile( prefix = "bam_index_stderr" ).name
             command = 'samtools index %s %s' % ( bam_filename, bam_index_filename )
-            proc = subprocess.Popen( args=command, shell=True, stderr=open( stderr_name, 'wb' ) )
-            return_code = proc.wait()
-            if return_code:
+            try:
+                subprocess.check_call( args=command, shell=True, stderr=open( stderr_name, 'wb' ) )
+            except:
                 for line in open( stderr_name ):
                     print >> sys.stderr, line
-                os.unlink( stderr_name ) #clean up
-                cleanup_before_exit( tmp_dir )
                 raise Exception( "Error indexing BAM file" )
-            os.unlink( stderr_name ) #clean up
+            finally:
+                os.unlink( stderr_name )
 
 def __main__():
     #Parse Command Line
@@ -74,8 +77,7 @@ def __main__():
     parser.add_option( '-e', '--phone_home', dest='phone_home', action='store', type="string", default='STANDARD', help='What kind of GATK run report should we generate(NO_ET|STANDARD|STDOUT)' )
     parser.add_option( '-K', '--gatk_key', dest='gatk_key', action='store', type="string", default=None, help='What kind of GATK run report should we generate(NO_ET|STANDARD|STDOUT)' )
     (options, args) = parser.parse_args()
-    
-    tmp_dir = tempfile.mkdtemp( prefix='tmp-gatk-' )
+
     if options.pass_through_options:
         cmd = ' '.join( options.pass_through_options )
     else:
@@ -87,42 +89,50 @@ def __main__():
     elif options.max_jvm_heap_fraction is not None:
         cmd = cmd.replace( 'java ', 'java -XX:DefaultMaxRAMFraction=%s  -XX:+UseParallelGC ' % ( options.max_jvm_heap_fraction ), 1 )
     bam_filenames = []
-    if options.datasets:
-        for ( dataset_arg, filename, galaxy_ext, prefix ) in options.datasets:
-            gatk_filename = gatk_filename_from_galaxy( filename, galaxy_ext, target_dir = tmp_dir, prefix = prefix )
-            if dataset_arg:
-                cmd = '%s %s "%s"' % ( cmd, gatk_filetype_argument_substitution( dataset_arg, galaxy_ext ), gatk_filename )
-            if galaxy_ext == "bam":
-                bam_filenames.append( gatk_filename )
-    index_bam_files( bam_filenames, tmp_dir )
-    #set up stdout and stderr output options
-    stdout = open_file_from_option( options.stdout, mode = 'wb' )
-    stderr = open_file_from_option( options.stderr, mode = 'wb' )
-    #if no stderr file is specified, we'll use our own
-    if stderr is None:
-        stderr = tempfile.NamedTemporaryFile( prefix="gatk-stderr-", dir=tmp_dir )
-    
-    proc = subprocess.Popen( args=cmd, stdout=stdout, stderr=stderr, shell=True, cwd=tmp_dir )
-    return_code = proc.wait()
-    
-    if return_code:
-        stderr_target = sys.stderr
-    else:
-        stderr_target = sys.stdout
-    stderr.flush()
-    stderr.seek(0)
-    while True:
-        chunk = stderr.read( CHUNK_SIZE )
-        if chunk:
-            stderr_target.write( chunk )
+    tmp_dir = tempfile.mkdtemp( prefix='tmp-gatk-' )
+    try:
+        if options.datasets:
+            for ( dataset_arg, filename, galaxy_ext, prefix ) in options.datasets:
+                gatk_filename = gatk_filename_from_galaxy( filename, galaxy_ext, target_dir = tmp_dir, prefix = prefix )
+                if dataset_arg:
+                    cmd = '%s %s "%s"' % ( cmd, gatk_filetype_argument_substitution( dataset_arg, galaxy_ext ), gatk_filename )
+                if galaxy_ext == "bam":
+                    bam_filenames.append( gatk_filename )
+                if galaxy_ext == 'fasta':
+                    subprocess.check_call( 'samtools faidx "%s"' % gatk_filename, shell=True )
+                    subprocess.check_call( 'java -jar %s R=%s O=%s' % ( os.path.join(os.environ['JAVA_JAR_PATH'], 'CreateSequenceDictionary.jar'), gatk_filename, os.path.splitext(gatk_filename)[0] + '.dict' ), shell=True )
+        index_bam_files( bam_filenames )
+        #set up stdout and stderr output options
+        stdout = open_file_from_option( options.stdout, mode = 'wb' )
+        stderr = open_file_from_option( options.stderr, mode = 'wb' )
+        #if no stderr file is specified, we'll use our own
+        if stderr is None:
+            stderr = tempfile.NamedTemporaryFile( prefix="gatk-stderr-", dir=tmp_dir )
+
+        proc = subprocess.Popen( args=cmd, stdout=stdout, stderr=stderr, shell=True, cwd=tmp_dir )
+        return_code = proc.wait()
+
+        if return_code:
+            stderr_target = sys.stderr
         else:
-            break
-    stderr.close()
+            stderr_target = sys.stdout
+        stderr.flush()
+        stderr.seek(0)
+        while True:
+            chunk = stderr.read( CHUNK_SIZE )
+            if chunk:
+                stderr_target.write( chunk )
+            else:
+                break
+        stderr.close()
+    finally:
+        cleanup_before_exit( tmp_dir )
+
     #generate html reports
     if options.html_report_from_directory:
         for ( html_filename, html_dir ) in options.html_report_from_directory:
             html_report_from_directory( open( html_filename, 'wb' ), html_dir )
-    
-    cleanup_before_exit( tmp_dir )
 
-if __name__=="__main__": __main__()
+
+if __name__ == "__main__":
+    __main__()
