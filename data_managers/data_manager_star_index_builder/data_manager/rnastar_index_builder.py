92b219e48573882b4e348c17d9dacb38194e1271
fubar <ross.lazarus@gmail.com>
2014-10-10 12:08:38 +1100

    Some more fixes for rna star data manager Not easy to get this working right.

--- data_managers/data_manager_star_index_builder/data_manager/rnastar_index_builder.py
+++ data_managers/data_manager_star_index_builder/data_manager/rnastar_index_builder.py
@@ -6,12 +6,14 @@
 import sys
 import os
 import tempfile
+import shutil
 import optparse
 import subprocess
 
 from json import loads, dumps
 
-DEFAULT_DATA_TABLE_NAME = "rnastar_indexes"
+DEFAULT_DATA_TABLE_NAME = "rnastar_index"
+CHUNK_SIZE = 2**20
 
 def get_id_name( params, dbkey, fasta_description=None):
     #TODO: ensure sequence_id is unique and does not already appear in location file
@@ -26,60 +28,77 @@ def get_id_name( params, dbkey, fasta_description=None):
             sequence_name = dbkey
     return sequence_id, sequence_name
 
-def build_rnastar_index( data_manager_dict, fasta_filename, target_directory, dbkey, sequence_id, sequence_name, data_table_name,
+
+def _run_command( command, target_directory ):
+    tmp_stderr = tempfile.NamedTemporaryFile( prefix = "tmp-data-manager-rnastar_index_builder-stderr" )
+    return_code = subprocess.call( command, shell=False, stdout=sys.stdout, stderr=tmp_stderr.fileno() )
+    if return_code:
+        tmp_stderr.flush()
+        tmp_stderr.seek( 0 )
+        sys.stderr.write( "### Error building index:\n" )
+        while True:
+            chunk = tmp_stderr.read( CHUNK_SIZE )
+            if not chunk:
+                break
+            sys.stderr.write( chunk )
+        sys.exit( return_code )
+    tmp_stderr.close()
+    return return_code
+
+
+def build_rnastar_index( data_manager_dict, fasta_filename,  target_directory, 
+    dbkey, sequence_id, sequence_name, data_table_name,
     sjdbOverhang,sjdbGTFfile, sjdbFileChrStartEnd,sjdbGTFtagExonParentTranscript,sjdbGTFfeatureExon,sjdbGTFchrPrefix,n_threads):
     #TODO: allow multiple FASTA input files
-    #tmp_dir = tempfile.mkdtemp( prefix='tmp-data-manager-bwa-index-builder-' )
-    fasta_base_name = os.path.split( fasta_filename )[-1]
-    sym_linked_fasta_filename = os.path.join( target_directory, fasta_base_name )
-    os.symlink( fasta_filename, sym_linked_fasta_filename )
-    pdict={'target_directory':target_directory,'n_threads':n_threads, 'sjdbFileChrStartEnd':sjdbFileChrStartEnd,
-           'sjdbGTFtagExonParentTranscript':sjdbGTFtagExonParentTranscript, 'sjdbGTFfeatureExon':sjdbGTFfeatureExon,
-           'sjdbGTFchrPrefix':sjdbGTFchrPrefix,'sjdbOverhang':sjdbOverhang, 'sjdbGTFfile':sjdbGTFfile,
-           'sym_linked_fasta_filename':sym_linked_fasta_filename}
+    fasta_base_name = os.path.basename( fasta_filename )
+    #sym_linked_fasta_filename = os.path.join( tmp_dir, fasta_base_name )
+    #os.symlink( fasta_filename, sym_linked_fasta_filename )
+    #print >> sys.stdout,'made',sym_linked_fasta_filename
+    cl = ['STAR','--runMode', 'genomeGenerate', '--genomeFastaFiles', fasta_filename, '--genomeDir', target_directory, '--runThreadN',n_threads ]
     
-    cl = 'STAR --runMode genomeGenerate --genomeFastaFiles %(sym_linked_fasta_filename)s --genomeDir %(target_directory)s --runThreadN %(n_threads)s' % pdict
     if sjdbGTFfile:
-         cl += '''--sjdbGTFchrPrefix %(sjdbGTFchrPrefix)s --sjdbGTFfeatureExon %(sjdbGTFfeatureExon)s --sjdbOverhang %(sjdbOverhang)s
-   --sjdbGTFfile %(sjdbGTFfile)s --sjdbGTFtagExonParentTranscript %(sjdbGTFtagExonParentTranscript)s''' %  pdict
+         cl += [ '--sjdbGTFfeatureExon', sjdbGTFfeatureExon,'--sjdbGTFtagExonParentTranscript',sjdbGTFtagExonParentTranscript]
+         if (sjdbGTFchrPrefix > ''):
+             cl += ['--sjdbGTFchrPrefix', sjdbGTFchrPrefix]
+         cl += ['--sjdbOverhang', sjdbOverhang, '--sjdbGTFfile', sjdbGTFfile]
     elif sjdbFileChrStartEnd:
-        cl += '--sjdbFileChrStartEnd %(sjdbFileChrStartEnd)s --sjdbOverhang %(sjdbOverhangs)s' % pdict
-    args = cl.split(' ')
-    proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
-    return_code = proc.wait()
-    if return_code:
-        sys.exit( return_code )
+        cl += ['--sjdbFileChrStartEnd', sjdbFileChrStartEnd, '--sjdbOverhang', sjdbOverhang]
+    print >> sys.stdout, 'cl =',' '.join(cl)
+    return_code = _run_command(command=cl,target_directory=target_directory)
     data_table_entry = dict( value=sequence_id, dbkey=dbkey, name=sequence_name, path=fasta_base_name )
-    _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry )
-
-def _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry ):
-    data_manager_dict['data_tables'] = data_manager_dict.get( 'data_tables', {} )
-    data_manager_dict['data_tables'][ data_table_name ] = data_manager_dict['data_tables'].get( data_table_name, [] )
-    data_manager_dict['data_tables'][ data_table_name ].append( data_table_entry )
+    data_manager_dict.setdefault('data_tables',{})
+    data_manager_dict['data_tables'].setdefault('data_table_name',[])
+    data_manager_dict['data_tables'][ 'data_table_name' ].append( data_table_entry )
     return data_manager_dict
+
 
 def main():
     #Parse Command Line
     parser = optparse.OptionParser()
-    parser.add_option( '-f', '--fasta_filename', dest='fasta_filename', action='store', type="string", default=None, help='fasta_filename' )
-    parser.add_option( '-d', '--fasta_dbkey', dest='fasta_dbkey', action='store', type="string", default=None, help='fasta_dbkey' )
-    parser.add_option( '-t', '--fasta_description', dest='fasta_description', action='store', type="string", default=None, help='fasta_description' )
-    parser.add_option( '-n', '--data_table_name', dest='data_table_name', action='store', type="string", default=None, help='data_table_name' )
-    parser.add_option( '--out_file', default=None)
-    parser.add_option( '--out_index_path', default=None)
+    parser.add_option( '--fasta_filename', dest='fasta_filename', action='store', type="string", default=None, help='fasta_filename' )
+    parser.add_option( '--fasta_dbkey', dest='fasta_dbkey', action='store', type="string", default=None, help='fasta_dbkey' )
+    parser.add_option( '--fasta_description', dest='fasta_description', action='store', type="string", default=None, help='fasta_description' )
+    parser.add_option( '--data_table_name', dest='data_table_name', action='store', type="string", default=None, help='data_table_name' )
     parser.add_option( '--sjdbGTFfile', type="string", default=None )
     parser.add_option( '--sjdbGTFchrPrefix', type="string", default=None )
     parser.add_option( '--sjdbGTFfeatureExon', type="string", default=None )
     parser.add_option( '--sjdbGTFtagExonParentTranscript', type="string", default=None )
     parser.add_option( '--sjdbFileChrStartEnd', type="string", default=None )
-    parser.add_option( '--sjdbOverhang', type="int", default=100 )
-    parser.add_option( '--runThreadN', type="int", default=4 )
+    parser.add_option( '--sjdbOverhang', type="string", default='100' )
+    parser.add_option( '--runThreadN', type="string", default='4' )
     (options, args) = parser.parse_args()
     
-    filename = options.out_file
+    #filename = options.out_file
+    filename = args[0]
     params = loads( open( filename ).read() )
-    target_directory = options.out_index_path
-    os.mkdirs( target_directory )
+    
+    target_directory = params[ 'output_data' ][0]['extra_files_path'].encode('ascii','replace')
+    try:
+         os.mkdir( target_directory )
+         print >> sys.stdout, target_directory,'created'
+    except OSError:
+        print >> sys.stdout, target_directory,'already exists'
+
     data_manager_dict = {}
     
     dbkey = options.fasta_dbkey
@@ -90,7 +109,8 @@ def main():
     sequence_id, sequence_name = get_id_name( params, dbkey=dbkey, fasta_description=options.fasta_description )
     
     #build the index
-    build_rnastar_index( data_manager_dict, options.fasta_filename, target_directory, dbkey, sequence_id, sequence_name, data_table_name=options.data_table_name,
+    data_manager_dict = build_rnastar_index( data_manager_dict = data_manager_dict, fasta_filename = options.fasta_filename, target_directory = target_directory,
+      dbkey = dbkey, sequence_id = sequence_id, sequence_name = sequence_name, data_table_name=options.data_table_name,
       sjdbOverhang=options.sjdbOverhang,sjdbGTFfile=options.sjdbGTFfile,
       sjdbFileChrStartEnd=options.sjdbFileChrStartEnd,sjdbGTFtagExonParentTranscript=options.sjdbGTFtagExonParentTranscript,
       sjdbGTFfeatureExon=options.sjdbGTFfeatureExon,sjdbGTFchrPrefix=options.sjdbGTFchrPrefix,
