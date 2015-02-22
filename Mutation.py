7e6452f7fb7864a3d8f6d449f2620dfde9cf76f4
Stephen Crosby <stcrosby@gmail.com>
2015-02-21 18:01:23 -0600

    Emailed suggestions added. Thanks for the comments.

--- Mutation.py
+++ Mutation.py
@@ -1,9 +1,13 @@
 import random
 def insertion(mut_seq,changes_list):
     i = random.randint(0,len(mut_seq)-1)
-    base = ['A','T','C','G'][random.randint(0,3)]
-    new_seq = mut_seq[:i] + str(base) + mut_seq[i:]
-    changes_list += ['Insertion of '+str(base)+' at position '+str(i+1)]
+    insertion_length = int(random.uniform(1,100))
+    #Probability should decrease with insertion size?
+    for number in range(insertion_length):
+        bases = ('A','T','C','G')
+        base = random.choice(bases)
+        new_seq = mut_seq[:i] + str(base) + mut_seq[i:]
+        changes_list += ['Insertion of '+str(base)+' at position '+str(i+1)]
     return new_seq,changes_list
 
 def deletion(mut_seq,changes_list):
@@ -33,7 +37,8 @@ def translocation(mut_seq,changes_list):
     i = random.randint(0,len(mut_seq)-1)
     j = random.randint(0,len(mut_seq)-1)
     if i==j:
-        1==1
+        changes_list += 'No Change'
+        return mut_seq,changes_list
     elif i<j:
         endpoints = (i,j)
     else:
@@ -56,28 +61,42 @@ def translocation(mut_seq,changes_list):
 
 
 def main():
-    bases = ['A','T','C','G']
+    number_of_sequences = 2
+    change_list_output = open('change-list.txt', 'w')
+    sequence_output = open('mutated.fa', 'w')
+    bases = ('A','T','C','G')
     mutation_probs = [45,35,15,5]
     base_seq= ''
     for iteration in range(50):
-        base_seq += bases[random.randint(0,3)]
+        base_seq += random.choice(bases)
     mut_seq = base_seq
-    print 'Base Sequence:', base_seq
-    changes_list = []
-    for evolutionary_event in range(50):
-        Prob = random.randint(0,100)
-        if Prob in range(0,mutation_probs[0]):
-            mut_seq,changes_list = insertion(mut_seq,changes_list)
-        elif Prob in range(mutation_probs[0],mutation_probs[0]+mutation_probs[1]):
-            mut_seq,changes_list = deletion(mut_seq,changes_list)
-        elif Prob in range(sum(mutation_probs[0:2]),sum(mutation_probs[0:3])):
-            mut_seq,changes_list = inversion(mut_seq,changes_list)
-        else:
-            mut_seq,changes_list = translocation(mut_seq,changes_list)
-    print 'New Sequence: ',mut_seq
-    print 'Changes:'
-    for change in changes_list:
-        print change
+    sequence_output.write('>original\n%s\n' % base_seq)
+    i=1
+    for sequence in range(number_of_sequences):
+        changes_list = []
+        for evolutionary_event in range(50):
+            Prob = random.randint(0,100)
+            if Prob in range(0,mutation_probs[0]):
+                mut_seq,changes_list = insertion(mut_seq,changes_list)
+            elif Prob in range(mutation_probs[0],mutation_probs[0]+mutation_probs[1]):
+                mut_seq,changes_list = deletion(mut_seq,changes_list)
+            elif Prob in range(sum(mutation_probs[0:2]),sum(mutation_probs[0:3])):
+                mut_seq,changes_list = inversion(mut_seq,changes_list)
+            else:
+                mut_seq,changes_list = translocation(mut_seq,changes_list)
+        sequence_output.write('>inv'+str(i)+'\n%s\n' % mut_seq)
+        for change in changes_list:
+            change_list_output.write(change + "\n")
+        change_list_output.write("End of Sequence" + str(i) + '\n')
+        i+=1
+    change_list_output.close()
+    sequence_output.close()
+    #print 'Base Sequence:', base_seq
+    #print 'New Sequence: ',mut_seq
+    #print 'Changes:'
+    #for change in changes_list:
+        #print change
     
 
-main()
+if __name__ == '__main__':
+    main()
