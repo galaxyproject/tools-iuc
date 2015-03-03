14323ea8e1cbbf14fb2223d5e791245f63a2c012
fubar <ross.lazarus@gmail.com>
2015-03-03 21:35:17 +1100

    Trying to set a default for a data table parameter? Not working. Added str() for all parameters when testing for =="yes" etc..

--- tools/tool_factory_2/getlocalrpackages.py
+++ tools/tool_factory_2/getlocalrpackages.py
@@ -27,12 +27,11 @@ def find_packages(prefix="package_r_"):
     # ['fubar', 'package_r_3_1_1', '63cdb9b2234c']
     if len(fpaths) > 0:
         res = [['%s rev %s owner %s' % (x[1],x[2],x[0]),fpaths[i],False] for i,x in enumerate(fver)]
+        res[0][2] = True # selected if more than one
         res.insert(0,['Use default (system) interpreter','system',False])
     else:
-        res = [['Use default (system) interpreter','system',False],
-        ['**WARNING** NO package env.sh files found - is the "find" system command working? Are any interpreters installed?','system',True]]
-    if len(res) > 2:
-        res[1][2] = True # selected if more than one
+        res = [['Use default (system) interpreter','system',True],
+        ['**WARNING** NO package env.sh files found - is the "find" system command working? Are any interpreters installed?','system',False]]
     # return a triplet - user_sees,value,selected - all unselected if False
     return res
 
