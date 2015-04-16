1225d50deb9781fc7844224c229906353576fbdc
Eric Rasche <rasche.eric@yandex.ru>
2015-04-16 16:26:31 -0500

    more filehandle fixes

--- circos_create_file.py
+++ circos_create_file.py
@@ -44,42 +44,41 @@ def xmfa_parse(xmfa_handle):
 	return return_list
 
 
-def links(backbone_file):
+def links(backbone_file_handle):
 	header_parsed = False
 	links = {}
-	with open(backbone_file, 'r') as handle:
-		for line in handle:
-			if not header_parsed:
-				# There will be 2N where N is the number of genomes in our header_parsed
-				header_parsed = line.split('\t')
-				genome_count = len(header_parsed) / 2
-				# Data structure to hold the links
-				# A from-to list (from col/to col) which will be used to check links between genomes
-				# https://docs.python.org/2/library/itertools.html
-				# from_to for a genome_count of 3 looks like: [(0,  1),  (0,  2),  (1,  2)]
-				# Preferred as it will expand to ANY number of comparisons
-				from_to = list(itertools.combinations(range(genome_count), 2))
-				header_parsed = True
-			else:
-				# Now down to actual parsing
-				link_data = line.split()
-				# Iterate over pairs of columns
-				for x, y in from_to:
-					# Access the link data in that column
-					a_left = link_data[2 * x].strip()
-					a_right = link_data[2 * x + 1].strip()
-					b_left = link_data[2 * y].strip()
-					b_right = link_data[2 * y + 1].strip()
-					# if any of them are zero,  then we can continue,  as this
-					# isn't a "true" link. Circos will plot links from "0 0",  so
-					# any links with "0 0" need to be removed.
-					if a_left != 0 and b_left != 0:
-						key = '%s-%s' % (x, y)
-						if key not in links:
-							links[key] = []
+	for line in backbone_file_handle:
+		if not header_parsed:
+			# There will be 2N where N is the number of genomes in our header_parsed
+			header_parsed = line.split('\t')
+			genome_count = len(header_parsed) / 2
+			# Data structure to hold the links
+			# A from-to list (from col/to col) which will be used to check links between genomes
+			# https://docs.python.org/2/library/itertools.html
+			# from_to for a genome_count of 3 looks like: [(0,  1),  (0,  2),  (1,  2)]
+			# Preferred as it will expand to ANY number of comparisons
+			from_to = list(itertools.combinations(range(genome_count), 2))
+			header_parsed = True
+		else:
+			# Now down to actual parsing
+			link_data = line.split()
+			# Iterate over pairs of columns
+			for x, y in from_to:
+				# Access the link data in that column
+				a_left = link_data[2 * x].strip()
+				a_right = link_data[2 * x + 1].strip()
+				b_left = link_data[2 * y].strip()
+				b_right = link_data[2 * y + 1].strip()
+				# if any of them are zero,  then we can continue,  as this
+				# isn't a "true" link. Circos will plot links from "0 0",  so
+				# any links with "0 0" need to be removed.
+				if a_left != 0 and b_left != 0:
+					key = '%s-%s' % (x, y)
+					if key not in links:
+						links[key] = []
 
-						links['%s-%s' % (x, y)].append(
-							map(int, [a_left, a_right, b_left, b_right]))
+					links['%s-%s' % (x, y)].append(
+						map(int, [a_left, a_right, b_left, b_right]))
 	return links
 
 def reverse_complement(sequence):
@@ -101,8 +100,8 @@ def percent_sequence_identity(seq1, seq2):
 	return float(results.count(True)) / len(results)
 
 
-def write_link_file(names_list, links, link_output, directory):
-	with open(os.path.join(directory, link_output),  'w') as handle:
+def write_link_file(names_list, links, link_output):
+	with open(link_output,  'w') as handle:
 		for key in links:
 			key_from, key_to = key.split('-')  # Rememver, we keyed on 1-3 0-2 3-2 / etc
 			for link in links[key]:  # List of from/to
@@ -162,39 +161,34 @@ def karyotype(seq_file):
 						 (record.id, record.id, len(record.seq), colors_list[i]))
 	return genome_list, karyotype
 
-def add_pct_identity(link_dict, sequence_file, alignment_list):
-	with open(sequence_file, 'r') as handle:
-		lines_list = handle.readlines()
-		for key in link_dict:
-			list_of_links = link_dict[key]
-			key_pair = map(int, key.split('-'))
 
-			for element in list_of_links:
-				first_index_set = [
-					min(abs(element[0]), abs(element[1])),
-					max(abs(element[0]), abs(element[1]))]
-				second_index_set = [
-					min(abs(element[2]), abs(element[3])),
-					max(abs(element[2]), abs(element[3]))]
+def add_pct_identity(link_dict, alignment_list):
+	for key in link_dict:
+		key_pair = map(int, key.split('-'))
 
-				sect_1 = alignment_list[key_pair[0]][first_index_set[0]:first_index_set[1]]
-				sect_2 = alignment_list[key_pair[1]][second_index_set[0]:second_index_set[1]]
-				print key_pair[0], first_index_set[0], first_index_set[1], sect_1
-				print key_pair[0], second_index_set[0], second_index_set[1], sect_2
-				print
+		good_links = []
+		for element in link_dict[key]:
+			first_index_set = [
+				min(abs(element[0]), abs(element[1])),
+				max(abs(element[0]), abs(element[1]))]
+			second_index_set = [
+				min(abs(element[2]), abs(element[3])),
+				max(abs(element[2]), abs(element[3]))]
 
-				if element[0] < 0:
-					sect_1 = reverse_complement(sect_1)
-				if element[2] < 0:
-						sect_2 = reverse_complement(sect_2)
+			sect_1 = alignment_list[key_pair[0]][first_index_set[0]:first_index_set[1]]
+			sect_2 = alignment_list[key_pair[1]][second_index_set[0]:second_index_set[1]]
 
-				if first_index_set[0] != 0 and second_index_set != 0:
-					element += [percent_sequence_identity(sect_1, sect_2)]
-					#pprint.pprint(element)
-			#for item in key_pair:
-			#	print lines_list[2*int(item)+1].strip()[]
-		pprint.pprint(link_dict)
-		return link_dict
+			if element[0] < 0:
+				sect_1 = reverse_complement(sect_1)
+			if element[2] < 0:
+				sect_2 = reverse_complement(sect_2)
+
+			if first_index_set[0] != 0 and second_index_set != 0:
+				good_links.append(element + [percent_sequence_identity(sect_1, sect_2)])
+
+		link_dict[key] = good_links
+	pprint.pprint(link_dict)
+	return link_dict
 
 
 def main():
@@ -235,15 +229,16 @@ def main():
 	# Construct genome-genome link dictionary
 	link_dict = links(args.backbone)
 	# Add %ID to each of the links
-	link_dict = add_pct_identity(link_dict, args.sequence, alignment_list)
+	link_dict = add_pct_identity(link_dict, alignment_list)
 	# Collect 'chromosome' IDs and ...
-	genome_id_list, karyotype_data = karyotype(args.sequence, directory)
+	genome_id_list, karyotype_data = karyotype(args.sequence)
 	# write karyotype data to output/karyotype.txt
-	with open(os.path.join(directory, 'karyotype.txt'), 'w') as handle:
+	with open(os.path.join(args.output, 'karyotype.txt'), 'w') as handle:
 		for chrom in karyotype_data:
 			handle.write(chrom)
 	# Write output/links.txt
-	write_link_file(genome_id_list, link_dict, link_output, directory)
+	write_link_file(genome_id_list, link_dict,
+					os.path.join(args.output, 'links.txt'))
 
 	sample_conf = open('sample_conf.conf', 'r')
 	output_conf = open(os.path.join(directory, output_conf_filename), 'w')
