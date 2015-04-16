ded6c703e7243832e564a8ff904d584b8a2882b7
Eric Rasche <rasche.eric@yandex.ru>
2015-04-16 15:40:07 -0500

    Fix spacing bug

--- circos_create_file.py
+++ circos_create_file.py
@@ -64,21 +64,22 @@ def links(backbone_file, link_output):
 				link_data = line.split()
 				# Iterate over pairs of columns
 				for x, y in from_to:
+					print x, y
 					# Access the link data in that column
 					a_left = link_data[2 * x].strip()
 					a_right = link_data[2 * x + 1].strip()
 					b_left = link_data[2 * y].strip()
 					b_right = link_data[2 * y + 1].strip()
-				# if any of them are zero,  then we can continue,  as this
-				# isn't a "true" link. Circos will plot links from "0 0",  so
-				# any links with "0 0" need to be removed.
-				if a_left != 0 and b_left != 0:
-					key = '%s-%s' % (x, y)
-					if key not in links:
-						links[key] = []
+					# if any of them are zero,  then we can continue,  as this
+					# isn't a "true" link. Circos will plot links from "0 0",  so
+					# any links with "0 0" need to be removed.
+					if a_left != 0 and b_left != 0:
+						key = '%s-%s' % (x, y)
+						if key not in links:
+							links[key] = []
 
-					links['%s-%s' % (x, y)].append(
-						map(int, [a_left, a_right, b_left, b_right]))
+						links['%s-%s' % (x, y)].append(
+							map(int, [a_left, a_right, b_left, b_right]))
 	return links
 
 def reverse_complement(sequence):
@@ -88,15 +89,16 @@ def reverse_complement(sequence):
 		new_seq+=DNA_pairing_dict[letter]
 	return new_seq
 
+
 def percent_sequence_identity(seq1, seq2):
-	i=0
-	total_length = max(len(seq1), len(seq2))
-	matches = 0
-	while i < min(len(seq1), len(seq2)):
-		if seq1[i]==seq2[i]:
-			matches+=1
-		i+=1
-	return float(matches)/float(total_length)
+	if len(seq1) == 0 or len(seq2) == 0:
+		return 0
+
+	results = [seq1[i] == seq2[i]
+				for i in range(min(len(seq1), len(seq2)))]
+	# Count the trues, return that as % of length
+	# Currently will underestimate sequences
+	return float(results.count(True)) / len(results)
 
 def write_link_file(names_list,  links,  link_output, directory):
 	with open(os.path.join(directory, link_output),  'w') as handle:
@@ -176,28 +178,34 @@ def karyotype(seq_file, karyotype_name, directory):
 def add_pct_identity(link_dict, sequence_file, alignment_list):
 	with open(sequence_file, 'r') as handle:
 		lines_list = handle.readlines()
-		key_pair = []
 		for key in link_dict:
 			list_of_links = link_dict[key]
-			key_pair+=key[0]
-			key_pair+=key[-1]
+			key_pair = map(int, key.split('-'))
+
 			for element in list_of_links:
-				first_index_set = [min(abs(int(element[0])), abs(int(element[1]))), max(abs(int(element[0])), abs(int(element[1])))]
-				second_index_set = [min(abs(int(element[2])), abs(int(element[3]))), max(abs(int(element[2])), abs(int(element[3])))]
-				sect_1 = alignment_list[int(key_pair[0])+1][first_index_set[0]:first_index_set[1]]
-				sect_2 = alignment_list[int(key_pair[1])+1][second_index_set[0]:second_index_set[1]]
-				if int(element[0]) < 0:
+				first_index_set = [
+					min(abs(element[0]), abs(element[1])),
+					max(abs(element[0]), abs(element[1]))]
+				second_index_set = [
+					min(abs(element[2]), abs(element[3])),
+					max(abs(element[2]), abs(element[3]))]
+
+				sect_1 = alignment_list[key_pair[0]][first_index_set[0]:first_index_set[1]]
+				sect_2 = alignment_list[key_pair[1]][second_index_set[0]:second_index_set[1]]
+				print key_pair[0], first_index_set[0], first_index_set[1], sect_1
+				print key_pair[0], second_index_set[0], second_index_set[1], sect_2
+				print
+
+				if element[0] < 0:
 					sect_1 = reverse_complement(sect_1)
-				if int(element[2]) < 0:
+				if element[2] < 0:
 						sect_2 = reverse_complement(sect_2)
-				#print key_pair[0], sect_1
-				#print key_pair[1], sect_2
-				if sect_1!= '' and sect_2!= '':
+
+				if first_index_set[0] != 0 and second_index_set != 0:
 					element += [percent_sequence_identity(sect_1, sect_2)]
 					#pprint.pprint(element)
 			#for item in key_pair:
 			#	print lines_list[2*int(item)+1].strip()[]
-			key_pair = []
 		pprint.pprint(link_dict)
 		return link_dict
 
@@ -236,10 +244,10 @@ if __name__ == '__main__':
 		#print "\n%s..%s" % (i, i + 100)
 		#print '\n'.join(x[i:i + 100] for x in alignment_list[1:])
 	link_dict = links(backbone_file, link_output)
-	link_dict = add_pct_identity(link_dict, os.path.join(directory, seq_filename), alignment_list)
 	print link_dict
 	import sys
 	sys.exit()
+	link_dict = add_pct_identity(link_dict, os.path.join(directory, seq_filename), alignment_list)
 	write_link_file(karyotype(os.path.join(directory, seq_filename), karyotype_name, directory), link_dict, link_output, directory)
 	sample_conf = open('sample_conf.conf', 'r')
 	output_conf = open(os.path.join(directory, output_conf_filename), 'w')
