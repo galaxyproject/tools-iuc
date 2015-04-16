857dee977995504b4edc3e525301dbc335a77ee7
Eric Rasche <rasche.eric@yandex.ru>
2015-04-16 13:51:27 -0500

    Whitespace fixes

--- New_Mut.py
+++ New_Mut.py
@@ -4,73 +4,73 @@ import os.path
 #DNA_pairing_dict = {'A':'T','G':'C','T':'A','C':'G'}
 #transcription_dict = {'A':'U','G':'C','T':'A','C':'G'}
 
-class Nucleic_Acid:     
-    class DNA: 
-        def __init__(self,length):
-            self.variety = 'DNA'
-            self.nucl_seq = ''
-            self.list_of_changes = []
-            for nucleotide in range(length):
-                self.nucl_seq+=('A','T','G','C')[random.randint(0,3)]
-            
-        def reverse_complement(self,start,end):
-            DNA_pairing_dict = {'A':'T','G':'C','T':'A','C':'G'}        
-            rev_comp_template = self.nucl_seq[start:end]
-            rev_comp_product = ''
-            for letter in rev_comp_template[::-1]:
-                if self.variety == 'DNA':
-                    rev_comp_product += DNA_pairing_dict[letter]                   
-            return rev_comp_product        
-        
-        def mutation_event(self):
-            length = random.randint(1,100)
-            start = random.randint(0,len(self.nucl_seq))
-            def insertion(self,length,start):
-                insertion_fragment = ''.join([('A','T','G','C')[random.randint(0,3)] for instance in range(length)])
-                self.nucl_seq = self.nucl_seq[:start+1]+insertion_fragment+self.nucl_seq[start+1:]
-                
-            def deletion(self,length,start):
-                self.nucl_seq = self.nucl_seq[:start+1]+self.nucl_seq[start+length+1:]
-            
-            def inversion(self,length,start):
-                self.nucl_seq = self.nucl_seq[:start+1]+self.reverse_complement(start+1,start+1+length)+self.nucl_seq[start+1+length:]
-                self.list_of_changes += ('inversion',start,start+length)                                                                                                      
-            def translocation(self,length,start):
-                translocated_segment = self.nucl_seq[start:start+length+1]
-                new_DNA = self.nucl_seq[:start]+self.nucl_seq[start+length+1:]
-                new_start_pos = random.randint(0,len(new_DNA))
-                self.nucl_seq = new_DNA[:new_start_pos]+translocated_segment+new_DNA[new_start_pos:]
-            prob = random.randint(1,100)
-            if prob<=35:
-                insertion(self,length,start)
-            elif prob<=70:
-                deletion(self,length,start)
-            if prob<=90:
-                translocation(self,length,start)
-            else:
-		inversion(self,length,start)
-    
-    
+class Nucleic_Acid:
+	class DNA:
+		def __init__(self,length):
+			self.variety = 'DNA'
+			self.nucl_seq = ''
+			self.list_of_changes = []
+			for nucleotide in range(length):
+				self.nucl_seq+=('A','T','G','C')[random.randint(0,3)]
+
+		def reverse_complement(self,start,end):
+			DNA_pairing_dict = {'A':'T','G':'C','T':'A','C':'G'}
+			rev_comp_template = self.nucl_seq[start:end]
+			rev_comp_product = ''
+			for letter in rev_comp_template[::-1]:
+				if self.variety == 'DNA':
+					rev_comp_product += DNA_pairing_dict[letter]
+			return rev_comp_product
+
+		def mutation_event(self):
+			length = random.randint(1,100)
+			start = random.randint(0,len(self.nucl_seq))
+			def insertion(self,length,start):
+				insertion_fragment = ''.join([('A','T','G','C')[random.randint(0,3)] for instance in range(length)])
+				self.nucl_seq = self.nucl_seq[:start+1]+insertion_fragment+self.nucl_seq[start+1:]
+
+			def deletion(self,length,start):
+				self.nucl_seq = self.nucl_seq[:start+1]+self.nucl_seq[start+length+1:]
+
+			def inversion(self,length,start):
+				self.nucl_seq = self.nucl_seq[:start+1]+self.reverse_complement(start+1,start+1+length)+self.nucl_seq[start+1+length:]
+				self.list_of_changes += ('inversion',start,start+length)
+			def translocation(self,length,start):
+				translocated_segment = self.nucl_seq[start:start+length+1]
+				new_DNA = self.nucl_seq[:start]+self.nucl_seq[start+length+1:]
+				new_start_pos = random.randint(0,len(new_DNA))
+				self.nucl_seq = new_DNA[:new_start_pos]+translocated_segment+new_DNA[new_start_pos:]
+			prob = random.randint(1,100)
+			if prob<=35:
+				insertion(self,length,start)
+			elif prob<=70:
+				deletion(self,length,start)
+			if prob<=90:
+				translocation(self,length,start)
+			else:
+				inversion(self,length,start)
+
+
 
 if __name__ == '__main__':
-    output = open(os.path.join('/home/users/CPT/CPT/491/scrosby/Circos',"test2_0409.fa"),'w')
-    init_list = [Nucleic_Acid.DNA(random.randint(1000,2000)) for number_of_sequences in range(1)]
-    mut_list = []
-    ops_list = []
-    for number_of_sequences in range(5):
-        obj2 = Nucleic_Acid.DNA(random.randint(1000,2000))
-        obj2.nucl_seq = init_list[0].nucl_seq
-        for event in range(500):
-            obj2.mutation_event()
-        mut_list += [obj2]
-        ops_list += [obj2.list_of_changes]
-    output.write('>Original\n')
-    output.write(str(init_list[0].nucl_seq)+'\n')
-    i=0
-    for sequence in mut_list:
-        i+=1
-        output.write('>Mut%s\n' % i)
-        output.write(str(sequence.nucl_seq) + '\n')
-    output.close()
+	output = open(os.path.join('/home/users/CPT/CPT/491/scrosby/Circos',"test2_0409.fa"),'w')
+	init_list = [Nucleic_Acid.DNA(random.randint(1000,2000)) for number_of_sequences in range(1)]
+	mut_list = []
+	ops_list = []
+	for number_of_sequences in range(5):
+		obj2 = Nucleic_Acid.DNA(random.randint(1000,2000))
+		obj2.nucl_seq = init_list[0].nucl_seq
+		for event in range(500):
+			obj2.mutation_event()
+		mut_list += [obj2]
+		ops_list += [obj2.list_of_changes]
+	output.write('>Original\n')
+	output.write(str(init_list[0].nucl_seq)+'\n')
+	i=0
+	for sequence in mut_list:
+		i+=1
+		output.write('>Mut%s\n' % i)
+		output.write(str(sequence.nucl_seq) + '\n')
+	output.close()
    #print ops_list
-        
+
--- circos_create_file.py
+++ circos_create_file.py
@@ -4,41 +4,41 @@ import itertools
 import re
 
 def xmfa_parse(xmfa,num_seq):
-    start_regex = re.compile('>\s[1-9]:')
-    seq_info_list = [{} for num in range(num_seq+1)]
-    seq_string = ''
-    seq_string_2 = ''
-    i=0
-    return_list = []
-    with open(xmfa,'r') as handle:
-        for line in handle.readlines():
-            if start_regex.match(line) != None:
-                if i>0:
-                    seq_info_list[int(current_seq)][int(seq_start)] = seq_string
-                seq_start = ''
-                seq_string = ''
-                current_seq = line[2]
-                for character in line[4::]:
-                    if character != '-':
-                        seq_start += str(character)
-                    else:
-                        break
-                i+=1
-            elif line[0] != '=':
-                seq_string += line.strip()
+	start_regex = re.compile('>\s[1-9]:')
+	seq_info_list = [{} for num in range(num_seq+1)]
+	seq_string = ''
+	seq_string_2 = ''
+	i=0
+	return_list = []
+	with open(xmfa,'r') as handle:
+		for line in handle.readlines():
+			if start_regex.match(line) != None:
+				if i>0:
+					seq_info_list[int(current_seq)][int(seq_start)] = seq_string
+				seq_start = ''
+				seq_string = ''
+				current_seq = line[2]
+				for character in line[4::]:
+					if character != '-':
+						seq_start += str(character)
+					else:
+						break
+				i+=1
+			elif line[0] != '=':
+				seq_string += line.strip()
 	else:
-	    seq_info_list[int(current_seq)][int(seq_start)] = seq_string
-    for d in seq_info_list:
-        seq_string_2 = ''
-        for element in d:
-            seq_string_2 += d[element]
-        return_list += [seq_string_2]
-    return return_list
+		seq_info_list[int(current_seq)][int(seq_start)] = seq_string
+	for d in seq_info_list:
+		seq_string_2 = ''
+		for element in d:
+			seq_string_2 += d[element]
+		return_list += [seq_string_2]
+	return return_list
 
 def links(backbone_file,link_output):
 	lines = open(backbone_file,'r').readlines()
 	# There will be 2N where N is the number of genomes in our header
-	header = lines[0].split('\t') 
+	header = lines[0].split('\t')
 	genome_count = len(header) / 2
 	# Data structure to hold the links
 	links = {}
@@ -49,24 +49,24 @@ def links(backbone_file,link_output):
 	from_to = list(itertools.combinations(range(genome_count), 2))
 	# Now down to actual parsing
 	for line in lines[1:]:
-	  # Split with a tab. I can't remember if the file is tab separated, if not, then use `.split()` and it'll split on any whitespace.
-	  link_data = line.split('\t')
-	  # Iterate over pairs of columns
-	  for x, y in from_to:
-	    # Access the link data in that column
-	    a_left = link_data[2*x].strip()
-	    a_right = link_data[2*x + 1].strip()
-	    b_left = link_data[2*y].strip()
-	    b_right = link_data[2*y + 1].strip()
-	    # if any of them are zero, then we can continue, as this isn't a "true" link.
-	    # Circos will plot links from "0 0", so any links with "0 0" need to be removed.
-	    if a_left != 0  and b_left != 0:
-	        try:
-	            links['%s-%s' % (x,y)].append([a_left, a_right, b_left, b_right])
-	        except:
-	            links['%s-%s' % (x,y)] = [
-	          [a_left, a_right, b_left, b_right]
-		    ]
+		# Split with a tab. I can't remember if the file is tab separated, if not, then use `.split()` and it'll split on any whitespace.
+		link_data = line.split('\t')
+		# Iterate over pairs of columns
+		for x, y in from_to:
+		# Access the link data in that column
+		a_left = link_data[2*x].strip()
+		a_right = link_data[2*x + 1].strip()
+		b_left = link_data[2*y].strip()
+		b_right = link_data[2*y + 1].strip()
+		# if any of them are zero, then we can continue, as this isn't a "true" link.
+		# Circos will plot links from "0 0", so any links with "0 0" need to be removed.
+		if a_left != 0  and b_left != 0:
+			try:
+				links['%s-%s' % (x,y)].append([a_left, a_right, b_left, b_right])
+			except:
+				links['%s-%s' % (x,y)] = [
+			        [a_left, a_right, b_left, b_right]
+			    ]
 	return links
 
 def reverse_complement(sequence):
@@ -91,9 +91,9 @@ def write_link_file(names_list, links, link_output,directory):
 		for key in links:
 			key_from, key_to = key.split('-') # Rememver, we keyed on 1-3 0-2 3-2 / etc
 			for link in links[key]: # List of from/to
-        # Create the list by re-arranging the link_data
-        # Circos will plot links from "0 0", so any links with "0 0" need to be removed.
-				if len(link) == 5: 
+		# Create the list by re-arranging the link_data
+		# Circos will plot links from "0 0", so any links with "0 0" need to be removed.
+				if len(link) == 5:
 					if int(link[0]) > 0 and int(link[2])>0:
 						data = [names_list[int(key_from)]] + link[0:2] + [names_list[int(key_to)]] + link[2:4]
 						if float(link[4])<0.3:
@@ -120,7 +120,7 @@ def write_link_file(names_list, links, link_output,directory):
 							#print ' '.join(data) + " color=lblue \n"
 					else:
 						link[2] = str(-1*int(link[2]))
-						link[3] = str(-1*int(link[3]))						
+						link[3] = str(-1*int(link[3]))
 						data = [names_list[int(key_from)]] + link[0:2] + [names_list[int(key_to)]] + link[2:4]
 						if float(link[4])<0.3:
 							handle.write(' '.join(data) + " color=lred \n")
@@ -129,10 +129,10 @@ def write_link_file(names_list, links, link_output,directory):
 							handle.write(' '.join(data) + " color=red \n")
 							#print ' '.join(data) + " color=lblue \n"
 						else:
-							handle.write(' '.join(data) + " color=dred \n")	
+							handle.write(' '.join(data) + " color=dred \n")
 							#print ' '.join(data) + " color=lblue \n"
-					
-					
+
+
 
 def karyotype(seq_file,karyotype_name,directory):
 	i=0
@@ -142,14 +142,14 @@ def karyotype(seq_file,karyotype_name,directory):
 	seq_file = open(seq_file,'r')
 	preceding_line_was_header = 0
 	for line in seq_file.readlines():
-                if preceding_line_was_header == 1:
+				if preceding_line_was_header == 1:
 			genome_tuple = (name,len(line))
 			genome_list += [genome_tuple]
-                preceding_line_was_header = 0
+				preceding_line_was_header = 0
 		name = ''
 		genome_tuple = ('','')
-                if line[0] == '>':
-                        preceding_line_was_header = 1
+				if line[0] == '>':
+						preceding_line_was_header = 1
 			#Circos does not like ">" in the genome names.
 			name = line[1:].strip()
 	seq_file.close()
@@ -177,7 +177,7 @@ def add_pct_identity(link_dict,sequence_file,alignment_list):
 				if int(element[0]) < 0:
 					sect_1 = reverse_complement(sect_1)
 				if int(element[2]) < 0:
-				        sect_2 = reverse_complement(sect_2)
+						sect_2 = reverse_complement(sect_2)
 				#print key_pair[0],sect_1
 				#print key_pair[1],sect_2
 				if sect_1!= '' and sect_2!= '':
@@ -207,7 +207,7 @@ def main():
 	link_dict = add_pct_identity(link_dict,os.path.join(directory,seq_filename),alignment_list)
 	write_link_file(karyotype(os.path.join(directory,seq_filename),karyotype_name,directory),link_dict,link_output,directory)
 	sample_conf = open('sample_conf.conf','r')
-        output_conf = open(os.path.join(directory,output_conf_filename),'w')
+		output_conf = open(os.path.join(directory,output_conf_filename),'w')
 	for line in sample_conf.readlines():
 		i = 0
 		while i!= len(line):
@@ -231,7 +231,7 @@ def main():
 				break
 	sample_conf.close()
 	output_conf.close()
-	
-	
+
+
 if __name__ == '__main__':
 	main()
