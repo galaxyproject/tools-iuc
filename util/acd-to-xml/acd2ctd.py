9853e715dba4a1ebf5c7d64ccddf1579d60e7132
Eric Rasche <rasche.eric@yandex.ru>
2015-05-08 18:03:08 -0500

    More work on getting options in the right places

--- util/acd-to-xml/acd2ctd.py
+++ util/acd-to-xml/acd2ctd.py
@@ -43,47 +43,52 @@ tool = CTDModel(
     description=root.find('metadata').find('documentation').text
 )
 
+def handle_parameter(child):
+    kwargs = {
+        'type': child.attrib['type']
+    }
+    if child.attrib['type'] in INPUT_TYPE_MAPPING.keys():
+        kwargs['type'] = 'input-file'
+        if child.find('knowntype') is not None:
+            kwargs['file_formats'] = [child.find('knowntype').text]
+
+        kwargs.update(INPUT_TYPE_MAPPING[child.attrib['type']])
+    elif child.attrib['type'] in ('integer', 'float'):
+        num_min = None
+        num_max = None
+        if child.find('minimum') is not None:
+            num_min = float(child.find('minimum').text)
+        if child.find('maximum') is not None:
+            num_max = float(child.find('maximum').text)
+        num_default = float(child.find('default').text)
+
+        if child.attrib['type'] == 'integer':
+            if num_min is not None:
+                num_min = int(num_min)
+            if num_max is not None:
+                num_max = int(num_max)
+            num_default = int(num_default)
+
+        kwargs['num_range'] = (num_min, num_max)
+        kwargs['default'] = num_default
+        kwargs['type'] = int if child.attrib['type'] == 'integer' else float
+    elif child.attrib['type'] in ('boolean', 'toggle'):
+        kwargs['type'] = str
+        kwargs['choices'] = ['true', 'false']
+
+    if child.find('information') is not None:
+        kwargs['description'] = child.find('information').text
+
+    kwargs['required'] = required
+
+    print kwargs
+    return kwargs
+
 
 def handle_section(section, subparams, required=False):
     for child in section.getchildren():
         if child.tag == 'parameter':
-            kwargs = {
-                'type': child.attrib['type']
-            }
-            if child.attrib['type'] in INPUT_TYPE_MAPPING.keys():
-                kwargs['type'] = 'input-file'
-                if child.find('knowntype') is not None:
-                    kwargs['file_formats'] = [child.find('knowntype').text]
-
-                kwargs.update(INPUT_TYPE_MAPPING[child.attrib['type']])
-            elif child.attrib['type'] in ('integer', 'float'):
-                num_min = None
-                num_max = None
-                if child.find('minimum') is not None:
-                    num_min = float(child.find('minimum').text)
-                if child.find('maximum') is not None:
-                    num_max = float(child.find('maximum').text)
-                num_default = float(child.find('default').text)
-
-                if child.attrib['type'] == 'integer':
-                    if num_min is not None:
-                        num_min = int(num_min)
-                    if num_max is not None:
-                        num_max = int(num_max)
-                    num_default = int(num_default)
-
-                kwargs['num_range'] = (num_min, num_max)
-                kwargs['default'] = num_default
-                kwargs['type'] = int if child.attrib['type'] == 'integer' else float
-
-            if child.find('information') is not None:
-                kwargs['description'] = child.find('information').text
-
-
-            if required:
-                kwargs['required'] = True
-
-            print kwargs
+            kwargs = handle_parameter(child)
             subparams.add(
                 child.attrib['name'],
                 **kwargs
@@ -96,28 +101,39 @@ def handle_section(section, subparams, required=False):
         else:
             print child.tag
 
+output_section = None
 for section in root.findall('section'):
+    print section.attrib
     if section.attrib['id'] in ACD_INPUTS:
-        required = section.attrib['id'] == 'required'
+        required = section.attrib['id'] in ('required', 'input')
         subparams = tool.add_group(section.attrib['id'],
                                    section.find('metadata').find('information').text)
         handle_section(section, subparams, required=required)
     else:
-        print section.attrib
-        for parameter in section.findall('parameter'):
-            print parameter.attrib
-            kwargs = {
-                'required': True,
-                'type': 'output-file',
-            }
-            if parameter.find('extension') is not None:
-                kwargs['file_formats'] = [parameter.find('extension').text]
-            if parameter.find('information') is not None:
-                kwargs['description'] = parameter.find('information').text
+        if output_section is None:
+            output_section = tool.add_group('output', 'Output Options')
 
-            tool.add(
-                parameter.attrib['name'],
-                **kwargs
-            )
+        for parameter in section.findall('parameter'):
+            if parameter.attrib['type'] in ('toggle', ):
+                kwargs = handle_parameter(parameter)
+                output_section.add(
+                    parameter.attrib['name'],
+                    **kwargs
+                )
+            else:
+                print parameter.attrib
+                kwargs = {
+                    'required': True,
+                    'type': 'output-file',
+                }
+                if parameter.find('extension') is not None:
+                    kwargs['file_formats'] = [parameter.find('extension').text]
+                if parameter.find('information') is not None:
+                    kwargs['description'] = parameter.find('information').text
+
+                tool.add(
+                    parameter.attrib['name'],
+                    **kwargs
+                )
 
 tool.write_ctd('example.ctd')
