9674e5a286ee5aa49995c5dd4296e50415a86c65
Eric Rasche <esr@tamu.edu>
2015-05-13 14:40:42 -0500

    Make parameters more generic

--- tools/jbrowse/jbrowse.py
+++ tools/jbrowse/jbrowse.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import shutil
 import argparse
 import subprocess
 import hashlib
@@ -17,35 +16,53 @@ def process_genome(jbrowse_dir, genome):
     subprocess.check_output(['perl', 'bin/prepare-refseqs.pl', '--fasta', genome], cwd=jbrowse_dir)
 
 
-def process_annotations(jbrowse_dir, annot_file, annot_label, annot_format,
+def process_annotations(jbrowse_dir, data, label, format,
                         **kwargs):
-    key = hashlib.md5(annot_file).hexdigest()
+    key = hashlib.md5(data).hexdigest()
 
-    cmd = ['perl', 'bin/flatfile-to-json.pl', TN_TABLE.get(annot_format, 'gff'),
-           annot_file, '--trackLabel', key, '--key', annot_label]
+    if format in ('gff', 'gff3', 'bed'):
+        cmd = ['perl', 'bin/flatfile-to-json.pl', TN_TABLE.get(format, 'gff'),
+               data, '--trackLabel', key, '--key', label]
+    elif format in ('bigwig', 'wig'):
+        cmd = []
+    elif format in ('bam', ):
+        cmd = []
+    elif format in ('blastxml', ):
+        cmd = []
     subprocess.check_output(cmd, cwd=jbrowse_dir)
 
+
+def clone_jbrowse(jbrowse_dir, destination):
+    # JBrowse seems to have included some bad symlinks, cp ignores bad symlinks
+    # unlike copytree
+    cmd = ['mkdir', '-p', destination]
+    subprocess.check_output(cmd)
+    cmd = ['cp', '-r', jbrowse_dir, destination]
+    subprocess.check_output(cmd)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="", epilog="")
     parser.add_argument('genome', type=file, help='Input genome file')
 
-    parser.add_argument('--gff3', type=file, nargs='*', help='GFF3/BED/GBK File')
-    parser.add_argument('--gff3_format', choices=['gff3', 'gff', 'bed', 'gbk'], nargs='*', help='GFF3/BED/GBK Format')
-    parser.add_argument('--gff3_label', type=str, nargs='*', help='GFF3/BED/GBK Label')
+    parser.add_argument('--annotation', type=file, nargs='*',
+                        help='annotation data')
+    parser.add_argument('--annotation_format', choices=['gff3', 'gff', 'bed', 'bam', 'wig', 'bigwig'],
+                        nargs='*', help='annotation format')
+    parser.add_argument('--annotation_label', type=str, nargs='*',
+                        help='annotation label')
 
     parser.add_argument('--jbrowse', help='Folder containing a jbrowse release')
     parser.add_argument('--outdir', help='Output directory', default='out')
     args = parser.parse_args()
 
     jbrowse_dir = os.path.join(args.outdir, 'JBrowse-1.11.6')
-    shutil.copytree(args.jbrowse, jbrowse_dir)
+    clone_jbrowse(args.jbrowse, args.outdir)
 
     process_genome(jbrowse_dir, os.path.realpath(args.genome.name))
 
-    for gff3, gff3_format, gff3_label in zip(args.gff3, args.gff3_format, args.gff3_label):
-        gff3_path = os.path.realpath(gff3.name)
-        process_annotations(jbrowse_dir, gff3_path, gff3_label, gff3_format)
+    for annotation, format, label in zip(args.annotation, args.annotation_format, args.annotation_label):
+        path = os.path.realpath(annotation.name)
+        process_annotations(jbrowse_dir, path, label, format)
 
     print """
     <html>
