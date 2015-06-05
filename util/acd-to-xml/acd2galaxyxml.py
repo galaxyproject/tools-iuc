1ba85c7f8406f0e549a032886275d520b97200ca
Eric Rasche <esr@tamu.edu>
2015-06-05 13:24:19 -0500

    More progress

--- util/acd-to-xml/acd2galaxyxml.py
+++ util/acd-to-xml/acd2galaxyxml.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from lxml import etree as ET
+import re
 import sys
 import logging
 import pprint
@@ -13,30 +14,50 @@ INPUT_TYPE_MAPPING = {
     'assembly': {},
     'codon': {},
     'cpdb': {},
-    'datafile': {'file_formats': ['data']},
+    'datafile': {'file_format': 'data'},
     'directory': {},
     'dirlist': {},
     'features': {},
     'filelist': {},
-    'infile': {'file_formats': ['data']},
+    'infile': {'file_format': 'data'},
     'matrix': {},
     'matrixf': {},
-    'obo': {'file_formats': ['obo']},
+    'obo': {'file_format': 'obo'},
     'pattern': {},
     'refseq': {},
     'regexp': {},
-    'resource': {'file_formats': ['data']},
+    'resource': {'file_format': 'data'},
     'scop': {},
-    'sequence': {'file_formats': ['fasta']},
-    'seqall': {'file_formats': ['fasta']},
-    'seqset': {'file_formats': ['fasta']},
-    'seqsetall': {'file_formats': ['fasta']},
+    'sequence': {'file_format': 'fasta'},
+    'seqall': {'file_format': 'fasta'},
+    'seqset': {'file_format': 'fasta'},
+    'seqsetall': {'file_format': 'fasta'},
     'url': {'type': str},
-    'variation': {'file_formats': ['vcf']},
-    'xml': {'file_formats': ['xml']},
+    'variation': {'file_format': 'vcf'},
+    'xml': {'file_format': 'xml'},
     'taxon': {'type': str},
     'text': {'type': str},
 }
+
+
+def unbreak_strings(data):
+    return re.sub('\s*[\r\n]\s*', ' ', data)
+
+
+def valid_xml_char_ordinal(c):
+    # http://stackoverflow.com/questions/8733233/filtering-out-certain-bytes-in-python
+    codepoint = ord(c)
+    # conditions ordered by presumed frequency
+    return (
+        0x20 <= codepoint <= 0xD7FF or
+        codepoint in (0x9, 0xA, 0xD) or
+        0xE000 <= codepoint <= 0xFFFD or
+        0x10000 <= codepoint <= 0x10FFFF
+    )
+
+
+def clean_string(data):
+    return ''.join(c for c in data if valid_xml_char_ordinal(c)).decode('utf8')
 
 
 tree = ET.parse(sys.argv[1])
@@ -48,7 +69,7 @@ tool = ET.Element("tool",
                   version='6.6.0'
                   )
 desc = ET.Element("description")
-desc.text = root.find('metadata').find('documentation').text
+desc.text = unbreak_strings(root.find('metadata').find('documentation').text)
 tool.append(desc)
 macros = ET.Element("macros")
 macros_import = ET.Element("import")
@@ -62,107 +83,159 @@ version_command.text = "%s -version" % root.attrib['id']
 tool.append(version_command)
 
 
-def handle_parameter(child):
+def __input_file(child):
+    #<param label="Genome" name="positional_1" type="data" format="fasta"/>
     kwargs = {
+        'type': 'data'
+    }
+    if child.find('knowntype') is not None:
+        kwargs['file_format'] = child.find('knowntype').text
+
+    #if child.find('information') is not None:
+        #kwargs['help'] = child.find('information').text
+
+    if child.find('help') is not None:
+        kwargs['help'] = unbreak_strings(child.find('help').text)
+    # Unhandled: additional, default, nullok, relations, ... ?
+
+    kwargs.update(INPUT_TYPE_MAPPING[child.attrib['type']])
+    return kwargs
+
+
+def __numeric_input(child):
+    num_min = None
+    num_max = None
+    if child.find('minimum') is not None:
+        try:
+            num_min = float(child.find('minimum').text)
+        except Exception:
+            pass
+    if child.find('maximum') is not None:
+        try:
+            num_max = float(child.find('maximum').text)
+        except Exception:
+            pass
+
+    try:
+        num_default = float(child.find('default').text)
+    except Exception:
+        num_default = 0
+
+    if child.attrib['type'] == 'integer':
+        if num_min is not None:
+            num_min = int(num_min)
+        if num_max is not None:
+            num_max = int(num_max)
+        num_default = int(num_default)
+
+    kwargs = {
+        'maximum': str(num_max),
+        'default': str(num_default),
         'type': child.attrib['type']
     }
+
+    if num_min is not None:
+        kwargs['minimum'] = str(num_min)
+
+    if num_max is not None:
+        kwargs['maximum'] = str(num_max)
+
+    return kwargs
+
+
+def __boolean_input(child):
+    kwargs = {
+        'type': 'boolean',
+        'name': child.attrib['name'],
+        'truevalue': '-%s' % child.attrib['name'],
+        'falsevalue': '',
+    }
+    return kwargs
+
+
+def __list_input(child):
+    kwargs = {}
+    if child.find('delimiter') is not None:
+        delimiter = child.find('delimiter').text
+    else:
+        delimiter = ';'
+
+    if child.find('codedelimiter') is not None:
+        codedelimiter = child.find('codedelimiter').text
+    else:
+        codedelimiter = ':'
+
+    kv = {}
+    for x in child.find('values').text.split(delimiter):
+        if len(x.strip()) > 0:
+            try:
+                key, value = x.split(codedelimiter)
+            except:
+                tmp = x.split()
+                key = tmp[0]
+                value = ' '.join(tmp[1:])
+
+            kv[key.strip()] = value.strip()
+
+    kwargs['type'] = "string"
+    #kwargs['choices'] = [kv[k] for k in kv.keys()]
+    return kwargs
+
+
+def __selection_input(child):
+    kwargs = {}
+    if child.find('delimiter') is not None:
+        delimiter = child.find('delimiter').text
+    else:
+        delimiter = ';'
+
+    kv = {}
+    for i, x in enumerate(child.find('values').text.split(delimiter)):
+        if len(x.strip()) > 0:
+            kv[str(i)] = x.strip()
+
+    kwargs['type'] = str
+    #kwargs['choices'] = [kv[k] for k in kv.keys()]
+    return kwargs
+
+
+def handle_parameter(child):
+    kwargs = {'name': child.attrib['name']}
     if child.attrib['type'] in INPUT_TYPE_MAPPING.keys():
-        kwargs['type'] = 'input-file'
-        if child.find('knowntype') is not None:
-            kwargs['file_formats'] = [child.find('knowntype').text]
-
-        kwargs.update(INPUT_TYPE_MAPPING[child.attrib['type']])
+        kwargs.update(__input_file(child))
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] in ('integer', 'float'):
-        num_min = None
-        num_max = None
-        if child.find('minimum') is not None:
-            try:
-                num_min = float(child.find('minimum').text)
-            except Exception:
-                pass
-        if child.find('maximum') is not None:
-            try:
-                num_max = float(child.find('maximum').text)
-            except Exception:
-                pass
-
-        try:
-            num_default = float(child.find('default').text)
-        except Exception:
-            num_default = 0
-
-        if child.attrib['type'] == 'integer':
-            if num_min is not None:
-                num_min = int(num_min)
-            if num_max is not None:
-                num_max = int(num_max)
-            num_default = int(num_default)
-
-        kwargs['num_range'] = (num_min, num_max)
-        kwargs['default'] = num_default
-        kwargs['type'] = int if child.attrib['type'] == 'integer' else float
+        kwargs.update(__numeric_input(child))
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] in ('boolean', 'toggle'):
-        kwargs['type'] = str
-        kwargs['choices'] = ['true', 'false']
+        kwargs.update(__boolean_input(child))
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] == 'list':
-        if child.find('delimiter') is not None:
-            delimiter = child.find('delimiter').text
-        else:
-            delimiter = ';'
-
-        if child.find('codedelimiter') is not None:
-            codedelimiter = child.find('codedelimiter').text
-        else:
-            codedelimiter = ':'
-
-        kv = {}
-        for x in child.find('values').text.split(delimiter):
-            if len(x.strip()) > 0:
-                try:
-                    key, value = x.split(codedelimiter)
-                except:
-                    tmp = x.split()
-                    key = tmp[0]
-                    value = ' '.join(tmp[1:])
-
-                kv[key.strip()] = value.strip()
-
-        kwargs['type'] = str
-        kwargs['choices'] = [kv[k] for k in kv.keys()]
+        kwargs.update(__list_input(child))
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] == 'selection':
-        if child.find('delimiter') is not None:
-            delimiter = child.find('delimiter').text
-        else:
-            delimiter = ';'
-
-        kv = {}
-        for i, x in enumerate(child.find('values').text.split(delimiter)):
-            if len(x.strip()) > 0:
-                kv[str(i)] = x.strip()
-
-        kwargs['type'] = str
-        kwargs['choices'] = [kv[k] for k in kv.keys()]
+        kwargs.update(__selection_input(child))
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] == 'range':
         # TODO
         kwargs['type'] = 'string'
+        parameter = ET.Element('parameter', **kwargs)
     elif child.attrib['type'] == 'array':
         # TODO
         kwargs['type'] = 'string'
 
     if child.find('information') is not None:
-        kwargs['description'] = child.find('information').text
+        kwargs['label'] = unbreak_strings(child.find('information').text)
+        parameter = ET.Element('parameter', **kwargs)
 
-    log.debug(pprint.pformat(kwargs))
-    return kwargs
+    return parameter, None
 
 
 def build_section(acd_xml_section):
     log.debug('build_section %s', acd_xml_section.tag)
     if acd_xml_section.tag == 'parameter':
-        kwargs, command_string = handle_parameter(acd_xml_section)
-        log.debug('Param %s' % pprint.pformat(kwargs))
+        parameter, command_string = handle_parameter(acd_xml_section)
         command_string = "## $%s\n" % acd_xml_section.attrib['name']
-        parameter = ET.Element('parameter')
         return command_string, parameter
     elif acd_xml_section.tag == 'section':
         # Get section information
@@ -217,12 +290,14 @@ for section in root.findall('section'):
 
                 output_parameter = ET.Element('data', **kwargs)
                 outputs.append(output_parameter)
+                command.text += '-%s $%s' % (parameter.attrib['name'], parameter.attrib['name'])
 
 tool.append(command)
 tool.append(inputs)
 tool.append(outputs)
 tool_help = ET.Element("help")
-tool_help.text = "TODO\n\n@ATTRIBUTION@\n  "
+with open(sys.argv[2], 'r') as handle:
+    tool_help.text = ET.CDATA("\n" + clean_string(handle.read()))
 citations = ET.Element("expand", macros="citation")
 
 tool.append(tool_help)
