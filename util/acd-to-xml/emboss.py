07248ac387908408f2da7dc14c4d12738b0620bc
Eric Rasche <esr@tamu.edu>
2015-06-08 18:11:31 -0500

    Inputs are mostly working, lots of progress left to do

--- util/acd-to-xml/emboss.py
+++ util/acd-to-xml/emboss.py
@@ -121,35 +121,26 @@ class EPL(object):
 
         if param_table is None:
             raise Exception("Couldn't parse out html command table")
-        log.info(pprint.pformat(self._reassociate_associated_quals(param_table)))
 
-
-        for section in self.SECTION_ORDERING:
-            pass
+        # Associates the "Associated (unprompted) qualifiers" with the
+        # appropriate parameter.
+        #
+        # Sticking in self because we write HORRIBLY unclean code.
+        self.reassociated = self._reassociate_associated_quals(param_table)
 
         for section in root.findall('section'):
-            log.info(section.attrib)
-            # Top level attributes
-            if section.attrib['id'] in self.ACD_INPUTS:
-                command_string, input_additions = self.build_section(section)
-                command.text += command_string + "\n"
-                inputs.append(input_additions)
-            else:
-                for parameter in section.findall('parameter'):
-                    if parameter.attrib['type'] in ('toggle', ):
-                        log.warn("Unhandled output toggle")
-                    else:
-                        kwargs = {
-                            'name': parameter.attrib['name']
-                        }
-                        if parameter.find('aformat') is not None:
-                            kwargs['format'] = parameter.find('aformat').text
-                        elif parameter.find('extension') is not None:
-                            kwargs['format'] = parameter.find('extension').text
+            log.info('SECTION %s', pprint.pformat(section.attrib))
 
-                        output_parameter = ET.Element('data', **kwargs)
-                        outputs.append(output_parameter)
-                        command.text += '-%s $%s\n' % (parameter.attrib['name'], parameter.attrib['name'])
+            # Get commands, inputs and outputs
+            command_string, input_section, output_additions = self.build_section(section)
+            # Command is just a string
+            command.text += command_string
+            # Inputs is wrapped up in a <section />
+            if input_section is not None and not isinstance(input_section, list):
+                inputs.append(input_section)
+            # Outputs is a list however
+            for output_addition in output_additions:
+                outputs.append(output_addition)
 
         tool_help = ET.Element("help")
         testnode = ET.Element('tests')
@@ -167,32 +158,6 @@ class EPL(object):
 
         print ET.tostring(tool)
 
-    def _old_section_parsing(self):
-        pass
-        #for section in root.findall('section'):
-            #log.info(section.attrib)
-            ## Top level attributes
-            #if section.attrib['id'] in self.ACD_INPUTS:
-                #command_string, input_additions = self.build_section(section)
-                #command.text += command_string + "\n"
-                #inputs.append(input_additions)
-            #else:
-                #for parameter in section.findall('parameter'):
-                    #if parameter.attrib['type'] in ('toggle', ):
-                        #log.warn("Unhandled output toggle")
-                    #else:
-                        #kwargs = {
-                            #'name': parameter.attrib['name']
-                        #}
-                        #if parameter.find('aformat') is not None:
-                            #kwargs['format'] = parameter.find('aformat').text
-                        #elif parameter.find('extension') is not None:
-                            #kwargs['format'] = parameter.find('extension').text
-
-                        #output_parameter = ET.Element('data', **kwargs)
-                        #outputs.append(output_parameter)
-                        #command.text += '-%s $%s\n' % (parameter.attrib['name'], parameter.attrib['name'])
-
     def parse_cli(self, command_line):
         # Ignore the first two parts, as they're useless (% and appname)
         parts = shlex.split(command_line)[2:]
@@ -203,8 +168,6 @@ class EPL(object):
         for i in range(len(parts)):
             if parts[i].startswith('-'):
                 # Either a flag or a kwarg
-                log.debug('%s -> %s', i, parts[i])
-
                 if i < len(parts) - 1:
                     # If we still have an item after this, treat naively
                     if parts[i + 1].startswith('-'):
@@ -246,7 +209,6 @@ class EPL(object):
         for section in cls.SECTION_ORDERING:
             for param in clitable[section]:
                 param_key = param['Qualifier']
-                log.debug(param_key)
                 if '[-' in param_key:
                     param_key = param_key[1:param_key.index(']')]
                 elif '-[' in param_key:
@@ -254,6 +216,11 @@ class EPL(object):
                         param_key = param_key[0:param_key.index(' ')]
                 else:
                     log.warn(param_key)
+
+                if '[no]' in param_key:
+                    param_key = param_key.replace('[no]', '')
+                    # I have no idea what to do with this :(
+                    param['boolean'] = '[no]'
 
                 keyed_params[param_key] = param
                 keyed_params[param_key]['section'] = section
@@ -264,7 +231,7 @@ class EPL(object):
 
             for extra in clitable['Associated qualifiers'][key]:
                 keyed_params[real_param]['extra'].append(extra)
-        log.debug(pprint.pformat(keyed_params))
+        return keyed_params
 
     @classmethod
     def parse_cli_table(cls, soup):
@@ -333,8 +300,6 @@ class EPL(object):
             for line in code.text.strip().split('\n'):
                 if line.startswith('%'):
                     args, kwargs = self.parse_cli(line)
-                    log.debug(pprint.pformat(args))
-                    log.debug(pprint.pformat(kwargs))
         pass
 
     def __parse_test_inputs(self, html):
@@ -378,10 +343,11 @@ class EPL(object):
     def clean_string(cls, data):
         return ''.join(c for c in data if EPL.valid_xml_char_ordinal(c)).decode('utf8')
 
-    def __default_kwargs(self, child):
+    def __default_kwargs(self, child, assoc=None):
         kwargs = {
             'name': child.attrib['name'],
             'label': child.attrib['name'],
+            'help': '',
         }
 
         if child.find('information') is not None:
@@ -397,29 +363,39 @@ class EPL(object):
                 kwargs['selected'] = 'True'
         elif child.attrib['type'] in ('string',):
             kwargs['type'] = 'text'
+
+        if 'Description' in assoc:
+            if assoc['Description'] != kwargs['label']:
+                kwargs['help'] = assoc['Description']
+
+        if child.find('help') is not None:
+            kwargs['help'] = self.unbreak_strings(child.find('help').text)
+
+        # Add the (-argname)
+        help_arg = ' (-%s)' % child.attrib['name']
+        if len(kwargs['help']) == 0:
+            help_arg = help_arg.strip()
+        kwargs['help'] += help_arg
         #elif child.attrib['type'] == 'list':
         #elif child.attrib['type'] == 'selection':
         #elif child.attrib['type'] == 'range':
         return kwargs
 
-    def __file_input(self, child):
+    def __file_input(self, child, assoc=None):
         #<param label="Genome" name="positional_1" type="data" format="fasta"/>
-        kwargs = self.__default_kwargs(child)
+        kwargs = self.__default_kwargs(child, assoc=assoc)
 
         if child.find('knowntype') is not None:
             kwargs['file_format'] = self.KNOWNTYPE_MAPPING.get(child.find('knowntype').text, 'UNKNOWN')
 
-        if child.find('help') is not None:
-            kwargs['help'] = self.unbreak_strings(child.find('help').text)
         # Unhandled: additional, default, nullok, relations, ... ?
 
         kwargs.update(self.INPUT_TYPE_MAPPING[child.attrib['type']])
-        parameter = ET.Element('parameter', **kwargs)
+        parameter = ET.Element('param', **kwargs)
         cmd = "-{0} ${0}".format(child.attrib['name'])
         return parameter, cmd
 
-    @classmethod
-    def __numeric_input(self, child):
+    def __numeric_input(self, child, assoc=None):
         num_min = None
         num_max = None
         if child.find('minimum') is not None:
@@ -445,7 +421,7 @@ class EPL(object):
                 num_max = int(num_max)
             num_default = int(num_default)
 
-        kwargs = self.__default_kwargs(child)
+        kwargs = self.__default_kwargs(child, assoc=assoc)
         kwargs.update({
             'default': str(num_default),
         })
@@ -456,28 +432,33 @@ class EPL(object):
         if num_max is not None:
             kwargs['maximum'] = str(num_max)
 
-        parameter = ET.Element('parameter', **kwargs)
+        parameter = ET.Element('param', **kwargs)
         cmd = "-{0} ${0}".format(child.attrib['name'])
         return parameter, cmd
 
-    def __text_input(self, child):
-        kwargs = self.__default_kwargs(child)
-        parameter = ET.Element('parameter', **kwargs)
+    def __text_input(self, child, assoc=None):
+        kwargs = self.__default_kwargs(child, assoc=assoc)
+        parameter = ET.Element('param', **kwargs)
         cmd = '-{0} "${0}"'.format(child.attrib['name'])
         return parameter, cmd
 
-    def __boolean_input(self, child):
-        kwargs = self.__default_kwargs(child)
+    def __boolean_input(self, child, assoc=None):
+        kwargs = self.__default_kwargs(child, assoc=assoc)
         kwargs.update({
             'truevalue': '-%s' % child.attrib['name'],
             'falsevalue': '',
         })
-        parameter = ET.Element('parameter', **kwargs)
+
+        if 'boolean' in assoc and assoc['boolean'] == '[no]':
+            kwargs['truevalue'] = '-%s' % child.attrib['name']
+            kwargs['falsevalue'] = '-no%s' % child.attrib['name']
+
+        parameter = ET.Element('param', **kwargs)
         cmd = "${0}".format(child.attrib['name'])
         return parameter, cmd
 
-    def __list_input(self, child):
-        kwargs = {}
+    def __list_input(self, child, assoc=None):
+        kwargs = self.__default_kwargs(child, assoc=assoc)
         if child.find('delimiter') is not None:
             delimiter = child.find('delimiter').text
         else:
@@ -502,7 +483,7 @@ class EPL(object):
 
         kwargs['type'] = "select"
         #kwargs['choices'] = [kv[k] for k in kv.keys()]
-        parameter = ET.Element('parameter', **kwargs)
+        parameter = ET.Element('param', **kwargs)
         for key in kv:
             tmpkw = {'value': key}
 
@@ -515,9 +496,8 @@ class EPL(object):
         cmd = '-{0} "${0}"'.format(child.attrib['name'])
         return parameter, cmd
 
-    @classmethod
-    def __selection_input(cls, child):
-        kwargs = {'name': child.attrib['name']}
+    def __selection_input(self, child, assoc=None):
+        kwargs = self.__default_kwargs(child, assoc=assoc)
         if child.find('delimiter') is not None:
             delimiter = child.find('delimiter').text
         else:
@@ -530,54 +510,135 @@ class EPL(object):
 
         kwargs['type'] = str
         #kwargs['choices'] = [kv[k] for k in kv.keys()]
-        parameter = ET.Element('parameter', **kwargs)
+        parameter = ET.Element('param', **kwargs)
         return parameter, None
 
-    def handle_parameter(self, child):
+    def handle_parameter(self, child, assoc=None):
         if child.attrib['type'] in self.INPUT_TYPE_MAPPING.keys():
-            return self.__file_input(child)
+            return self.__file_input(child, assoc=assoc)
         elif child.attrib['type'] in ('integer', 'float'):
-            return self.__numeric_input(child)
+            return self.__numeric_input(child, assoc=assoc)
         elif child.attrib['type'] in ('string'):
-            return self.__text_input(child)
+            return self.__text_input(child, assoc=assoc)
         elif child.attrib['type'] in ('boolean', 'toggle'):
-            return self.__boolean_input(child)
+            return self.__boolean_input(child, assoc=assoc)
         elif child.attrib['type'] == 'list':
-            return self.__list_input(child)
+            return self.__list_input(child, assoc=assoc)
         elif child.attrib['type'] == 'selection':
-            return self.__selection_input(child)
+            return self.__selection_input(child, assoc=assoc)
         else:
             return ET.Comment("Unhanlded %s" % child.attrib['type']), None
 
+    def _generate_extra_inputs(self, extra, parent_name=None):
+        """
+        extra is something like::
+
+            {
+                'Allowed values': 'Any string',
+                'Default': '',
+                'Description': 'Output seq format',
+                'Qualifier': '-osformat2-osformat_outseq',
+                'Type': 'string'
+            }
+
+        ``parent_name`` is used to pass the command line name of the parent
+        output file
+        """
+        command = ''
+        quals = extra['Qualifier'].split('-')[1:]
+        if 'format' in quals[0]:
+            # TODO: support more than just fasta
+            command = '-%s "fasta"' % quals[0]
+        elif 'extension' in quals[0]:
+            command = '-%s "dat"' % quals[0]
+        elif 'directory' in quals[0]:
+            command = '-%s $(dirname $%s)' % (quals[0], parent_name)
+        elif 'dbname' in quals[0]:
+            pass
+        elif 'name' in quals[0]:
+            # This mega sucks. Sucks SO much.
+            command = '-%s $(basename $%s dat)' % (quals[0], parent_name)
+        elif 'single' in quals[0]:
+            pass
+        elif 'ufo' in quals[0]:
+            pass
+        else:
+            log.info('Unhandled extra %s', quals[0])
+        return command
+
+    def _generate_outputs(self, section):
+        assoc = self.reassociated.get('-' + section.attrib['name'], {})
+
+        return ("\n## %s\n" % section.attrib['name'], [])
+
+    def _generate_inputs(self, section):
+        inputs = []
+
+        assoc = self.reassociated.get('-' + section.attrib['name'], {})
+        input_param, input_cmd = self.handle_parameter(section, assoc=assoc)
+        inputs.append(input_param)
+        if input_cmd is None:
+            input_cmd = ''
+
+        for extra in assoc.get('extra', []):
+            id = section.attrib['name']
+            extra_cmd = self._generate_extra_inputs(extra, parent_name=id)
+            input_cmd += extra_cmd + "\n"
+
+        return (input_cmd, inputs)
+
     def build_section(self, acd_xml_section):
         if acd_xml_section.tag == 'parameter':
-            parameter, command_string = self.handle_parameter(acd_xml_section)
-            if command_string is None:
-                command_string = "## $%s\n" % acd_xml_section.attrib['name']
-            return command_string + "\n", parameter
+            command_string = ''
+            inputs = []
+            outputs = []
+
+            commands, input_additions = self._generate_inputs(acd_xml_section)
+            command_string += ''.join(commands)
+            for ia in input_additions:
+                inputs.append(ia)
+
+            commands, output_additions = self._generate_outputs(acd_xml_section)
+            command_string += ''.join(commands)
+            for oa in output_additions:
+                outputs.append(oa)
+
+            return command_string, inputs, outputs
         elif acd_xml_section.tag == 'section':
             # Get section information
             metadata = acd_xml_section.find('metadata')
+            section_title = "None"
             if metadata is not None:
                 section_title = metadata.find('information').text
-            else:
-                section_title = "None"  # acd_xml_section.attrib['id']
 
-            section = ET.Element('section', name=acd_xml_section.attrib['id'],
-                                 title=section_title)
+            # Complete command for entire section
             section_cmd = ""
+            # Some inputs
+            inputs = ET.Element('section', name=acd_xml_section.attrib['id'],
+                                title=section_title)
+            # Some output datasets, maybe
+            outputs = []
+
+            # For all the kids
             for child in acd_xml_section.getchildren():
+                # Skip metadata, we only want parameters and sections
                 if child.tag == 'metadata':
                     continue
-                else:
-                    command_string, parameter = self.build_section(child)
-                    if parameter is not None:
-                        section_cmd += command_string
-                        section.append(parameter)
-            return section_cmd, section
+
+                command_string, input_extra, output = self.build_section(child)
+
+                # Append command string
+                section_cmd += command_string
+                # Inputs are semi-special compared to outputs
+                for i in input_extra:
+                    inputs.append(i)
+                # Outputs are just all lumped together
+                outputs.extend(output)
+            # Return all of them
+            return section_cmd, inputs, outputs
         else:
-            return None, None
             log.warn("Unhanlded section %s", acd_xml_section.tag)
+            return None, None, None
 
 
 if __name__ == '__main__':
