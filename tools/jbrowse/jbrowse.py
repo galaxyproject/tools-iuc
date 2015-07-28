ee5760c27104ea12297feafdbb6be9e42845373e
Eric Rasche <rasche.eric@gmail.com>
2015-07-28 18:25:50 -0500

    Updates to JBrowse for more colour configuration
    - extensive updates to enable brewer palettes/custom colour config for
      bigwig tracks
    - various bug fixes and re-architecting
    - may re-work brewer palettes eventually--right now we just use the
      extrma. I don't know if JBrowse supports gradiations on the BigWig
      tracks, other than in "Density" mode

--- tools/jbrowse/jbrowse.py
+++ tools/jbrowse/jbrowse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import copy
 import argparse
 import subprocess
 import hashlib
@@ -47,6 +48,18 @@ BREWER_COLOUR_SCHEMES = [
     # (152, 78, 163),
     # (255, 127, 0),
 ]
+
+BREWER_DIVERGING_PALLETES = {
+    'BrBg': ("#543005", "#003c30"),
+    'PiYg': ("#8e0152", "#276419"),
+    'PRGn': ("#40004b", "#00441b"),
+    'PuOr': ("#7f3b08", "#2d004b"),
+    'RdBu': ("#67001f", "#053061"),
+    'RdGy': ("#67001f", "#1a1a1a"),
+    'RdYlBu': ("#a50026", "#313695"),
+    'RdYlGn': ("#a50026", "#006837"),
+    'Spectral': ("#9e0142", "#5e4fa2"),
+}
 
 
 # score comes from feature._parent.get('score') or feature.get('score')
@@ -99,7 +112,7 @@ class JbrowseConnector(object):
         self.subprocess_check_call(cmd)
         os.unlink(tmp.name)
 
-    def add_blastxml(self, data, key, **kwargs):
+    def add_blastxml(self, data, trackData, **kwargs):
         gff3_unrebased = tempfile.NamedTemporaryFile(delete=False)
         cmd = ['python', os.path.join(INSTALLED_TO, 'blastxml_to_gapped_gff3.py'),
                '--trim_end', '--min_gap', str(kwargs['min_gap']), data]
@@ -114,8 +127,6 @@ class JbrowseConnector(object):
         subprocess.check_call(cmd, cwd=self.jbrowse_dir, stdout=gff3_rebased)
         gff3_rebased.close()
 
-        label = hashlib.md5(data).hexdigest()
-
         red, green, blue = self._get_colours()
         color_function = COLOR_FUNCTION_TEMPLATE.format({
             'score': "feature._parent.get('score')",
@@ -125,19 +136,16 @@ class JbrowseConnector(object):
             'blue': blue,
         })
 
-        clientConfig = {
-            'label': kwargs['options']['style']['label'],
-            'color': color_function.replace('\n', ''),
-            'description': kwargs['options']['style']['description'],
-        }
+        clientConfig = trackData['style']
+        clientConfig['color'] = color_function.replace('\n', '')
         config = {'glyph': 'JBrowse/View/FeatureGlyph/Segments'}
         if 'category' in kwargs:
             config['category'] = kwargs['category']
 
         cmd = ['perl', 'bin/flatfile-to-json.pl',
                '--gff', gff3_rebased.name,
-               '--trackLabel', label,
-               '--key', key,
+               '--trackLabel', trackData['label'],
+               '--key', trackData['key'],
                '--clientConfig', json.dumps(clientConfig),
                '--config', json.dumps(config),
                '--trackType', 'JBrowse/View/Track/CanvasFeatures'
@@ -166,32 +174,60 @@ class JbrowseConnector(object):
                     pass
         return min_val, max_val
 
-    def add_bigwig(self, data, key, **kwargs):
-        label = hashlib.md5(data).hexdigest()
+    def _parse_colours(self, track):
+        # Wiggle tracks have a bicolor pallete
+        clientConfig = {}
+        if track['format'] == 'wiggle':
+            if track['options']['style']['color_config'] == 'brewer':
+                scheme = track['options']['style']['color']
+                if scheme not in BREWER_DIVERGING_PALLETES:
+                    raise Exception("Unknown pallete")
+
+                pos_color, neg_color = BREWER_DIVERGING_PALLETES[scheme]
+            else:
+                pos_color = track['options']['style']['color_pos'].replace('__pd__', '#')
+                neg_color = track['options']['style']['color_neg'].replace('__pd__', '#')
+
+            clientConfig['pos_color'] = pos_color
+            clientConfig['neg_color'] = neg_color
+        else:
+            # Other tracks either use "__auto__" or specify a colour
+            if track['options']['style']['color'] == '__auto__':
+                # Automatically generate the next brewer colour
+                red, green, blue = self._get_colours()
+                clientConfig['color'] = 'rgba({red}, {green}, {blue}, 1)' \
+                    .format(red=red, green=green, blue=blue)
+            else:
+                clientConfig['color'] = track['options']['style']['color'].replace('__pd__', '#')
+        return clientConfig
+
+    def add_bigwig(self, data, trackData, **kwargs):
         dest = os.path.join('data', 'raw', os.path.basename(data))
         cmd = ['ln', '-s', data, dest]
         self.subprocess_check_call(cmd)
 
-        track_data = {
-            "label": label,
+        trackData.update({
             "urlTemplate": os.path.join('..', dest),
-            "bicolor_pivot": "zero",
             "storeClass": "JBrowse/Store/SeqFeature/BigWig",
             "type": "JBrowse/View/Track/Wiggle/Density",
-            "key": key,
-        }
-        track_data.update(kwargs)
+        })
 
-        if 'min' not in track_data and 'max' not in track_data \
-                and 'autoscale' not in track_data:
-            track_data['autoscale'] = 'local'
+        if 'bicolor_pivot' not in trackData:
+            trackData['bicolor_pivot'] = kwargs['style'].get('bicolor_pivot', 'zero')
 
-        self._add_json(track_data)
+        if 'type' in kwargs:
+            trackData['type'] = kwargs['type']
 
-    def add_bam(self, data, key, **kwargs):
-        label = hashlib.md5(data).hexdigest()
+        if 'min' in kwargs and 'max' in kwargs:
+            trackData['min'] = kwargs['min']
+            trackData['max'] = kwargs['max']
+        else:
+            trackData['autoscale'] = kwargs.get('autoscale', 'local')
+
+        self._add_json(trackData)
+
+    def add_bam(self, data, trackData, **kwargs):
         dest = os.path.join('data', 'raw', os.path.basename(data))
-        # ln?
         cmd = ['ln', '-s', data, dest]
         self.subprocess_check_call(cmd)
 
@@ -199,31 +235,28 @@ class JbrowseConnector(object):
         cmd = ['ln', '-s', bai_source, dest + '.bai']
         self.subprocess_check_call(cmd)
 
-        track_data = {
+        trackData.update({
             "urlTemplate": os.path.join('..', dest),
-            "key": key,
-            "label": label,
             "type": "JBrowse/View/Track/Alignments2",
             "storeClass": "JBrowse/Store/SeqFeature/BAM",
-        }
+        })
+
         if 'category' in kwargs:
-            track_data['category'] = kwargs['category']
-        self._add_json(track_data)
+            trackData['category'] = kwargs['category']
+
+        self._add_json(trackData)
 
         if kwargs.get('auto_snp', False):
-            track_data = {
-                "storeClass": "JBrowse/Store/SeqFeature/BAM",
-                "urlTemplate": os.path.join('..', dest),
+            trackData2 = copy.copy(trackData)
+            trackData2.update({
                 "type": "JBrowse/View/Track/SNPCoverage",
-                "key": key + " - SNPs/Coverage",
-                "label": label + "_autosnp",
-            }
-            if 'category' in kwargs:
-                track_data['category'] = kwargs['category']
-            self._add_json(track_data)
+                "key": trackData['key'] + " - SNPs/Coverage",
+                "label": trackData['label']  + "_autosnp",
+            })
 
-    def add_vcf(self, data, key, **kwargs):
-        label = hashlib.md5(data).hexdigest()
+            self._add_json(trackData)
+
+    def add_vcf(self, data, clientConfig, **kwargs):
         dest = os.path.join('data', 'raw', os.path.basename(data))
         # ln?
         cmd = ['ln', '-s', data, dest]
@@ -233,33 +266,28 @@ class JbrowseConnector(object):
         cmd = ['tabix', '-p', 'vcf', dest + '.gz']
         self.subprocess_check_call(cmd)
 
-        track_data = {
-            "key": key,
-            "label": label,
+        trackData.update({
             "urlTemplate": os.path.join('..', dest + '.gz'),
             "type": "JBrowse/View/Track/HTMLVariants",
             "storeClass": "JBrowse/Store/SeqFeature/VCFTabix",
-        }
-        track_data.update(kwargs)
-        self._add_json(track_data)
+        })
+        self._add_json(trackData)
 
-    def add_features(self, data, key, format, **kwargs):
-        label = hashlib.md5(data).hexdigest()
-        cmd = ['perl', 'bin/flatfile-to-json.pl',
-               TN_TABLE.get(format, 'gff'), data,
-               '--trackLabel', label,
-               '--key', key]
+    def add_features(self, data, format, trackData, **kwargs):
+        cmd = [
+            'perl', 'bin/flatfile-to-json.pl',
+            TN_TABLE.get(format, 'gff'),
+            data,
+            '--trackLabel', trackData['label'],
+            '--key', trackData['key']
+        ]
 
         config = {}
+        clientConfig = trackData['style']
         if 'category' in kwargs:
             config['category'] = kwargs['category']
 
         if kwargs.get('match', False):
-            clientConfig = {
-                'label': 'description',
-                'description': 'Hit_titles',
-            }
-
             # Get min/max and build a scoring function since JBrowse doesn't
             min_val, max_val = self._min_max_gff(data)
 
@@ -292,30 +320,48 @@ class JbrowseConnector(object):
 
         self.subprocess_check_call(cmd)
 
-    def process_annotations(self, data, key, format, **kwargs):
-        clientConfig = {
-            'label': kwargs['options']['style']['label'],
-            'color': kwargs['options']['style']['color'],
-            'classname': kwargs['options']['style']['classname'],
-            'description': kwargs['options']['style']['description'],
+    def process_annotations(self, data, track):
+        format = track['ext']
+        kwargs = track['options']
+
+        # Base trackData section is pretty universally similar
+        trackData = {
+            "label": hashlib.md5(data).hexdigest(),
+            "key": track['label'],
         }
 
-        if clientConfig['color'] == '__auto__':
-            # Automatically generate the next brewer colour
-            red, green, blue = self._get_colours()
-            clientConfig['color'] = 'rgba(${red}, ${green}, ${blue}, 1)' \
-                .format(red=red, green=green, blue=blue)
+        clientConfig = {
+            'label': kwargs['style'].get('label', 'description'),
+            'classname': kwargs['style'].get('className', 'feature'),
+            'description': kwargs['style'].get('description', ''),
+            'height': kwargs['style'].get('height', '100px')
+        }
+
+        # Colour parsing is complex due to different track types having
+        # different colour options.
+        clientConfig.update(self._parse_colours(track))
+
+        if format == 'bigwig':
+            trackData
+
+        # Load clientConfig into trackData
+        trackData['style'] = clientConfig
+
+        print "TrackData"
+        import pprint; pprint.pprint(trackData)
+        print "kwargs"
+        import pprint; pprint.pprint(kwargs)
 
         if format in ('gff', 'gff3', 'bed'):
-            self.add_features(data, key, format, **kwargs)
+            self.add_features(data, format, trackData, **kwargs)
         elif format == 'bigwig':
-            self.add_bigwig(data, key, **kwargs)
+            self.add_bigwig(data, trackData, **kwargs)
         elif format == 'bam':
-            self.add_bam(data, key, **kwargs)
+            self.add_bam(data, trackData, **kwargs)
         elif format == 'blastxml':
-            self.add_blastxml(data, key, **kwargs)
+            self.add_blastxml(data, trackData, **kwargs)
         elif format == 'vcf':
-            self.add_vcf(data, key, **kwargs)
+            self.add_vcf(data, trackData, **kwargs)
 
     def clone_jbrowse(self, jbrowse_dir, destination):
         # JBrowse seems to have included some bad symlinks, cp ignores bad symlinks
@@ -361,7 +407,11 @@ if __name__ == '__main__':
                     extra[possible_partial_path])
         extra['category'] = track.get('category', 'Default')
 
-        jc.process_annotations(path, track['label'], track['ext'], **extra)
+        # Push modified options back into the track config
+        track['options'] = extra
+        # More data was needed in process_annotations so it becomes a little
+        # bit less clean.
+        jc.process_annotations(path, track)
 
     print """
     <html>
