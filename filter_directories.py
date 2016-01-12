0f2a00d9be280beebea22ab8250d5a68c7b45f08
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-13 00:54:01 +0100

    add initial conda testing

---
+++ filter_directories.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+for directory in sys.stdin:
+    while directory:
+        if os.path.exists( os.path.join(directory, '.shed.yml') ) or os.path.exists( os.path.join(directory, '.shed.yaml') ):
+            print(directory)
+            break
+        else:
+            directory = os.path.dirname( directory )
