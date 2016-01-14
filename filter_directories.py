b5664aa74977a48f270e634afdaa24aba49ee31f
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-14 16:32:13 +0100

    khmer should be removed from the blacklist if we want to test it

--- filter_directories.py
+++ filter_directories.py
@@ -3,11 +3,16 @@
 import os
 import sys
 
+bl = [tool.strip() for tool in open('.tt_blacklist', 'r')]
+
 for directory in sys.stdin:
     directory = directory.strip()
+    if directory.startswith('packages') or directory.startswith('data_managers'):
+        continue
     while directory:
         if os.path.exists( os.path.join(directory, '.shed.yml') ) or os.path.exists( os.path.join(directory, '.shed.yaml') ):
-            print(directory)
+            if directory not in bl:
+                print(directory)
             break
         else:
             directory = os.path.dirname( directory )
