6e360761398c906b0b531cde8b9b370c9df65ea2
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-14 16:11:29 +0100

    remove line endings

--- filter_directories.py
+++ filter_directories.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 for directory in sys.stdin:
+    directory = directory.strip()
     while directory:
         if os.path.exists( os.path.join(directory, '.shed.yml') ) or os.path.exists( os.path.join(directory, '.shed.yaml') ):
             print(directory)
