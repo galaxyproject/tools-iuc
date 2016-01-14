857cde0e1475d0b34767067479c2e1440e1aa927
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-14 20:32:19 +0100

    remove conda numpy build

--- filter_directories.py
+++ filter_directories.py
@@ -7,6 +7,8 @@ bl = [tool.strip() for tool in open('.tt_blacklist', 'r')]
 
 for directory in sys.stdin:
     directory = directory.strip()
+    if directory.startswith('packages') or directory.startswith('data_managers'):
+        continue
     while directory:
         if os.path.exists( os.path.join(directory, '.shed.yml') ) or os.path.exists( os.path.join(directory, '.shed.yaml') ):
             if directory not in bl:
