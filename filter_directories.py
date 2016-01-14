540c7653f5e9553bc0c41e955849d68da05e6ec9
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-01-14 17:01:52 +0100

    Move more logic into the python script

--- filter_directories.py
+++ filter_directories.py
@@ -3,11 +3,14 @@
 import os
 import sys
 
+bl = [tool.strip() for tool in open('.tt_blacklist', 'r')]
+
 for directory in sys.stdin:
     directory = directory.strip()
     while directory:
         if os.path.exists( os.path.join(directory, '.shed.yml') ) or os.path.exists( os.path.join(directory, '.shed.yaml') ):
-            print(directory)
+            if directory not in bl:
+                print(directory)
             break
         else:
             directory = os.path.dirname( directory )
