596c50c790c1a12d7d10b521783b4b4eec76d8f7
Nicola Soranzo <nsoranzo@tiscali.it>
2016-05-10 10:04:09 +0100

    flake8 Python files in datatypes/ and data_managers/ (#753)
    First step for #467.

--- data_managers/data_manager_gemini_database_downloader/data_manager/data_manager_gemini_download.py
+++ data_managers/data_manager_gemini_database_downloader/data_manager/data_manager_gemini_download.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import json
-import shlex
 import datetime
+import json
+import os
 import subprocess
+import sys
+
 
 def main():
-
     today = datetime.date.today()
     params = json.loads( open( sys.argv[1] ).read() )
     target_directory = params[ 'output_data' ][0]['extra_files_path']
     os.mkdir( target_directory )
     cmd = "gemini --annotation-dir %s update --dataonly %s %s" % (target_directory, params['param_dict']['gerp_bp'], params['param_dict']['cadd'] )
-    ret = subprocess.check_call( cmd, shell=True )
+    subprocess.check_call( cmd, shell=True )
     data_manager_dict = {
-                'data_tables':
-                    {'gemini_databases': [
-                            {'value': today.isoformat(), 'dbkey': 'hg19', 'name': 'GEMINI annotations (%s)' % today.isoformat(), 'path': './%s' % today.isoformat() }
-                                        ]
-                    }
-                }
+        'data_tables': {
+            'gemini_databases': [
+                {'value': today.isoformat(), 'dbkey': 'hg19', 'name': 'GEMINI annotations (%s)' % today.isoformat(), 'path': './%s' % today.isoformat() }
+            ]
+        }
+    }
 
-    #save info to json file
+    # save info to json file
     with open( sys.argv[1], 'wb' ) as out:
         out.write( json.dumps( data_manager_dict ) )
 
 if __name__ == "__main__":
     main()
-
