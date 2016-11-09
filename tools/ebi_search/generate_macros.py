b3caafbe3b85a65a981e49711f9c73cc04f05dc8
Bérénice Batut <bebatut@gmail.com>
2016-11-09 07:51:59 +0100

    Add fields extraction

--- tools/ebi_search/generate_macros.py
+++ tools/ebi_search/generate_macros.py
@@ -33,19 +33,62 @@ def extract_domains():
     return domains
 
 
+def extract_fiel_info(field_info):
+    searchable_fields = []
+    retrievable_fields = []
+
+    for line in field_info:
+        if line == "\n" :
+            continue
+
+        if line.find("\t") == -1:
+            continue
+
+        if line.startswith("field_id") or line.startswith("$facets"):
+            continue
+
+        split_line = line.split("\t")
+        field_id = split_line[0]
+        searchable = split_line[1]
+        retrievable = split_line[2]
+
+        if searchable == "true":
+            searchable_fields.append(field_id)
+
+        if retrievable == "true":
+            retrievable_fields.append(field_id)
+
+    return searchable_fields, retrievable_fields
+
+
+def extract_a_domain_fields(domain):
+    domain_id = domain.split(":")[0]
+    domain_name = domain.split(":")[1]
+
+    searchable_fields = []
+    retrievable_fields = []
+
+    field_info = WritableObject()
+    sys.stdout = field_info
+    ebeye_urllib3.getDomainDetails(domain_id)
+    sys.stdout = sys.__stdout__
+
+    searchable_fields, retrievable_fields = extract_fiel_info(field_info.content)
+
+    domain_info = {}
+    domain_info["name"] = domain_name
+    domain_info["searchable_fields"] = searchable_fields
+    domain_info["retrievable_fields"] = retrievable_fields
+
+    return domain_id, domain_info
+
+
 def extract_fields(domains):
     domains_fields = {}
 
     for domain in domains:
-        domain_id = domain.split(":")[0]
-        domain_name = domain.split(":")[1]
-
-        domains_fields.setdefault(domain_name, {})
-        domains_fields[domain_name].setdefault("name", domain_name)
-        domains_fields[domain_name].setdefault("searchable_fields", [])
-        domains_fields[domain_name].setdefault("retrievable_fields", [])
-
-
+        domain_id, domain_info = extract_a_domain_fields(domain)
+        domains_fields.setdefault(domain_id, domain_info)
 
     return domains_fields
 
