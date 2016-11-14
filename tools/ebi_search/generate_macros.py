32d28d961b046ad91743863a5d508265a3d8092d
Bérénice Batut <bebatut@gmail.com>
2016-11-14 12:20:42 +0100

    Simplify domain and field extraction

--- tools/ebi_search/generate_macros.py
+++ tools/ebi_search/generate_macros.py
@@ -5,91 +5,6 @@ import ebeye_urllib3
 import writableObject
 
 
-def extract_domains():
-    # redirection of sys.stdout
-    domain_hierarchy = writableObject.WritableObject()
-    sys.stdout = domain_hierarchy
-    ebeye_urllib3.getDomainHierarchy()
-    sys.stdout = sys.__stdout__
-
-    domains = []
-    last_level = 1
-    tmp_domains = ""
-    for line in domain_hierarchy.content:
-        if line == "\n":
-            continue
-        domain = line.split("\t")[-1]
-        level = line.count("\t")
-
-        if level <= last_level and tmp_domains != "":
-            domains.append(tmp_domains)
-        tmp_domains = domain
-        last_level = level
-    domains.append(tmp_domains)
-    return domains
-
-
-def extract_fiel_info(field_info):
-    searchable_fields = []
-    retrievable_fields = []
-
-    for line in field_info:
-        if line == "\n":
-            continue
-
-        if line.find("\t") == -1:
-            continue
-
-        if line.startswith("field_id") or line.startswith("$facets"):
-            continue
-
-        split_line = line.split("\t")
-        field_id = split_line[0]
-        searchable = split_line[1]
-        retrievable = split_line[2]
-
-        if searchable == "true":
-            searchable_fields.append(field_id)
-
-        if retrievable == "true":
-            retrievable_fields.append(field_id)
-
-    return searchable_fields, retrievable_fields
-
-
-def extract_a_domain_fields(domain):
-    domain_id = domain.split(":")[0]
-    domain_name = domain.split(":")[1]
-
-    searchable_fields = []
-    retrievable_fields = []
-
-    field_info = writableObject.WritableObject()
-    sys.stdout = field_info
-    ebeye_urllib3.getDomainDetails(domain_id)
-    sys.stdout = sys.__stdout__
-
-    searchable_fields, retrievable_fields = extract_fiel_info(
-        field_info.content)
-
-    domain_info = {}
-    domain_info["name"] = domain_name
-    domain_info["searchable_fields"] = sorted(searchable_fields)
-    domain_info["retrievable_fields"] = sorted(retrievable_fields)
-
-    return domain_id, domain_info
-
-
-def extract_fields(domains):
-    domains_fields = {}
-
-    for domain in domains:
-        domain_id, domain_info = extract_a_domain_fields(domain)
-        domains_fields.setdefault(domain_id, domain_info)
-
-    return domains_fields
-
-
 def add_option(value, name, selected="false"):
     to_write = "<option value=\"" + value + "\" selected=\"" + selected + "\">"
     to_write += name
@@ -192,8 +107,9 @@ def write_macros_file(macros_filepath, domains_fields):
 
 
 def generate_macros():
-    domains = extract_domains()
-    domains_fields = extract_fields(domains)
+    #domains = extract_domains()
+    #domains_fields = extract_fields(domains)
+    domains_fields = ebeye_urllib3.getDomainHierarchy()
     write_macros_file("macros.xml", domains_fields)
 
 
