4c4f03a41f33557fcf9d06566f002858c154ac12
Bérénice Batut <bebatut@gmail.com>
2017-01-14 11:27:00 +0100

    Add test data generation for split_libraries

--- tools/qiime/generate_test_data.sh
+++ tools/qiime/generate_test_data.sh
@@ -31,6 +31,29 @@ cp split_libraries/seqs.qual 'test-data/split_fastq_libraries_sequence_qualities
 cp split_libraries/seqs.fastq 'test-data/split_fastq_libraries_demultiplexed_sequences.fastq'
 rm -rf split_libraries
 
+# split_libraries
+split_libraries.py \
+    --map 'test-data/split_libraries_mapping_file.txt' \
+    -o split_libraries \
+    --fasta 'test-data/split_libraries_reads_1.fna,test-data/split_libraries_reads_2.fna' \
+    --qual 'test-data/split_libraries_reads_1.qual,test-data/split_libraries_reads_2.qual' \
+    --min_qual_score 25 \
+    --qual_score_window 0 \
+    --record_qual_scores \
+    --min_seq_length 200 \
+    --max_seq_length 1000 \
+    --max_ambig 6 \
+    --max_homopolymer 6 \
+    --max_primer_mismatch 0 \
+    --barcode_type 'golay_12' \
+    --max_barcode_errors 1.5 \
+    --start_numbering_at 1
+cp split_libraries/seqs.fna 'test-data/split_libraries_seqs.fna'
+cp split_libraries/split_library_log.txt 'test-data/split_libraries_split_library_log'
+cp split_libraries/histograms.txt 'test-data/split_libraries_histograms.txt'
+cp split_libraries/seqs_filtered.qual 'test-data/split_libraries_seqs_filtered.qual'
+rm -rf split_libraries
+
 # pick_open_reference_otus
 pick_open_reference_otus.py \
     --input_fps 'test-data/split_fastq_libraries_sequences.fasta' \
@@ -83,27 +106,6 @@ cp pick_open_reference_otus_3/final_otu_map_mc*.txt 'test-data/pick_open_referen
 cp pick_open_reference_otus_3/rep_set.tre 'test-data/pick_open_reference_otus_3_rep_set_tree.tre'
 rm -rf pick_open_reference_otus_3
 
-# make_emperor
-make_emperor.py \
-    --input_coords 'test-data/core_diversity_analyses_unweighted_unifrac_pc.txt' \
-    -o make_emperor_1 \
-    --map_fp 'test-data/core_diversity_analyses_map.txt' \
-    --number_of_axes '10' \
-    --add_unique_columns \
-    --number_of_segments 8
-rm -rf make_emperor_1
-
-make_emperor.py \
-    --input_coords 'test-data/core_diversity_analyses_unweighted_unifrac_pc.txt' \
-    -o make_emperor_2 \
-    --map_fp 'test-data/core_diversity_analyses_map.txt' \
-    --number_of_axes '10' \
-    --add_unique_columns \
-    --number_of_segments 8 \
-    --taxa_fp 'test-data/summarize_taxa_2_L3.txt' \
-    --n_taxa_to_keep 10
-rm -rf make_emperor_2
-
 # core_diversity_analyses
 # Data are from test data in https://github.com/biocore/qiime
 core_diversity_analyses.py \
@@ -125,6 +127,27 @@ core_diversity_analyses.py \
     --suppress_alpha_diversity \
     --suppress_group_significance
 rm -rf core_diversity_analyses_2
+
+# make_emperor
+make_emperor.py \
+    --input_coords 'test-data/core_diversity_analyses_unweighted_unifrac_pc.txt' \
+    -o make_emperor_1 \
+    --map_fp 'test-data/core_diversity_analyses_map.txt' \
+    --number_of_axes '10' \
+    --add_unique_columns \
+    --number_of_segments 8
+rm -rf make_emperor_1
+
+make_emperor.py \
+    --input_coords 'test-data/core_diversity_analyses_unweighted_unifrac_pc.txt' \
+    -o make_emperor_2 \
+    --map_fp 'test-data/core_diversity_analyses_map.txt' \
+    --number_of_axes '10' \
+    --add_unique_columns \
+    --number_of_segments 8 \
+    --taxa_fp 'test-data/summarize_taxa_2_L3.txt' \
+    --n_taxa_to_keep 10
+rm -rf make_emperor_2
 
 # summarize_taxa
 summarize_taxa.py \
