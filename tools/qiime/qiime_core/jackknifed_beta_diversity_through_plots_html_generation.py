7a4998ba76195258aa471e6e9089acd239c8b66e
Bérénice Batut <bebatut@gmail.com>
2017-01-20 18:57:10 +0100

    Fix flake8 errors

--- tools/qiime/qiime_core/jackknifed_beta_diversity_through_plots_html_generation.py
+++ tools/qiime/qiime_core/jackknifed_beta_diversity_through_plots_html_generation.py
@@ -1,85 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import os
 import argparse
-import re
 
 
 def generate_index_html(dir_list, args):
-    with open('index.html','w') as index_html_file:
-        index_html_file.write('<html>\n')
-        index_html_file.write('\t<head><title>Jackknifed beta diversity results</title></head>\n')
-        index_html_file.write('\t<body>\n')
-        index_html_file.write('\t\t<a href="http://www.qiime.org" target="_blank"><img src="http://qiime.org/_static/wordpressheader.png" alt="www.qiime.org""/></a>\n')
-        index_html_file.write('\t\t<p>\n')
-        index_html_file.write('\t\t\tBeta diversity metrics\n')
-        index_html_file.write('\t\t\t<ul>\n')
+    with open('index.html', 'w') as index_html_file:
+        s = '<html>\n'
+        s += '\t<head>\n'
+        s += '\t\t<title>Jackknifed beta diversity results</title>\n'
+        s += '\t</head>\n'
+        s += '\t<body>\n'
+        s += '\t\t<a href="http://www.qiime.org" target="_blank">'
+        s += '<img src="http://qiime.org/_static/wordpressheader.png"'
+        s += 'alt="www.qiime.org""/></a>\n'
+        s += '\t\t<p>\n'
+        s += '\t\t\tEmperor PCoA plot after jacknifed beta diversity\n'
+        s += '\t\t\t<ul>\n'
 
         for directory in dir_list:
-            if directory == 'rarefaction':
-                index_html_file.write('\t\t\t\t<li>' + directory)
-                index_html_file.write('\t\t\t\t<ul>\n')
-                for file_name in os.listdir(args.data_directory + '/' + directory):
-                    index_html_file.write('\t\t\t\t\t<li><a href="' + directory + 
-                    '/' + file_name + '">' + file_name + '</a></li>\n')
-                index_html_file.write('\t\t\t\t</ul>\n')
-                index_html_file.write('\t\t\t\t</li>\n')
+            if directory == 'rarefaction' or directory == 'unrarefied_bdiv':
+                continue
 
-            elif directory != 'unrarefied_bdiv':
-                index_html_file.write('\t\t\t\t<li>' + directory)
-                index_html_file.write('\t\t\t\t<ul>\n')
+            s += '\t\t\t\t<li><a href="' + directory
+            s += '/index.html">PCoA plots</a></td></li>\n'
 
-                index_html_file.write('\t\t\t\t\t<li>Rare distance matrix\n')
-                index_html_file.write('\t\t\t\t\t<ul>\n')
-                for file_name in os.listdir(args.data_directory + '/' + directory + '/rare_dm'):
-                    index_html_file.write('\t\t\t\t\t<li><a href="' + directory + 
-                    '/rare_dm/' + file_name + '">' + file_name + '</a></li>\n')
-                index_html_file.write('\t\t\t\t\t</ul>\n')
-                index_html_file.write('\t\t\t\t\t</li>\n')
+        s += '\t\t\t</ul>\n'
+        s += '\t\t</p>\n'
+        s += '\t</body>\n'
+        s += '</html>\n'
+        index_html_file.write(s)
 
-                index_html_file.write('\t\t\t\t\t<li>Rare UPGMA tree\n')
-                index_html_file.write('\t\t\t\t\t<ul>\n')
-                for file_name in os.listdir(args.data_directory + '/' + directory + '/rare_upgma'):
-                    index_html_file.write('\t\t\t\t\t<li><a href="' + directory + 
-                    '/rare_upgma/' + file_name + '">' + file_name + '</a></li>\n')
-                index_html_file.write('\t\t\t\t\t</ul>\n')
-                index_html_file.write('\t\t\t\t\t</li>\n')
-
-                index_html_file.write('\t\t\t\t\t<li>PCoA\n')
-                index_html_file.write('\t\t\t\t\t<ul>\n')
-                for file_name in os.listdir(args.data_directory + '/' + directory + '/pcoa'):
-                    index_html_file.write('\t\t\t\t\t<li><a href="' + directory + 
-                    '/pcoa/' + file_name + '">' + file_name + '</a></li>\n')
-                index_html_file.write('\t\t\t\t\t</ul>\n')
-                index_html_file.write('\t\t\t\t\t</li>\n')
-
-                index_html_file.write('\t\t\t\t\t<li><a href="' + directory + 
-                    '/emperor_pcoa_plots/index.html">PCoA plots</a></td></li>\n')
-            
-                index_html_file.write('\t\t\t\t</ul>\n')
-                index_html_file.write('\t\t\t\t</li>\n')
-
-        index_html_file.write('\t\t\t</ul>\n')
-        index_html_file.write('\t\t</p>\n')
-        index_html_file.write('\t</body>\n')
-        index_html_file.write('</html>\n')
 
 def build_html(args):
     os.mkdir(args.html_dir)
 
-    dir_list = [ name for name in os.listdir(args.data_directory) if 
-        os.path.isdir(os.path.join(args.data_directory, name)) ]
+    dir_list = [name for name in os.listdir(args.data_directory)
+                if os.path.isdir(os.path.join(
+                    args.data_directory,
+                    name))]
 
     generate_index_html(dir_list, args)
 
     os.system('cp -r index.html ' + args.html_dir)
 
     for directory in dir_list:
-        os.system('cp -r ' + args.data_directory + '/' + directory + ' ' + 
-            args.html_dir)
-    
+        if directory == 'rarefaction' or directory == 'unrarefied_bdiv':
+            continue
+        cmd = 'cp -r ' + args.data_directory + '/' + directory
+        cmd += '/emperor_pcoa_plots' + ' ' + args.html_dir + '/' + directory
+        os.system(cmd)
+
     os.system('mv ' + args.html_dir + '/index.html ' + args.html_file)
 
 if __name__ == '__main__':
