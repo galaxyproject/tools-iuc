11206a657e298e538f7e9f8121ec1915a57b69d4
Björn Grüning <bjoern@gruenings.eu>
2017-01-24 11:14:05 +0100

    Update dir2html.py

--- tools/macs2/dir2html.py
+++ tools/macs2/dir2html.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
+
 import os
 import sys
 from xml.sax.saxutils import escape
 
 
-def make_table( directory ):
+def make_table(directory):
     ret = ['<table class="fileList">\n']
-    for file in os.listdir( directory ):
-        ret.append('<tr><td class="file"><a href="%s">%s</a></td></tr>\n' % ( file, escape(file).replace( 'MACS2_', '' ) ))
+    for file in os.listdir(directory):
+        ret.append('<tr><td class="file"><a href="%s">%s</a></td></tr>\n' % (file, escape(file).replace('MACS2_', '')))
     ret.append('</table>')
     return ''.join(ret)
 
 
-def make_html( directory, stderr ):
+def make_html(directory, stderr):
     return '\n'.join(['<html>'
                       '<head>',
                       '   <title>Additional output created by MACS2</title>',
@@ -24,7 +25,7 @@ def make_html( directory, stderr ):
                       '</head>',
                       '<body>',
                       '<h1>Additional Files:</h1>',
-                      make_table( directory ),
+                      make_table(directory),
                       '<h3>Messages from MACS2:</h3>',
                       stderr.read().replace('\n', '<br>'),
                       '</body>',
@@ -34,7 +35,7 @@ def make_html( directory, stderr ):
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         directory_path = sys.argv[1]
-        stderr = open( sys.argv[2] )
-        print make_html( directory_path, stderr )
+        stderr = open(sys.argv[2])
+        print(make_html(directory_path, stderr))
     else:
-        sys.exit( 'Two parameter expected: directory path and stderr path' )
+        sys.exit('Two parameter expected: directory path and stderr path')
