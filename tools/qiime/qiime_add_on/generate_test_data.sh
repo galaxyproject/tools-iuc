f445c84e560d9c2f1f06ab6f223dd76680b2db5a
Aurélien Bernard <aurelien.bernard.bioinfo63@gmail.com>
2017-01-26 16:42:13 +0100

    Adding of make_otu_table command lines to the generate_test_data script
    The command lines required to generate the output files that will be used as comparison in "planemo test" have been added to the generate_test_data shell script.

--- tools/qiime/qiime_add_on/generate_test_data.sh
+++ tools/qiime/qiime_add_on/generate_test_data.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+# make_otu_table
+make_otu_table.py \
+    --otu_map_fp 'test-data/make_otu_table/otu_map.txt' \
+    --taxonomy 'test-data/make_otu_table/tax_assignments.txt' \
+    --exclude_otus_fp 'test-data/make_otu_table/chimeric_seqs.txt' \
+    --mapping_fp 'test-data/make_otu_table/mapping_file.txt' \
+    --output_biom_fp 'test-data/make_otu_table/OTU_table_chimeric.biom'
+
+make_otu_table.py \
+    --otu_map_fp 'test-data/make_otu_table/otu_map.txt' \
+    --taxonomy 'test-data/make_otu_table/tax_assignments.txt' \
+    --exclude_otus_fp 'test-data/make_otu_table/pynast_failures.fna' \
+    --mapping_fp 'test-data/make_otu_table/mapping_file.txt' \
+    --output_biom_fp 'test-data/make_otu_table/OTU_table_pynast.biom'
