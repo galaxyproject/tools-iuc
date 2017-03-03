21ae197c62a9d38f33c6a2b492fe2c2bf6420eca
Lance Parsons <lparsons@princeton.edu>
2017-03-03 13:59:12 -0500

    Upgrade to RSeQC 2.6.4
    Move to IUC, added some new tools, updated tests, use element_identifier
    where appropriate.

--- tools/rseqc/test-data/output.clipping_profile.r
+++ tools/rseqc/test-data/output.clipping_profile.r
@@ -1,5 +1,6 @@
 pdf("output.clipping_profile.pdf")
-read_pos=c(0,1,2,3,4,5,6,7,8,9,44,45,46,47,48,49,50)
-count=c(16,12,11,8,6,5,1,1,1,1,1,2,2,2,3,4,4)
-plot(read_pos,1-(count/40),col="blue",main="clipping profile",xlab="Position of reads",ylab="Mappability",type="b")
+read_pos=c(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50)
+clip_count=c(16.0,12.0,11.0,8.0,7.0,6.0,1.0,1.0,1.0,1.0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.0,1.0,1.0,2.0,3.0,4.0,4.0)
+nonclip_count= 40 - clip_count
+plot(read_pos, nonclip_count*100/(clip_count+nonclip_count),col="blue",main="clipping profile",xlab="Position of read",ylab="Non-clipped %",type="b")
 dev.off()
