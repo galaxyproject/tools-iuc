8074a67f7d415bb55bf16465290dbb3ba1c409c8
yhoogstrate <y.hoogstrate@gmail.com>
2017-03-15 15:09:03 +0100

    flake8

--- data_managers/data_manager_star_index_builder/data_manager/rna_star_index_builder.py
+++ data_managers/data_manager_star_index_builder/data_manager/rna_star_index_builder.py
@@ -3,6 +3,7 @@
 import json
 import optparse
 
+
 def main():
     parser = optparse.OptionParser()
     parser.add_option( '--config-file', dest='config_file', action='store', type="string")
@@ -12,9 +13,9 @@ def main():
     parser.add_option( '--subdir', dest='subdir', action='store', type="string" )
     parser.add_option( '--data-table', dest='data_table', action='store', type="string" )
     (options, args) = parser.parse_args()
-    
+
     if options.dbkey in [ None, '', '?' ]:
-        raise Exception( '"%s" is not a valid dbkey. You must specify a valid dbkey.' % ( dbkey ) )
+        raise Exception( '"%s" is not a valid dbkey. You must specify a valid dbkey.' % ( options.dbkey ) )
 
     data_manager_dict = {'data_tables': {options.data_table: [dict( value=options.value, dbkey=options.dbkey, name=options.name, path=options.subdir )]}}
     open( options.config_file, 'wb' ).write( json.dumps( data_manager_dict ) )
