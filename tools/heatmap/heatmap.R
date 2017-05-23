bee749f275a9811df00b9d26fbc20b25202c4ed4
Dave B <dave@bx.psu.edu>
2017-05-23 16:29:11 -0400

    Add support for and dependency on RColorBrewer.

--- tools/heatmap/heatmap.R
+++ tools/heatmap/heatmap.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env R
 library(argparse)
+library(RColorBrewer)
 
 parser <- ArgumentParser(description='Generate a heatmap from tabular data')
 
@@ -15,8 +16,12 @@ parser$add_argument('--dendrogram', dest='dendrogram', action="store")
 parser$add_argument('--col_min', dest='col_min', action="store")
 parser$add_argument('--col_max', dest='col_max', action="store")
 parser$add_argument('--output', dest='output', action="store")
+parser$add_argument('--palette', dest='palette', action="store")
+parser$add_argument('--shades', dest='shades', action="store")
+parser$add_argument('--select_color', dest='select_color', action="store_true")
 
 args <- parser$parse_args()
+
 options( show.error.messages=F,
          error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) },
          warn=-1 )
@@ -44,9 +49,14 @@ if (args$dendrogram == 'column' || args$dendrogram == 'both') {
     Colv_value = TRUE
 }
 
+if (args$select_color) {
+    rgb_palette = colorRampPalette(c(args$col_min, args$col_max), space="rgb")
+    colors = rgb_palette(as.numeric(args$shades))
+} else {
+    colors = brewer.pal(as.numeric(args$shades), args$palette)
+}
 ## Open output PDF file
 pdf( args$output )
-rgb_palette = colorRampPalette(c(args$col_min, args$col_max), space="rgb")
-heatmap(as.matrix(x), main=args$main, xlab=args$xlab, ylab=args$ylab, scale=args$scale, Rowv=Rowv_value, Colv=Colv_value, na.rm=na_rm_value, col=rgb_palette(10))
+heatmap(as.matrix(x), main=args$main, xlab=args$xlab, ylab=args$ylab, scale=args$scale, Rowv=Rowv_value, Colv=Colv_value, na.rm=na_rm_value, col=colors)
 ## Close the PDF file
 devname = dev.off()
