26063da2ede2472e38445ad7664d6bddae6b0acb
Dave B <dave@bx.psu.edu>
2017-06-06 15:55:29 -0400

    Some refactoring of the R script.

--- tools/heatmap/heatmap.R
+++ tools/heatmap/heatmap.R
@@ -25,8 +25,18 @@ args <- parser$parse_args()
 options( show.error.messages=F,
          error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) },
          warn=-1 )
-inp = read.table( args$input )
-x = inp[, as.numeric(unlist(strsplit(args$var_cols, split=",")))]
+
+header = FALSE
+if (args$header == 'yes') {
+  header = TRUE
+}
+
+inp = as.matrix(read.table(args$input, header=header, row.names=1, as.is=TRUE, check.names=FALSE))
+
+columns = lapply(as.numeric(unlist(strsplit(args$var_cols, split=","))), function(x) x - 1)
+
+x = apply(inp[1:nrow(inp),1:ncol(inp)], c(1,2), as.numeric)[,unlist(columns)]
+
 na_rm_value = FALSE
 Colv_value = NA
 Rowv_value = NA
@@ -35,16 +45,10 @@ if (args$na_remove == 'yes') {
     na_rm_value = TRUE
 }
 
-if (args$header == 'yes') {
-  colnames(x) = rapply(x[1,], as.character)
-  x = x[2:nrow(x),]
-}
-
-x = apply(x,2,as.numeric)
-
 if (args$dendrogram == 'row' || args$dendrogram == 'both') {
     Rowv_value = TRUE
 }
+
 if (args$dendrogram == 'column' || args$dendrogram == 'both') {
     Colv_value = TRUE
 }
@@ -55,8 +59,9 @@ if (args$select_color) {
 } else {
     colors = brewer.pal(as.numeric(args$shades), args$palette)
 }
+
 ## Open output PDF file
 pdf( args$output )
-heatmap(as.matrix(x), main=args$main, xlab=args$xlab, ylab=args$ylab, scale=args$scale, Rowv=Rowv_value, Colv=Colv_value, na.rm=na_rm_value, col=colors)
+heatmap(x, main=args$main, xlab=args$xlab, ylab=args$ylab, scale=args$scale, Rowv=Rowv_value, Colv=Colv_value, na.rm=na_rm_value, col=colors)
 ## Close the PDF file
 devname = dev.off()
