a056b8b7c4340094f05b3beaf42080062799798d
Bérénice Batut <bebatut@gmail.com>
2017-08-08 13:08:38 +0200

    enasearch_retrieve_data: add wrapper

--- tools/enasearch/generate_macros.py
+++ tools/enasearch/generate_macros.py
@@ -22,15 +22,38 @@ def sort_by_name(dict):
     return sorted(dict, key=dict.get)
 
 
-def write_analysis_fields(filepath):
+def write_analysis_fields():
     """
-    Write the analysis fields in a file
+    Write the analysis fields
     """
     s = '%s<xml name="analysis_fields">\n' % (spaces)
     fields = enasearch.get_returnable_fields(result="analysis", verbose=False)
     for f in fields:
         s += '%s<option value="%s">%s</option>\n' % (2*spaces, f, f)
     s += '%s</xml>\n' % (spaces)
+    return s
+
+def write_display_options():
+    """
+    Write the display options
+    """
+    s = '%s<xml name="display_options">\n' % (spaces)
+    when_s = '%s<xml name="when_display_options">\n' % (spaces)
+    options = enasearch.get_display_options(verbose=False)
+    for opt in options:
+        s += '%s<option value="%s">%s</option>\n' % (2*spaces, opt, options[opt]['description'])
+        when_s += '%s<when value="%s">\n' % (2*spaces, opt)
+        if opt == 'fasta' or opt == 'fastq':
+            when_s += '%s<param name="range_start" argument="--subseq_range" type="integer" optional="true" label="Start integer for subsequences"/>\n'  % (3*spaces)
+            when_s += '%s<param name="range_stop" argument="--subseq_range" type="integer" optional="true" label="Stop integer for subsequences"/>\n'  % (3*spaces)
+        else:
+            when_s += '%s<param argument="--offset" type="integer" optional="true" label="First record to get"/>\n'  % (3*spaces)
+            when_s += '%s<param argument="--length" type="integer" optional="true" label="Number of records to retrieve"/>\n'  % (3*spaces)
+        when_s += '%s</when>\n'  % (2*spaces)
+    s += '%s</xml>\n' % (spaces)
+    when_s += '%s</xml>\n' % (spaces)
+    s += when_s
+    return s
 
 
 def generate_search_macros(filepath):
@@ -39,7 +62,8 @@ def generate_search_macros(filepath):
     """
     s = '<?xml version="1.0" ?>\n'
     s += '<macros>\n'
-    s += write_analysis_fields("tool-data/analysis_fields.loc")
+    s += write_analysis_fields()
+    s += write_display_options()
     s += '</macros>\n'
     with open(filepath, "w") as file:
         file.write(s)
