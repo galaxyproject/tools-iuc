8382bb859ba2d724da9d52ea9fec5e82353f7fde
nekrut <anton@nekrut.org>
2018-03-23 16:34:03 -0400

    tool for gb2fa conversion

--- tool_collections/snpeff/gbk2fa.py
+++ tool_collections/snpeff/gbk2fa.py
@@ -15,7 +15,7 @@ gbk_filename = args.genbank_file
 faa_filename = args.fasta_file
 
 try:
-    f_type = magic.from_file(args.genbank_file, mime=True).decode('UTF-8')
+    f_type = magic.from_file(args.genbank_file, mime=True)
     if f_type == 'text/plain':
         input_handle  = open(gbk_filename, "r")
     elif f_type == 'application/gzip':
