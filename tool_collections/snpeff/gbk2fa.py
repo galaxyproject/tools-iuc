aa85accac8456acf2a9273905cbb529b693c95a0
nekrut <anton@nekrut.org>
2018-03-26 21:16:18 -0400

    more flake8 issue fixes

--- tool_collections/snpeff/gbk2fa.py
+++ tool_collections/snpeff/gbk2fa.py
@@ -17,6 +17,7 @@ gbk_filename = args.genbank_file
 fa_filename = args.fasta_file
 
 @contextlib.contextmanager
+
 def get_file_handle(gbk_filename):
     f_type = magic.from_file(args.genbank_file, mime=True)
     if f_type == 'text/plain':
@@ -24,14 +25,14 @@ def get_file_handle(gbk_filename):
     elif f_type == 'application/gzip':
         input_handle = gzip.open(gbk_filename, "rt")
     elif f_type == 'application/x-bzip2':
-        input_handle = bz2.open(gbk_filename, "rt")    
+        input_handle = bz2.open(gbk_filename, "rt")
     else:
         sys.exit("Cannot process file of type {}. Only plain, gzip'ed, and bzip2'ed genbank files are accepted ".format(f_type))
     yield input_handle
     input_handle.close()
 
 
-with get_file_handle(gbk_filename) as input_handle, open(fa_filename, "w") as output_handle: 
+with get_file_handle(gbk_filename) as input_handle, open(fa_filename, "w") as output_handle:
 
     for seq_record in SeqIO.parse(input_handle, "genbank"):
         if args.remove_version:
@@ -40,4 +41,3 @@ with get_file_handle(gbk_filename) as input_handle, open(fa_filename, "w") as ou
             seq_id = seq_record.id
         print('Writing FASTA record: {}'.format( seq_id ))
         output_handle.write(">{}\n{}\n".format(seq_id, seq_record.seq))
-    
