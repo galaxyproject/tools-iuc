072c8b762ae3dc3249132e66ad60f0f7c14f1582
Mehmet Tekman <m.tekman@ucl.ac.uk>
2018-04-17 17:57:49 +0200

    filtering module complete with 1 test case

--- tools/scater_pipe/scripts/common.R
+++ tools/scater_pipe/scripts/common.R
@@ -1,15 +1,17 @@
 libs <- c("scater", "SingleCellExperiment")
-res <- lapply(arr, require, character.only = TRUE)
+suppressPackageStartupMessages(
+    lapply(libs, require, character.only = TRUE)
+)
 
-                                        # Intergalactic
+# Intergalactic
 if (is.na(script_dir) || is.na(config_file) || script_dir == "None" || config_file == "None"){
    stop("Could not find script_dir or config_file")
 }
 
-                                        # Load params
+# Load params
 source(config_file)
 
-                                        # Common functions
+# Common functions
 message <- function(...){ print(sprintf(...)) }
 plotter <- function(fname, funct){
     name <- paste(fname, "svg", sep=".")
--- tools/scater_pipe/scripts/scater_filter.R
+++ tools/scater_pipe/scripts/scater_filter.R
@@ -10,10 +10,13 @@ source(paste(script_dir, "common.R", sep="/"))
 # Read input data
 x <- read.table(count_matrix, header=col_header, comment=commchar, row.names=row_head)
 
-message("Count matrix with %.0f cells and %.0f genes", dim(x)[1], dim(x)[2])
+#message("Count matrix with %.0f cells and %.0f genes", dim(x)[1], dim(x)[2])
 
 # Perform the spike-in filtering
-x_filt <- x[!grepl(filterout,x),]
+x_filt <- x
+if (filterout != "" ){
+    x_filt <- x[!grepl(filterout,x_filt),]
+}
 
 sce <- SingleCellExperiment(assays = list(counts = as.matrix(x_filt)))
 sce <- calculateQCMetrics(sce)
@@ -48,12 +51,13 @@ list_of_genes_with_n_cells <- cell_counts_per_gene > gdetect_mincells
 gdetect_matrix <- sce_f[list_of_genes_with_n_cells,]
 
 tab <- rbind(
+    dim(x),
     dim(sce),
     dim(sce_f),
     dim(gdetect_matrix)
 )
 colnames(tab) <- c("Genes", "Cells")
-rownames(tab) <- c("Initial Matrix", "After Filtering", "After Gene Detection")
+rownames(tab) <- c("Initial Matrix", "Filtered names", "Filtered counts", "Filtered Gene Detection")
 
 write.table(tab, "table.stats")
 write.table(counts(gdetect_matrix), "table.counts")
