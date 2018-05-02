591b4002fb4f0614b8f823a9c7894ee5a4d27f06
bimbam23 <bommel_23@gmx.net>
2018-05-02 15:55:39 +0200

    reformated

--- tools/rcorrector/FilterUncorrectabledPEfastq.py
+++ tools/rcorrector/FilterUncorrectabledPEfastq.py
@@ -17,10 +17,11 @@ or gzipped files on the fly, so long as the gzipped files end with 'gz'.
 """
 
 # import sys
-import gzip
 import argparse
-from itertools import izip,izip_longest
+import gzip
+from itertools import izip, izip_longest
 from os.path import basename
+
 
 def get_input_streams(r1file, r2file):
     if r1file[-2:] == 'gz':
@@ -29,21 +30,23 @@ def get_input_streams(r1file, r2file):
     else:
         r1handle = open(r1file, 'r')
         r2handle = open(r2file, 'r')
-    return r1handle,r2handle
+    return r1handle, r2handle
 
 
-def grouper(iterable, n, fillvalue = None):
+def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
-    return izip_longest(fillvalue = fillvalue, * args)
+    return izip_longest(fillvalue=fillvalue, * args)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "options for filtering and logging rCorrector fastq outputs")
-    parser.add_argument('-1', '--left_reads', dest = 'leftreads', type = str, help = 'R1 fastq file')
-    parser.add_argument('-2', '--right_reads', dest = 'rightreads', type = str, help = 'R2 fastq file')
-    parser.add_argument('-o', '--out_prefix', dest = 'outprefix', type = str, help = "prefix for filtered fastq output")
+    parser = argparse.ArgumentParser(description="options for filtering and logging rCorrector fastq outputs")
+    parser.add_argument('-1', '--left_reads', dest='leftreads', type=str, help='R1 fastq file')
+    parser.add_argument('-2', '--right_reads', dest='rightreads', type=str, help='R2 fastq file')
+    parser.add_argument('-o', '--out_prefix', dest='outprefix', type=str, help="prefix for filtered fastq output")
     opts = parser.parse_args()
-    r1out = open(opts.outprefix + '_'+ basename(opts.leftreads).replace('.gz', ''), 'w')
+    r1out = open(opts.outprefix + '_' + basename(opts.leftreads).replace('.gz', ''), 'w')
     r2out = open(opts.outprefix + '_' + basename(opts.rightreads).replace('.gz', ''), 'w')
     r1_cor_count = 0
     r2_cor_count = 0
@@ -69,13 +72,13 @@ if __name__ == "__main__":
                     r2_cor_count += 1
                 if 'cor' in head1 or 'cor' in head2:
                     pair_cor_count += 1
-                head1 = head1.split('l:')[0][:-1] # keeps all before the low kmer count statistic and removes the trailing whitespace character
+                head1 = head1.split('l:')[0][:-1]  # keeps all before the low kmer count statistic and removes the trailing whitespace character
                 head2 = head2.split('l:')[0][:-1]
                 #head1 = head1.replace(' cor', '')
                 #head2 = head2.replace(' cor', '')
                 r1out.write('%s\n' % '\n'.join([head1, seq1, placeholder1, qual1]))
                 r2out.write('%s\n' % '\n'.join([head2, seq2, placeholder2, qual2]))
     unfix_log = open('rmunfixable.log', 'w')
-    unfix_log.write('total PE reads:%s\nremoved PE reads:%s\nretained PE reads:%s\nR1 corrected:%s\nR2 corrected:%s\npairs corrected:%s\n' % (counter, unfix_count, counter-unfix_count, r1_cor_count, r2_cor_count, pair_cor_count))
+    unfix_log.write('total PE reads:%s\nremoved PE reads:%s\nretained PE reads:%s\nR1 corrected:%s\nR2 corrected:%s\npairs corrected:%s\n' % (counter, unfix_count, counter - unfix_count, r1_cor_count, r2_cor_count, pair_cor_count))
     r1out.close()
 r2out.close()
