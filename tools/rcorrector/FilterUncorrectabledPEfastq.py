106d242efd871c69c95ef733271e8a63f3b39c6a
bimbam23 <bommel_23@gmx.net>
2018-05-03 08:46:38 +0200

    lint fixes2

--- tools/rcorrector/FilterUncorrectabledPEfastq.py
+++ tools/rcorrector/FilterUncorrectabledPEfastq.py
@@ -19,7 +19,8 @@ or gzipped files on the fly, so long as the gzipped files end with 'gz'.
 # import sys
 import argparse
 import gzip
-# from itertools import izip, izip_longest
+from itertools import izip_longest
+# izip, 
 from os.path import basename
 
 
