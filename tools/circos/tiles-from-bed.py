2003365b2a0280306afe7444f5d26ce1d3be6eff
Helena Rasche <hxr@hx42.org>
2018-06-29 15:37:39 +0200

    do not support colors

--- tools/circos/tiles-from-bed.py
+++ tools/circos/tiles-from-bed.py
@@ -1,49 +1,30 @@
 #!/usr/bin/env python
 import logging
 import sys
-import hashlib
-
-from BCBio import GFF
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
 
 if __name__ == '__main__':
-    colormap = {}
-    # We need to ensure the colour maps do not overlap (in name) across
-    # multiple runs of this script. That would cause a circos error.
-    # The best, reproducible idea I've had so far is hashing.
-    input_str = '|'.join(sys.argv[1:])
-    uniq_id = hashlib.md5(str(input_str).encode('utf-8')).hexdigest()[0:6]
+    with open(sys.argv[1], 'r') as handle:
+        for line in handle:
+            lineData = line.strip().split()
+            # BED3+ chrom chromStart chromEnd
+            # BED6+ name score strand
+            # BED9+ thickStart thickEnd itemRgb
+            kv = {}
+            if len(lineData) >= 6:
+                kv['strand'] = lineData[5].replace('+', '1').replace('-', '-1')
+                kv['name'] = lineData[3]
+                kv['value'] = lineData[4]
 
-    for idx, file_path in enumerate(sys.argv[1:]):
-        log.info("Processing %s", file_path)
+            line = [
+                lineData[0],  # chrom
+                lineData[1],  # chromStart
+                lineData[2],  # chromEnd
+                ','.join(['%s=%s' % x for x in kv.items()])
+            ]
 
-        with open(file_path, 'r') as handle:
-            for line in handle:
-                lineData = line.strip().split()
-                # BED3+ chrom chromStart chromEnd
-                # BED6+ name score strand
-                # BED9+ thickStart thickEnd itemRgb
-                kv = {}
-                if len(lineData) >= 9:
-                    color = lineData[8]
-                    if color not in colormap:
-                        colormap[color] = 'gx-band-%s-%s' % (uniq_id, len(colormap.keys()))
-
-                    kv['color'] = colormap[color]
-                if len(lineData) >= 6:
-                    kv['strand'] = lineData[5].replace('+', '1').replace('-', '-1')
-                    kv['name'] = lineData[3]
-                    kv['value'] = lineData[4]
-
-                line = [
-                    lineData[0],  # chrom
-                    lineData[1],  # chromStart
-                    lineData[2],  # chromEnd
-                    ','.join(['%s=%s' % x for x in kv.items()])
-                ]
-
-                sys.stdout.write('\t'.join(line))
-                sys.stdout.write('\n')
+            sys.stdout.write('\t'.join(line))
+            sys.stdout.write('\n')
--- tools/circos/tiles-from-gff3.py
+++ tools/circos/tiles-from-gff3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import logging
 import sys
-import hashlib
 
 from BCBio import GFF
 
@@ -10,43 +9,27 @@ log = logging.getLogger()
 
 
 if __name__ == '__main__':
-    colormap = {}
-    # We need to ensure the colour maps do not overlap (in name) across
-    # multiple runs of this script. That would cause a circos error.
-    # The best, reproducible idea I've had so far is hashing.
-    input_str = '|'.join(sys.argv[1:])
-    uniq_id = hashlib.md5(str(input_str).encode('utf-8')).hexdigest()[0:6]
+    for record in GFF.parse(sys.argv[1]):
+        if len(record.features) == 0:
+            continue
 
-    for idx, file_path in enumerate(sys.argv[1:]):
-        log.info("Processing %s", file_path)
+        for feature in sorted(record.features, key=lambda x: x.location.start):
+            # chrom chromStart chromEnd
+            # name score strand
+            # thickStart thickEnd itemRgb
 
-        for record in GFF.parse(file_path):
-            if len(record.features) == 0:
-                continue
+            kv = {
+                'strand': 0 if not feature.location.strand else feature.location.strand,
+                'name': feature.qualifiers.get('Name', [None])[0] or feature.id,
+                'value': feature.qualifiers.get('score', [0])[0],
+            }
 
-            for feature in sorted(record.features, key=lambda x: x.location.start):
-                # chrom chromStart chromEnd
-                # name score strand
-                # thickStart thickEnd itemRgb
+            line = [
+                record.id,
+                str(int(feature.location.start)),
+                str(int(feature.location.end)),
+                ','.join(['%s=%s' % x for x in kv.items()])
+            ]
 
-                kv = {
-                    'strand': 0 if not feature.location.strand else feature.location.strand,
-                    'name': feature.qualifiers.get('Name', [None])[0] or feature.id,
-                    'value': feature.qualifiers.get('score', [0])[0],
-                }
-                if 'color' in feature.qualifiers:
-                    color = feature.qualifiers['color'][0]
-                    if color not in colormap:
-                        colormap[color] = 'gx-band-%s-%s' % (uniq_id, len(colormap.keys()))
-
-                    kv['color'] = colormap[color]
-
-                line = [
-                    record.id,
-                    str(int(feature.location.start)),
-                    str(int(feature.location.end)),
-                    ','.join(['%s=%s' % x for x in kv.items()])
-                ]
-
-                sys.stdout.write('\t'.join(line))
-                sys.stdout.write('\n')
+            sys.stdout.write('\t'.join(line))
+            sys.stdout.write('\n')
