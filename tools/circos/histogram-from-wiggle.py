09a5a587c36ca23ee5bac88c84c5d8d6c825ec3b
Helena Rasche <hxr@hx42.org>
2018-07-02 13:34:15 +0200

    updates

--- tools/circos/histogram-from-wiggle.py
+++ tools/circos/histogram-from-wiggle.py
@@ -15,13 +15,13 @@ if __name__ == '__main__':
     for idx, item in enumerate(files):
         with open(item, 'r') as handle:
             for chrom, start, end, value in walker.walk(handle):
-                key = '%s %s %s' % (chrom, start, end)
+                key = '%s\t%s\t%s' % (chrom, start, end)
                 if key not in bins:
                     bins[key] = [0] * len(files)
                 bins[key][idx] = value
 
     for (k, v) in bins.items():
         sys.stdout.write(k)
-        sys.stdout.write(' ')
-        sys.stdout.write(' '.join(map(str, v)))
+        sys.stdout.write('\t')
+        sys.stdout.write(','.join(map(str, v)))
         sys.stdout.write('\n')
