3c05c8c8d2862ebb4b24d0c9c7047ef5aebf5bf8
Mehmet Tekman <m.tekman@ucl.ac.uk>
2018-07-24 12:31:06 +0200

    Much neater graph with labels

--- tools/crosscontamination_filter/scripts/crosscontamination_filter.R
+++ tools/crosscontamination_filter/scripts/crosscontamination_filter.R
@@ -210,34 +210,89 @@ library(ggplot2)
 library(gridExtra)
 
 contaminationPlot <- function(columncounts, title = "", indexes.plates, indexes.fullbc, indexes.truebc)
-{
+{   
     par(mfrow=c(2,1))
-
     dfer <- data.frame(colcounts=columncounts)
 
     ## Remove spots where plates and full barcodes mix
-    indexes.fullbc = indexes.fullbc[!(indexes.fullbc %in% indexes.plates)]
+    #indexes.fullbc = indexes.fullbc[!(indexes.fullbc %in% indexes.plates)]
 
     nit <- length(indexes.truebc)
     nif <- length(indexes.fullbc)
     nip <- length(indexes.plates)
     mval <- max(dfer)
 
-    truebcs <- data.frame(x=indexes.truebc, y=rep(0,nit), xend=indexes.truebc, yend=rep(mval,nit))
-    fullbcs <- data.frame(x=indexes.fullbc, y=rep(0,nif), xend=indexes.fullbc, yend=rep(mval,nif))
-    platess <- data.frame(x=indexes.plates, y=rep(0,nip), xend=indexes.plates, yend=rep(2*mval/5,nip))
+    ## Aesthetic
+    min.height <- -300
+    tf.spacing.left <- 12
+    tf.spacing.right <- 12
+    
+    tf.height <- mval - 10000
+    bn.height <- mval - 2000
+    plate.color <- "grey"
+    plate.color.text <- "black"
+    plate.alpha.text <- 0.5
+    plate.height <- 2*mval/5
+    
+    truebcs <- data.frame(x=indexes.truebc, y=rep(min.height,nit), xend=indexes.truebc, yend=rep(mval,nit))
+    fullbcs <- data.frame(x=indexes.fullbc, y=rep(min.height,nif), xend=indexes.fullbc, yend=rep(mval,nif))
+    platess <- data.frame(x=indexes.plates, y=rep(min.height,nip), xend=indexes.plates, yend=rep(plate.height,nip))
+    connecting.bar <- data.frame(x=min(indexes.plates), y=min.height, xend=max(indexes.plates), yend=min.height)
+    
 
     p1 <- ggplot() +
+        geom_segment(data=truebcs, aes(x=x,y=y,xend=xend,yend=yend - 4000), col='grey', lty=2, size=0.2) +
+        geom_segment(data=fullbcs, aes(x=x,y=y,xend=xend,yend=yend), col='blue', lty=1, size=0.4, alpha=0.2) +
+        geom_segment(data=platess, aes(x=x,y=y,xend=xend,yend=yend), col=plate.color, lty=1, size=1) +
+        geom_segment(data=connecting.bar, aes(x=x,y=y + 200 ,xend=xend,yend=yend), col=plate.color, lty=1, size=1) +
         geom_point(
             date=dfer, aes(x=1:length(rownames(dfer)), y=dfer$colcounts),
             pch = 16, cex = 1) +
         theme(plot.title = element_text(hjust = 0.5)) +
         labs(title=paste("Contamination Plot\n", title), y="Library Size", x="Cell No.") +
-        geom_segment(data=truebcs, aes(x=x,y=y,xend=xend,yend=yend), col='darkgreen', lty=2, size=0.5) +
-        geom_segment(data=fullbcs, aes(x=x,y=y,xend=xend,yend=yend), col='blue', lty=1, size=0.5) +
-        geom_segment(data=platess, aes(x=x,y=y,xend=xend,yend=yend), col='blue', lty=1, size=1) +
         scale_y_continuous(breaks=seq(0,mval + 10000, 10000)) +
         scale_x_continuous(breaks=seq(0,length(rownames(dfer)),500))
+
+    ## Add true/false and batch labels
+    res <- lapply(indexes.truebc, function(xval){
+        batch <- match(xval, indexes.truebc)
+        
+        p1 <<- p1 +
+            annotate("text", x=xval - tf.spacing.left, size=2, y=tf.height, angle=90,
+                     label=" true positives", color = "dark blue", alpha = 0.5) +
+            annotate("text", x=xval + tf.spacing.right, size=2, y=tf.height, angle=-90,
+                     label="false positives", color = "black", alpha = 0.5) +
+            annotate("text", x=xval, size=4, y=bn.height, angle=-90,
+                     label=paste("B",batch,sep=""), color = "grey", alpha = 0.8)
+    })
+
+    ## Add Plate labels
+    res <- lapply(indexes.plates, function(p){
+        plate.num <- match(p, indexes.plates)
+        left.of <- 18
+        right.of <- 18
+
+        c.label <- paste("Plate", plate.num, sep="")
+        b.label <- paste("Plate", plate.num - 1, sep="")
+        siz <- 3
+        hei <- plate.height - 3000
+
+        # Right label
+        if (plate.num <  length(indexes.plates)){
+            p1 <<- p1 +
+                annotate("text", x=p + right.of, size=siz, y=hei, angle=-90,
+                         label=c.label, color = plate.color.text, alpha=plate.alpha.text)
+        }
+
+        # Left label
+        if (plate.num > 1){
+            p1 <<- p1 +
+                annotate("text", x=p - left.of, size=siz, y=hei, angle=90,
+                         label=b.label, color = plate.color.text,  alpha=plate.alpha.text)
+        }
+    })
+        
+
 
     ggsave("test.pdf", device="pdf")
 
@@ -255,6 +310,5 @@ contaminationPlot(
     calculateFullBarcodeIndexes(num.batches, num.barcodes),
     calculateRealBarcodeIndexes(spec$format, num.barcodes)
 )
-
 ## TODO -- filter out unwanted, and plot contamination plot and histogram as seperate pages of a PDF
 ##      -- also fix the axes report the minimum
