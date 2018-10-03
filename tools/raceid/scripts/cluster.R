2f7dc7bcefbc5c1413e54bda113749ce438afaf7
Mehmet Tekman <mtekman89@gmail.com>
2018-10-03 21:54:44 +0200

    Tidied plots and added better titles, fix to incompatible ggplots

--- tools/raceid/scripts/cluster.R
+++ tools/raceid/scripts/cluster.R
@@ -56,19 +56,26 @@ do.outlier <- function(sc){
     print(plotbackground(sc))
     print(plotsensitivity(sc))
     print(plotoutlierprobs(sc))
-                                        # Heatmaps
+    ## Heatmaps
     x <- clustheatmap(sc, final=FALSE)
     x <- clustheatmap(sc, final=TRUE)
     return(sc)
 }
 
 do.clustmap <- function(sc){
+    test <- list()
+    test$side = 3
+    test$line = 2.5
     sc <- do.call(comptsne, c(sc, cluster.comptsne))
     sc <- do.call(compfr, c(sc, cluster.compfr))
     print(plotmap(sc, final = FALSE, fr = FALSE))
+    print(do.call(mtext, c("Initial Clustering tSNE", test)))
     print(plotmap(sc, final = TRUE, fr = FALSE))
+    print(do.call(mtext, c("Final Clustering tSNE", test)))
     print(plotmap(sc, final = FALSE, fr = TRUE))
+    print(do.call(mtext, c("Initial Clustering Fruchterman-Reingold", test)))
     print(plotmap(sc, final = TRUE, fr = TRUE))
+    print(do.call(mtext, c("Final Clustering Fruchterman-Reingold", test)))
     return(sc)
 }
 
@@ -88,14 +95,18 @@ mkgenelist <- function(sc){
 }
 
 pdf(out.pdf)
+par(mfrow=c(2,2))
 sc <- do.filter(sc)
 message(paste(" - Source:: genes:",nrow(sc@expdata),", cells:",ncol(sc@expdata)))
 message(paste(" - Filter:: genes:",nrow(sc@ndata),", cells:",ncol(sc@ndata)))
 message(paste("         :: ",
               sprintf("%.1f", 100 * nrow(sc@ndata)/nrow(sc@expdata)), "% of genes remain,",
               sprintf("%.1f", 100 * ncol(sc@ndata)/ncol(sc@expdata)), "% of cells remain"))
+par(mfrow=c(2,2))
 sc <- do.cluster(sc)
+par(mfrow=c(2,2))
 sc <- do.outlier(sc)
+par(mfrow=c(2,2), mar=c(1,1,6,1))
 sc <- do.clustmap(sc)
 dev.off()
 
