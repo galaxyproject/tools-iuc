bcacf9229649e6c994b585381869e5b1296717e2
Mehmet Tekman <mtekman89@gmail.com>
2018-10-08 23:56:42 +0200

    cluster inspect fix

--- tools/raceid/scripts/clusterinspect.R
+++ tools/raceid/scripts/clusterinspect.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env R
-VERSION = "0.1"
+VERSION = "0.2"
 
 args = commandArgs(trailingOnly = T)
 
@@ -11,39 +11,25 @@ if (length(args) != 1){
 suppressWarnings(suppressPackageStartupMessages(require(RaceID)))
 source(args[1])
 
-do.inspect.symbolmap <- function(sc){  
+## layout
+test <- list()
+test$side = 3
+test$line = 3
+
+do.inspect.symbolmap <- function(sc){
     if (!is.null(plotsym.use.typeremoveregex)){
         plotsym$types = sub(plotsym.use.typeremoveregex, "", colnames(sc@ndata))
-        
+
         if (!is.null(plotsym.use.typeremoveregex.subselect)){
             plotsym$subset = plotsym$types[grep(plotsym.use.typeremoveregex.subselect, plotsym$types)]
         }
     }
     plotsym$fr = FALSE
     print(do.call(plotsymbolsmap, c(sc, plotsym)))
+    print(do.call(mtext, c("Symbols tSNE", test)))
     plotsym$fr = TRUE
     print(do.call(plotsymbolsmap, c(sc, plotsym)))
-}
-
-do.inspect.genesofinterest <- function(sc){
-    plotexp$logsc=FALSE; plotexp$fr = FALSE
-    print(do.call(plotexpmap, c(sc, plotexp)))
-
-    plotexp$logsc=TRUE; plotexp$fr = FALSE
-    print(do.call(plotexpmap, c(sc, plotexp)))
-
-    plotexp$logsc=FALSE; plotexp$fr = FALSE
-    print(do.call(plotexpmap, c(sc, plotexp)))
-
-    plotexp$logsc=TRUE; plotexp$fr = FALSE
-    print(do.call(plotexpmap, c(sc, plotexp)))
-
-    if (!is.null(plotmarkg$samples)){
-        reg <- plotmarkg$samples
-        plotmarkg$samples <- sub("(\\_\\d+)$","", colnames(sc@ndata))
-    }
-    
-    print(do.call(plotmarkergenes, c(sc, plotmarkg)))
+    print(do.call(mtext, c("Symbols FR", test)))
 }
 
 do.inspect.diffgene <- function(sc){
@@ -68,12 +54,49 @@ do.inspect.diffgene <- function(sc){
 
     A <- getSubNames(gfdat.A.use, sc)
     B <- getSubNames(gfdat.B.use, sc)
-    
+
     fdat <- getfdata(sc, n=c(A,B))
     dexp <- diffexpnb(fdat, A=A, B=B)
     ## options for diffexpnb are mostly about DESeq, ignore
     plotdiffg$x = dexp
     print(do.call(plotdiffgenesnb, c(plotdiffg)))
+    print(do.call(mtext, c("Diff Genes", test)))
+}
+
+
+do.inspect.genesofinterest <- function(sc){
+    if (is.null(plotexp$n)){ ## No title, and one gene? Use gene name
+        if (length(plotexp$g) == 1){
+            plotexp$n <- plotexp$g
+        } else {
+            plotexp$n <- paste(plotexp$g, collapse=", ")
+        }
+    }
+
+    title <- paste(":", plotexp$n)
+    plotexp$n <- ""
+
+    plotexp$logsc=FALSE; plotexp$fr = FALSE
+    print(do.call(plotexpmap, c(sc, plotexp)))
+    print(do.call(mtext, c(paste("tSNE", title), test)))
+
+    plotexp$logsc=TRUE; plotexp$fr = FALSE
+    print(do.call(plotexpmap, c(sc, plotexp)))
+    print(do.call(mtext, c(paste("tSNE (Log)", title), test)))
+
+    plotexp$logsc=FALSE; plotexp$fr = TRUE
+    print(do.call(plotexpmap, c(sc, plotexp)))
+    print(do.call(mtext, c(paste("FR", title), test)))
+
+    plotexp$logsc=TRUE; plotexp$fr = TRUE
+    print(do.call(plotexpmap, c(sc, plotexp)))
+    print(do.call(mtext, c(paste("FR (Log)", title), test)))
+
+    if (!is.null(plotmarkg$samples)){
+        reg <- plotmarkg$samples
+        plotmarkg$samples <- sub("(\\_\\d+)$","", colnames(sc@ndata))
+    }
+    print(do.call(plotmarkergenes, c(sc, plotmarkg)))
 }
 
 sc <- in.rdat
@@ -83,5 +106,3 @@ if (perform.symbolmap) do.inspect.symbolmap(sc)
 if (perform.genesofinterest) do.inspect.genesofinterest(sc)
 if (perform.diffgene) do.inspect.diffgene(sc)
 dev.off()
-
-
