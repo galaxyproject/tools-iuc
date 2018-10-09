2095896e15067b96d2ffe2068eafed176d69c38f
Mehmet Tekman <mtekman89@gmail.com>
2018-10-09 22:16:18 +0000

    Updated plot labels in trajectory inspect

--- tools/raceid/scripts/trajectoryinspect.R
+++ tools/raceid/scripts/trajectoryinspect.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env R
-VERSION = "0.1"
+VERSION = "0.2"
 
 args = commandArgs(trailingOnly = T)
 
@@ -11,6 +11,13 @@ if (length(args) != 1){
 suppressWarnings(suppressPackageStartupMessages(require(RaceID)))
 suppressWarnings(suppressPackageStartupMessages(require(FateID)))
 source(args[1])
+
+test <- list()
+test$side = 3
+test$line = 2.5
+second <- test
+second$cex = 0.5
+second$line = 2.5
 
 do.trajectoryinspection.stemID <- function(ltr){
     makeBranchLink <- function(i,j,k){
@@ -29,10 +36,15 @@ do.trajectoryinspection.stemID <- function(ltr){
         head(bra$diffgenes$z, trjsid.numdiffgenes),
         file=out.diffgenes)
 
-    print(do.call(plotmap, c(bra$scl, final=TRUE, fr=FALSE)))
-    print(do.call(plotmap, c(bra$scl, final=TRUE, fr=TRUE)))
+    par(mfrow = c(2,2), cex=0.5)
     print(do.call(plotmap, c(bra$scl, final=FALSE, fr=FALSE)))
+    print(do.call(mtext, c("Initial Clusters (tSNE)", test)))
+    print(do.call(plotmap, c(bra$scl, final=TRUE, fr=FALSE)))
+    print(do.call(mtext, c("Final Clusters (tSNE)", test)))
     print(do.call(plotmap, c(bra$scl, final=FALSE, fr=TRUE)))
+    print(do.call(mtext, c("Initial Clusters (F-R)", test)))
+    print(do.call(plotmap, c(bra$scl, final=TRUE, fr=TRUE)))
+    print(do.call(mtext, c("Final Clusters (F-R)", test)))
 }
 
 do.trajectoryinspection.fateID <- function(ltr){
@@ -57,19 +69,22 @@ do.trajectoryinspection.fateID <- function(ltr){
     trjfid.plotheat$x = ps$nodes.z
     trjfid.plotheat$ypart = unique(ps$nodes)
     print(do.call(plotheatmap, c(trjfid.plotheat)))
+    print(do.call(mtext, c("Average z-score for all modules derived from SOM", test)))
     ##Plot z-score profile of each gene ordered by SOM modules:
     trjfid.plotheat$x = ps$all.z
     trjfid.plotheat$ypart = ps$nodes
     print(do.call(plotheatmap, c(trjfid.plotheat)))
+    print(do.call(mtext, c("z-score profile of each gene ordered by SOM modules", test)))
     ##Plot normalized expression profile of each gene ordered by SOM modules:
     trjfid.plotheat$x = ps$all.e
     trjfid.plotheat$ypart = ps$nodes
     print(do.call(plotheatmap, c(trjfid.plotheat)))
+    print(do.call(mtext, c("Normalized expression profile of each gene ordered by SOM modules", test)))
     ##Plot binarized expression profile of each gene (z-score < -1, -1 < z-score < 1, z-score > 1):
     trjfid.plotheat$x = ps$all.b
     trjfid.plotheat$ypart = ps$nodes
     print(do.call(plotheatmap, c(trjfid.plotheat)))
-
+    print(do.call(mtext, c("Binarized expression profile of each gene", test)))
     ## This should be written out, and passed back into the tool
     ## to perform sominspect
     return(list(fs=fs,ps=ps,y=y,fcol=fcol,nf=n$f))
@@ -95,7 +110,15 @@ do.trajectoryinspection.fateID.sominspect <- function(domo){
 
     ## The average pseudo-temporal expression profile of this group
     ## can be plotted by the function plotexpression:
+    par(mfrow = c(1,1))
+    test$cex = 1
+    second$line = 1.5
+    if (trjfidsomi$name == "Title") trjfidsomi$name = ""
     print(do.call(plotexpression, c(trjfidsomi)))
+    mess2 <- paste(c(trjfidsomi.use.genes), collapse=", ")
+    mess1 <- "Average pseudo-temporal expression profile"
+    print(do.call(mtext, c(mess1, test)))
+    print(do.call(mtext, c(mess2, second)))
 }
 
 ltr <- in.rdat
