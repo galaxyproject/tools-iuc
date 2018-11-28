747c126a9a77b51da5fbbc1d23cc6ebba31e14a0
bellenger-l <bellenger_lea_31@hotmail.fr>
2018-11-28 10:58:35 +0100

    Add seurat normalization module

---
+++ tools/seurat/seurat-normalization.R
@@ -0,0 +1,34 @@
+options( show.error.messages=F, error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
+
+# we need that to not crash galaxy with an UTF8 error on German LC settings.
+loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+
+suppressPackageStartupMessages({
+    library(Seurat)
+    library(optparse)
+})
+
+option_list <- list(
+    make_option("--data", type="character", help="Input Seurat RDS objectx"),
+    make_option("--scale", type="double", default=NULL, help="Scale factor used for normalization"),
+    make_option("--rds", type="character", help="Output Seurat RDS object")
+  )
+
+parser <- OptionParser(usage = "%prog [options] file", option_list=option_list)
+args = parse_args(parser)
+
+load(args$data)
+
+if(is.null(args$scale)){
+    scale_factor = median(seuset@meta.data$nUMI)
+} else {
+    scale_factor = args$scale
+}
+
+seuset <- NormalizeData(object = seuset, normalization.method = "LogNormalize", 
+    scale.factor = scale_factor)
+
+#Save Seurat object
+save(seuset, file = args$rds)
+
+sessionInfo()
