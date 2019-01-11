46c8ed98c652037e9071d5898a713aa06639e26d
Maria Doyle <maria.doyle@petermac.org>
2019-01-11 23:29:52 +1100

    Add plotdexseq tool

---
+++ tools/dexseq/plotdexseq.R
@@ -0,0 +1,33 @@
+## Setup R error handling to go to stderr
+options( show.error.messages=F, error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
+# we need that to not crash galaxy with an UTF8 error on German LC settings.
+Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+
+suppressPackageStartupMessages({
+    library("DEXSeq")
+    library('getopt')
+})
+
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+args <- commandArgs(trailingOnly = TRUE)
+
+#get options, using the spec as defined by the enclosed list.
+#we read the options from the default: commandArgs(TRUE).
+spec = matrix(c(
+    'rdata', 'r', 1, "character",
+    'primaryfactor', 'p', 1, "character",
+    'geneid', 'g', 1, "character",
+    'fdr', 'c', 1, "double"
+), byrow=TRUE, ncol=4);
+opt = getopt(spec);  
+
+res <- readRDS(opt$rdata)
+
+pdf("plot.pdf")
+plotDEXSeq(res, opt$geneid, FDR=opt$fdr, fitExpToVar=opt$primaryfactor,
+    norCounts=FALSE, expression=TRUE, splicing=FALSE,
+    displayTranscripts=FALSE, names=FALSE, legend=TRUE,
+    color=NULL, color.samples=NULL, transcriptDb=NULL)
+dev.off()
+
+sessionInfo()
