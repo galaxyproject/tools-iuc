82d23be279e931852e34354d882ba1decb885423
Maria Doyle <maria.doyle@petermac.org>
2019-01-13 19:49:58 +1100

    Add option to output QC metrics matrix

--- tools/scpipe/scpipe.R
+++ tools/scpipe/scpipe.R
@@ -42,6 +42,7 @@ option_list <- list(
     make_option(c("-gene_fl","--gene_fl"), type="logical", help="Remove low abundant genes"),
     make_option(c("-max_reads","--max_reads"), type="integer", help="Maximum reads processed"),
     make_option(c("-min_count","--min_count"), type="integer", help="Minimum count to keep"),
+    make_option(c("-metrics_matrix","--metrics_matrix"), type="logical", help="QC metrics matrix"),
     make_option(c("-keep_outliers","--keep_outliers"), type="logical", help="Keep outlier cells"),
     make_option(c("-report","--report"), type="logical", help="HTML report of plots"),
     make_option(c("-rdata","--rdata"), type="logical", help="Output RData file"),
@@ -149,6 +150,11 @@ if (is.null(args$keep_outliers)) {
   sce = remove_outliers(sce)
   gene_counts <- counts(sce)
   write.table(data.frame("gene_id"=rownames(gene_counts), gene_counts), file="gene_count.tsv", sep="\t", quote=FALSE, row.names=FALSE)
+}
+
+if (!is.null(args$metrics_matrix)) {
+  metrics <- colData(sce, internal=TRUE)
+  write.table(data.frame("cell_id"=rownames(metrics), metrics), file="metrics_matrix.tsv", sep="\t", quote=FALSE, row.names=FALSE)
 }
 
 if (!is.null(args$report) & (!is.null(fa_fn))) {
