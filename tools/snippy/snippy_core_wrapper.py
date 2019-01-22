e4b13c4a3e0aa46c56ce8a29da0e3fc8dc6c71ce
Dan Fornika <dfornika@gmail.com>
2019-01-21 16:44:37 -0800

    Remove debugging print statements

--- tools/snippy/snippy_core_wrapper.py
+++ tools/snippy/snippy_core_wrapper.py
@@ -29,9 +29,7 @@ def main():
 
     for indir in args.indirs.split(','):
         base_name = os.path.basename(indir)
-        sys.stderr.write(indir + '\n')
         copyfile(indir, base_name)
-        sys.stderr.write('\n'.join([indir, base_name]) + '\n')
         subprocess.Popen(['tar', '-xf', base_name])
 
     extracted_dirs = [f.path for f in os.listdir('.') if f.is_dir() ]
@@ -39,7 +37,6 @@ def main():
     for extracted_dir in extracted_dirs:
         snippy_core_command_line.append(extracted_dir)
 
-    print(snippy_core_command_line)
     subprocess.Popen(snippy_core_command_line)
 
 if __name__ == '__main__':
