cc5b56743fcce976ed74e3657a1537e24d963881
Maria Doyle <maria.doyle@petermac.org>
2019-02-17 19:16:34 +1100

    Add check that sample ids match

--- tools/limma_voom/limma_voom.R
+++ tools/limma_voom/limma_voom.R
@@ -305,6 +305,8 @@ if (!is.null(opt$filesPath)) {
     # Process factors
     if (is.null(opt$factInput)) {
             factorData <- read.table(opt$factFile, header=TRUE, sep="\t", strip.white=TRUE)
+            if(!setequal(factorData[, 1], colnames(counts)))
+                stop("Sample IDs in counts and factors files don't match")
             # order samples as in counts matrix
             factorData <- factorData[match(colnames(counts), factorData[, 1]), ]
             factors <- factorData[, -1, drop=FALSE]
