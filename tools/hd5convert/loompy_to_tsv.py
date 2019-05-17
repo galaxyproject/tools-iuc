1f38a31275cf1c5f2a525d322ffe50d825d3d9ad
Alexander OSTROVSKY <alexanderostrovsky@Caroline.local>
2019-05-17 15:30:38 -0400

    second tool, import

--- tools/hd5convert/loompy_to_tsv.py
+++ tools/hd5convert/loompy_to_tsv.py
@@ -6,77 +6,77 @@ import argparse
 
 import loompy
 
-PARSER = argparse.ArgumentParser(description="Loompy file converter flags")
-PARSER.add_argument('--VERSION', action='version', version='%(prog)s 0.1.0',
+parser = argparse.ArgumentParser(description="Loompy file converter flags")
+parser.add_argument('--version', action='version', version='%(prog)s 0.1.0',
                     help="Displays tool version")
-PARSER.add_argument("-f", "--FILE", help="loom file to import")
-ARGS = PARSER.parse_args()
+parser.add_argument("-f", "--file", help="loom file to import")
+args = parser.parse_args()
 
-FILE = ARGS.FILE
+file = args.file
 
-MATRICES = []
-ALLCOLS = []
-COLSTRINGS = []
-ALLROWS = []
+matrices = []
+allcols = []
+colstrings = []
+allrows = []
 
 # Build background info for all attributes and layers
-LOOMPYFILE = loompy.connect(FILE)
-ROW_ATTRIBUTES = LOOMPYFILE.ra.keys()  # List of row attributes
-for ROW in ROW_ATTRIBUTES:  # Each list represents rownames for row_attributes
-    C_ROW = LOOMPYFILE.ra[ROW]
-    C_ROW = [str(R) for R in C_ROW]
-    ALLROWS.append(C_ROW)
-COL_ATTRIBUTES = LOOMPYFILE.ca.keys()  # List of column attributes
-for COL in COL_ATTRIBUTES:  # Each list represents colnames for col_attributes
-    C_COL = LOOMPYFILE.ca[COL]
-    C_COL = [str(C) for C in C_COL]
-    ALLCOLS.append(C_COL)
-LAYERS = LOOMPYFILE.layers.keys()  # List of layers
-for LAYER in LAYERS:  # List with each element being a loompy layer
-    C_LAYER = LOOMPYFILE[LAYER]
-    C_LAYER = C_LAYER[:, :]
-    C_LAYER = C_LAYER.astype(str)
-    MATRICES.append(C_LAYER)
+loompyfile = loompy.connect(file)
+row_attributes = loompyfile.ra.keys()  # List of row attributes
+for row in row_attributes:  # Each list represents rownames for row_attributes
+    c_row = loompyfile.ra[row]
+    c_row = [str(r) for r in c_row]
+    allrows.append(c_row)
+col_attributes = loompyfile.ca.keys()  # List of column attributes
+for col in col_attributes:  # each list represents colnames for col_attributes
+    c_col = loompyfile.ca[col]
+    c_col = [str(c) for c in c_col]
+    allcols.append(c_col)
+layers = loompyfile.layers.keys()  # List of layers
+for layer in layers:  # List with each element being a loompy layer
+    c_layer = loompyfile[layer]
+    c_layer = c_layer[:, :]
+    c_layer = c_layer.astype(str)
+    matrices.append(c_layer)
 
 # Create column attribute output
-with open("attributes/col_attr.tsv", "w") as COLOUT:
-    COL_ATTRIBUTES = "\t".join(COL_ATTRIBUTES) + "\n"
-    COLOUT.write(COL_ATTRIBUTES)
-    for LENGTH in range(0, len(C_COL)):
-        ATTRIBUTESTRING = ""
-        for COL in ALLCOLS:
-            ATTRIBUTESTRING = ATTRIBUTESTRING + COL[LENGTH] + "\t"
-        while ATTRIBUTESTRING[-1] == "\t":
-            ATTRIBUTESTRING = ATTRIBUTESTRING[:-1]
-        COLOUT.write(ATTRIBUTESTRING)
-        COLOUT.write("\n")
+with open("attributes/col_attr.tsv", "w") as colout:
+    col_attributes = "\t".join(col_attributes) + "\n"
+    colout.write(col_attributes)
+    for length in range(0, len(c_col)):
+        attributestring = ""
+        for col in allcols:
+            attributestring = attributestring + col[length] + "\t"
+        while attributestring[-1] == "\t":
+            attributestring = attributestring[:-1]
+        colout.write(attributestring)
+        colout.write("\n")
 # Create row attribute output
-with open("attributes/row_attr.tsv", "w") as ROWOUT:
-    ROW_ATTRIBUTES = "\t".join(ROW_ATTRIBUTES) + "\n"
-    ROWOUT.write(ROW_ATTRIBUTES)
-    for LENGTH in range(0, len(C_ROW)):
-        ATTRIBUTESTRING = ""
-        for ROW in ALLROWS:
-            ATTRIBUTESTRING = ATTRIBUTESTRING + ROW[LENGTH] + "\t"
-        while ATTRIBUTESTRING[-1] == "\t":
-            ATTRIBUTESTRING = ATTRIBUTESTRING[:-1]
-        ROWOUT.write(ATTRIBUTESTRING)
-        ROWOUT.write("\n")
+with open("attributes/row_attr.tsv", "w") as rowout:
+    row_attributes = "\t".join(row_attributes) + "\n"
+    rowout.write(row_attributes)
+    for length in range(0, len(c_row)):
+        attributestring = ""
+        for row in allrows:
+            attributestring = attributestring + row[length] + "\t"
+        while attributestring[-1] == "\t":
+            attributestring = attributestring[:-1]
+        rowout.write(attributestring)
+        rowout.write("\n")
 
 # Build output files for each layer
-for X in range(0, len(LAYERS)):
+for x in range(0, len(layers)):
     # Output file name generation
-    if LAYERS[X] in LAYERS[0: X]:  # Different output names if layers have same names somehow
-        REPEATS = LAYERS[0, X].count(LAYER[X])
-        OUTPUTNAME = "output/" + LAYERS[X] + REPEATS + ".tsv"
-    elif LAYERS[X] == "":  # Empty layer name
-        OUTPUTNAME = "output/mainmatrix.tsv"
+    if layers[x] in layers[0: x]:  # Different output names if layers have same names somehow
+        repeats = layers[0, x].count(layer[x])
+        outputname = "output/" + layers[x] + repeats + ".tsv"
+    elif layers[x] == "":  # Empty layer name
+        outputname = "output/mainmatrix.tsv"
     else:
-        OUTPUTNAME = "output/" + str(LAYERS[X]) + ".tsv"  # Usual case
+        outputname = "output/" + str(layers[x]) + ".tsv"  # Usual case
 # Matrix output
-    with open(OUTPUTNAME, "w") as OUTPUTMATRIX:
-        for LINE in MATRICES[X]:
-            LINE = "\t".join(LINE)
-            LINE += "\n"
-            LINE = LINE
-            OUTPUTMATRIX.write(LINE)
+    with open(outputname, "w") as outputmatrix:
+        for line in matrices[x]:
+            line = "\t".join(line)
+            line += "\n"
+            line = line
+            outputmatrix.write(line)
