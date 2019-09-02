7022d1836ad3c6914ce5b09be24175772bd045e1
Graham Etherington <graham.etherington@earlham.ac.uk>
2019-09-02 15:01:25 +0100

    Added t-SNE plots and updated code

--- tools/scater/scater-plot-dist-scatter.R
+++ tools/scater/scater-plot-dist-scatter.R
@@ -9,6 +9,7 @@ library(workflowscriptscommon)
 library(LoomExperiment)
 library(scater)
 library(ggpubr)
+library(scales)
 
 # parse options
 
@@ -26,10 +27,17 @@ option_list = list(
     default = NA,
     type = 'character',
     help = "Path of the PDF output file to save plot to."
+  ),
+  make_option(
+    c("-l", "--log-scale"),
+    action="store", 
+    default="FALSE",
+    type = 'character',
+    help = "Plot log scale (reccommended for large datasets)."
   )
 )
 
-opt <- wsc_parse_args(option_list, mandatory = c('input_loom', 'output_plot_file'))
+opt <- wsc_parse_args(option_list, mandatory = c('input_loom', 'output_plot_file', 'log_scale'))
 
 # Check parameter values
 
@@ -51,6 +59,7 @@ cf_dm <- as.data.frame(count_feats)
 read_bins <- max(total_counts / 1e6) / 20
 feat_bins <- max(total_features) / 20
 
+
 #make the plots
 plot <- ggplot(cf_dm, aes(x=total_counts / 1e6, y=total_features)) + geom_point(shape=1) + geom_smooth() + xlab("Read count (millions)") +
    ylab("Feature count") + ggtitle("Scatterplot of reads vs features")
@@ -58,5 +67,17 @@ plot1 <- qplot(total_counts / 1e6, geom="histogram", binwidth = read_bins, ylab=
 plot2 <- qplot(total_features, geom="histogram", binwidth = feat_bins, ylab="Number of cells", xlab = "Feature counts", fill=I("darkseagreen3")) + ggtitle("Feature counts per cell")
 plot3 <- plotColData(scle, y="pct_counts_MT", x="total_features_by_counts") + ggtitle("% MT genes") + geom_point(shape=1) + theme(text = element_text(size=15)) + theme(plot.title = element_text(size=15))
 
-final_plot <- ggarrange(plot1, plot2, plot, plot3, ncol=2, nrow=2)
-ggsave(opt$output_plot_file, final_plot, device="pdf")
+
+if (identical(opt$log_scale, 'FALSE'))
+{
+  final_plot <- ggarrange(plot1, plot2, plot, plot3, ncol=2, nrow=2)
+  ggsave(opt$output_plot_file, final_plot, device="pdf")
+} else 
+  {
+    plot_log_both <- plot + scale_x_continuous(trans = 'log10') + scale_y_continuous(trans = 'log10')
+    plot1_log <- plot1 + scale_y_continuous(trans = 'log10')
+    plot2_log <- plot2 + scale_y_continuous(trans = 'log10')
+    plot3_log <- plot3 + scale_y_log10(labels=number)
+    final_plot_log <- ggarrange(plot1_log, plot2_log, plot_log_both, plot3_log, ncol=2, nrow=2)
+    ggsave(opt$output_plot_file, final_plot_log, device="pdf")
+}
