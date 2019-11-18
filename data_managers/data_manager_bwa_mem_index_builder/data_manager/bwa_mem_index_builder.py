1abc08681b21c51ee8b7ebd5e25be19b3b9a8f69
mvdbeek <m.vandenbeek@gmail.com>
2019-11-18 15:30:34 +0100

    Fix bwa_mem_data_manager on python3
    and autopep8 and add test.

--- data_managers/data_manager_bwa_mem_index_builder/data_manager/bwa_mem_index_builder.py
+++ data_managers/data_manager_bwa_mem_index_builder/data_manager/bwa_mem_index_builder.py
@@ -13,7 +13,7 @@ TWO_GB = 2**30 * 2
 DEFAULT_DATA_TABLE_NAME = "bwa_mem_indexes"
 
 
-def get_id_name( params, dbkey, fasta_description=None):
+def get_id_name(params, dbkey, fasta_description=None):
     # TODO: ensure sequence_id is unique and does not already appear in location file
     sequence_id = params['param_dict']['sequence_id']
     if not sequence_id:
@@ -27,64 +27,73 @@ def get_id_name( params, dbkey, fasta_description=None):
     return sequence_id, sequence_name
 
 
-def build_bwa_index( data_manager_dict, fasta_filename, params, target_directory, dbkey, sequence_id, sequence_name, data_table_name=DEFAULT_DATA_TABLE_NAME ):
+def build_bwa_index(data_manager_dict, fasta_filename, params, target_directory, dbkey, sequence_id, sequence_name, data_table_name=DEFAULT_DATA_TABLE_NAME):
     # TODO: allow multiple FASTA input files
-    fasta_base_name = os.path.split( fasta_filename )[-1]
-    sym_linked_fasta_filename = os.path.join( target_directory, fasta_base_name )
-    os.symlink( fasta_filename, sym_linked_fasta_filename )
+    fasta_base_name = os.path.split(fasta_filename)[-1]
+    sym_linked_fasta_filename = os.path.join(target_directory, fasta_base_name)
+    os.symlink(fasta_filename, sym_linked_fasta_filename)
     if params['param_dict']['index_algorithm'] == 'automatic':
-        if os.stat( fasta_filename ).st_size < TWO_GB:  # use 2 GB as cut off for memory vs. max of 2gb database size; this is somewhat arbitrary
+        if os.stat(fasta_filename).st_size < TWO_GB:  # use 2 GB as cut off for memory vs. max of 2gb database size; this is somewhat arbitrary
             index_algorithm = 'is'
         else:
             index_algorithm = 'bwtsw'
     else:
         index_algorithm = params['param_dict']['index_algorithm']
 
-    args = [ 'bwa', 'index', '-a', index_algorithm ]
-    args.append( sym_linked_fasta_filename )
-    proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
+    args = ['bwa', 'index', '-a', index_algorithm]
+    args.append(sym_linked_fasta_filename)
+    proc = subprocess.Popen(args=args, shell=False, cwd=target_directory)
     return_code = proc.wait()
     if return_code:
         print("Error building index.", file=sys.stderr)
-        sys.exit( return_code )
-    data_table_entry = dict( value=sequence_id, dbkey=dbkey, name=sequence_name, path=fasta_base_name )
-    _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry )
+        sys.exit(return_code)
+    data_table_entry = dict(value=sequence_id, dbkey=dbkey, name=sequence_name, path=fasta_base_name)
+    _add_data_table_entry(data_manager_dict, data_table_name, data_table_entry)
 
 
-def _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry ):
-    data_manager_dict['data_tables'] = data_manager_dict.get( 'data_tables', {} )
-    data_manager_dict['data_tables'][ data_table_name ] = data_manager_dict['data_tables'].get( data_table_name, [] )
-    data_manager_dict['data_tables'][ data_table_name ].append( data_table_entry )
+def _add_data_table_entry(data_manager_dict, data_table_name, data_table_entry):
+    data_manager_dict['data_tables'] = data_manager_dict.get('data_tables', {})
+    data_manager_dict['data_tables'][data_table_name] = data_manager_dict['data_tables'].get(data_table_name, [])
+    data_manager_dict['data_tables'][data_table_name].append(data_table_entry)
     return data_manager_dict
 
 
 def main():
     parser = optparse.OptionParser()
-    parser.add_option( '-f', '--fasta_filename', dest='fasta_filename', action='store', type="string", default=None, help='fasta_filename' )
-    parser.add_option( '-d', '--fasta_dbkey', dest='fasta_dbkey', action='store', type="string", default=None, help='fasta_dbkey' )
-    parser.add_option( '-t', '--fasta_description', dest='fasta_description', action='store', type="string", default=None, help='fasta_description' )
-    parser.add_option( '-n', '--data_table_name', dest='data_table_name', action='store', type="string", default=None, help='data_table_name' )
+    parser.add_option('-f', '--fasta_filename', dest='fasta_filename', action='store', type="string", default=None, help='fasta_filename')
+    parser.add_option('-d', '--fasta_dbkey', dest='fasta_dbkey', action='store', type="string", default=None, help='fasta_dbkey')
+    parser.add_option('-t', '--fasta_description', dest='fasta_description', action='store', type="string", default=None, help='fasta_description')
+    parser.add_option('-n', '--data_table_name', dest='data_table_name', action='store', type="string", default=None, help='data_table_name')
     (options, args) = parser.parse_args()
 
     filename = args[0]
 
-    params = loads( open( filename ).read() )
-    target_directory = params[ 'output_data' ][0]['extra_files_path']
-    os.mkdir( target_directory )
+    params = loads(open(filename).read())
+    target_directory = params['output_data'][0]['extra_files_path']
+    os.mkdir(target_directory)
     data_manager_dict = {}
 
     dbkey = options.fasta_dbkey
 
-    if dbkey in [ None, '', '?' ]:
-        raise Exception( '"%s" is not a valid dbkey. You must specify a valid dbkey.' % ( dbkey ) )
+    if dbkey in [None, '', '?']:
+        raise Exception('"%s" is not a valid dbkey. You must specify a valid dbkey.' % (dbkey))
 
-    sequence_id, sequence_name = get_id_name( params, dbkey=dbkey, fasta_description=options.fasta_description )
+    sequence_id, sequence_name = get_id_name(params, dbkey=dbkey, fasta_description=options.fasta_description)
 
     # build the index
-    build_bwa_index( data_manager_dict, options.fasta_filename, params, target_directory, dbkey, sequence_id, sequence_name, data_table_name=options.data_table_name or DEFAULT_DATA_TABLE_NAME )
+    build_bwa_index(
+        data_manager_dict,
+        options.fasta_filename,
+        params,
+        target_directory,
+        dbkey,
+        sequence_id,
+        sequence_name,
+        data_table_name=options.data_table_name or DEFAULT_DATA_TABLE_NAME
+    )
 
     # save info to json file
-    open( filename, 'wb' ).write( dumps( data_manager_dict ) )
+    open(filename, 'w').write(dumps(data_manager_dict, sort_keys=True))
 
 
 if __name__ == "__main__":
