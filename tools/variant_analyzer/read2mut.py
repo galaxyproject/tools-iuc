00d2cda9645b447795cb3c69f11bd1a2a86eb21c
Monika Heinzl <k1556322@students.jku.at>
2019-11-19 11:28:43 +0100

    fixed linting3

--- tools/variant_analyzer/read2mut.py
+++ tools/variant_analyzer/read2mut.py
@@ -697,7 +697,7 @@ def read2mut(argv):
     # sheet 2
     header_line2 = ('variant ID', 'cvrg', 'AC alt (all tiers)', 'AF  (all tiers)', 'cvrg (tiers 1.1-2.4)', 'AC alt (tiers 1.1-2.4)', 'AF (tiers 1.1-2.4)', 'AC alt (Du Novo)', 'AF (Du Novo)',
                     'tier 1.1', 'tier 1.2', 'tier 2.1', 'tier 2.2', 'tier 2.3', 'tier 2.4',
-                    'tier 3.1', 'tier 3.2', 'tier 4.1', 'tier 4.2',  'AF 1.1-1.2', 'AF 1.1-2.1', 'AF 1.1-2.2',
+                    'tier 3.1', 'tier 3.2', 'tier 4.1', 'tier 4.2', 'AF 1.1-1.2', 'AF 1.1-2.1', 'AF 1.1-2.2',
                     'AF 1.1-2.3', 'AF 1.1-2.4', 'AF 1.1-3.1', 'AF 1.1-3.2', 'AF 1.1-4.1', 'AF 1.1-4.2')
 
     ws2.write_row(0, 0, header_line2)
