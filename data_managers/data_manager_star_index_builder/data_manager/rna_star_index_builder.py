38c4817a63feabfaaf20d757025f1c509e19f438
mvdbeek <m.vandenbeek@gmail.com>
2019-11-22 10:48:35 +0100

    Fix rgrnastar requirements and test
    Uses regex for match rna_star data manager output.
    Uses yield to add star requirements, as macro is re-used in star tool.

--- data_managers/data_manager_star_index_builder/data_manager/rna_star_index_builder.py
+++ data_managers/data_manager_star_index_builder/data_manager/rna_star_index_builder.py
@@ -6,21 +6,21 @@ import json
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument( '--config-file' )
-    parser.add_argument( '--value' )
-    parser.add_argument( '--dbkey' )
-    parser.add_argument( '--name' )
-    parser.add_argument( '--subdir' )
-    parser.add_argument( '--data-table' )
-    parser.add_argument( '--with-gene-model', action='store_true' )
-    parser.add_argument( '--index-version' )
+    parser.add_argument('--config-file')
+    parser.add_argument('--value')
+    parser.add_argument('--dbkey')
+    parser.add_argument('--name')
+    parser.add_argument('--subdir')
+    parser.add_argument('--data-table')
+    parser.add_argument('--with-gene-model', action='store_true')
+    parser.add_argument('--index-version')
 
     args = parser.parse_args()
 
-    if args.dbkey in [ None, '', '?' ]:
+    if args.dbkey in [None, '', '?']:
         raise Exception(
             '"%s" is not a valid dbkey. You must specify a valid dbkey.'
-            % ( args.dbkey )
+            % (args.dbkey)
         )
 
     with_gene_model = "0"
@@ -41,7 +41,7 @@ def main():
             ]
         }
     }
-    open( args.config_file, 'w' ).write( json.dumps( data_manager_dict ) )
+    open(args.config_file, 'w').write(json.dumps(data_manager_dict, sort_keys=True))
 
 
 if __name__ == "__main__":
