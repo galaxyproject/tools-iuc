ce82f787f1f035debfc86de09a271a9ec8d27e0e
Jim Johnson <jj@umn.edu>
2019-12-04 09:19:58 -0600

    Add test case

--- data_managers/data_manager_cat/data_manager/data_manager_cat.py
+++ data_managers/data_manager_cat/data_manager/data_manager_cat.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import os.path
 import subprocess
+import sys
 import tarfile
 import tempfile
 import zipfile
@@ -42,15 +43,18 @@ def url_download(url, workdir):
     os.remove(file_path)
 
 
-def cat_prepare(install_dir):
-    cmd = ['CAT', 'prepare' '--fresh', '-q']
+def cat_prepare(install_dir, db_dir=None, tax_dir=None):
+    if db_dir and tax_dir:
+        cmd = ['CAT', 'prepare', '--existing', '-d', db_dir, '-t', tax_dir]
+    else:
+        cmd = ['CAT', 'prepare', '--fresh', '-q']
     cmd_stdout = tempfile.NamedTemporaryFile()
     cmd_stderr = tempfile.NamedTemporaryFile()
-    return_code = subprocess.call(cmd, shell=True, cwd=install_dir,
+    return_code = subprocess.call(cmd, shell=False, cwd=install_dir,
                                   stdout=cmd_stdout, stderr=cmd_stderr)
     if return_code:
-        msg = "stdout:\n%s\nstderr:\n%s" % (cmd_stdout.read().decode('utf-8'),
-                                            cmd_stderr.read().decode('utf-8'))
+        msg = "stdout:\n%s\nstderr:\n%s" % (cmd_stdout.read(),
+                                            cmd_stderr.read())
         cmd_stdout.close()
         cmd_stderr.close()
         raise Exception('Error: (%s), returncode=%s %s'
@@ -59,31 +63,41 @@ def cat_prepare(install_dir):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_file')
-    parser.add_argument('--install_path')
+    parser.add_argument('--config_file', required=True)
+    parser.add_argument('--install_path', default=None)
     parser.add_argument('--db_url', default=None)
+    parser.add_argument('--database_folder', default=None)
+    parser.add_argument('--taxonomy_folder', default=None)
     args = parser.parse_args()
-
-    if not os.path.exists(args.install_path):
-        os.makedirs(args.install_path)
-    if args.db_url:
-        url_download(args.db_url, args.install_path)
-    else:
-        cat_prepare(args.install_path)
 
     cat_path = None
     cat_db = None
     tax_db = None
-    for root, dirs, files in os.walk(args.install_path):
-        for dname in dirs:
-            if dname.endswith('CAT_database'):
-                cat_db = dname
-            elif dname.endswith('taxonomy'):
-                tax_db = dname
-        if cat_db and tax_db:
-            cat_path = root
-            break
-
+    if args.database_folder and args.taxonomy_folder:
+        cat_path = os.path.dirname(args.database_folder)
+        cat_db = os.path.basename(args.database_folder)
+        tax_db = os.path.basename(args.taxonomy_folder)
+        cat_prepare(os.getcwd(),
+                    db_dir=args.database_folder,
+                    tax_dir=args.taxonomy_folder)
+    elif not args.install_path:
+        sys.exit(1)
+    else:
+        if not os.path.exists(args.install_path):
+            os.makedirs(args.install_path)
+        if args.db_url:
+            url_download(args.db_url, args.install_path)
+        else:
+            cat_prepare(args.install_path)
+        for root, dirs, files in os.walk(args.install_path):
+            for dname in dirs:
+                if dname.endswith('CAT_database'):
+                    cat_db = dname
+                elif dname.endswith('taxonomy'):
+                    tax_db = dname
+            if cat_db and tax_db:
+                cat_path = root
+                break
     cat_dir = os.path.basename(cat_path)
     dm_dict = {}
     dm_dict['data_tables'] = dm_dict.get('data_tables', {})
