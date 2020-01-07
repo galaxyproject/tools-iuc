ff29818093a6725a14a9968b774275cbd4724d83
Lucille Delisle <lucille.delisle@epfl.ch>
2020-01-07 11:29:46 +0100

    update tests

--- tools/pygenometracks/test-data/testpyGT.sh
+++ tools/pygenometracks/test-data/testpyGT.sh
@@ -7,7 +7,7 @@ pgt --tracks test-data/test5.ini --region X:3000000-3300000 -o test-data/test_gt
 pgt --tracks test-data/test6.ini --region X:2760000-2802000 -o test-data/test_narrowPeak.png
 pgt --tracks test-data/test7.ini --region chrX:3300000-3500000 -o test-data/test_gtf_flybase_param.png
 pgt --tracks test-data/test8.ini --region chrX:3300000-3500000 -o test-data/test_ucsc_param.png
-pgt --tracks test-data/test9.ini --region X:3130000-3140000 -o test-data/test_arrowhead_zoom.png
+pgt --tracks test-data/test9.ini --region X:3133000-3138000 -o test-data/test_arrowhead_zoom.png
 pgt --tracks test-data/test10.ini --region X:3340000-3380000 -o test-data/test_middle_triangle.png
 pgt --tracks test-data/test11.ini --region chrX:3250000-3400000 -o test-data/test_TADs_bdgm.png
 
