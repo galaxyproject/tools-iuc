b0844e73f75e1a2fbd112306f653dd5d83d8ac14
Dan Fornika <dfornika@gmail.com>
2020-02-04 15:20:58 -0800

    Add test

--- tools/kma/test-data/ERR884056.res
+++ tools/kma/test-data/ERR884056.res
@@ -1,3 +1,3 @@
 #Template	Score	Expected	Template_length	Template_Identity	Template_Coverage	Query_Identity	Query_Coverage	Depth	q_value	p_value
-b0842_1     	    2018	    9024	    1233	   11.03	   11.19	   98.55	  893.48	    1.72	 4445.86	1.0e-26
-b0842_3     	   35808	     577	    1233	   99.84	  100.00	   99.84	  100.00	   29.71	34112.88	1.0e-26
+b0842_1     	    1721	    9114	    1233	   21.01	   21.25	   98.85	  470.61	    1.46	 5044.88	1.0e-26
+b0842_3     	   36084	     523	    1233	   99.84	  100.00	   99.84	  100.00	   29.96	34542.72	1.0e-26
--- tools/kma/test-data/generate_test_data.sh
+++ tools/kma/test-data/generate_test_data.sh
@@ -6,15 +6,18 @@
 gunzip b0842.fasta.gz
 
 # Take first 5 alleles to reduce size of test data
-head -n 10 b0842.fasta > ecoli_b0842_1to5.fasta
+mkdir ecoli_cgMLST
+head -n 10 b0842.fasta > ecoli_cgMLST/ecoli_b0842_1to5.fasta
 
-kma index -i ecoli_b0842_1to5.fasta -o ecoli_b0842_1to5
+kma index -k 8 -i ecoli_cgMLST/ecoli_b0842_1to5.fasta -o ecoli_cgMLST/ecoli_b0842_1to5
 
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR884/ERR884056/ERR884056_1.fastq.gz
 
-bwa index ecoli_b0842_1to5.fasta
+# Use bwa to map reads to reduced E. coli locus b0842
+# and extract only mapped reads (to reduce size of test dataset)
+bwa index ecoli_cgMLST/ecoli_b0842_1to5.fasta
 
-bwa mem ecoli_b0842_1to5.fasta ERR884056_1.fastq.gz -o ERR884056_1_ecoli_b0842_1to5.sam
+bwa mem ecoli_cgMLST/ecoli_b0842_1to5.fasta ERR884056_1.fastq.gz -o ERR884056_1_ecoli_b0842_1to5.sam
 
 samtools view ERR884056_1_ecoli_b0842_1to5.sam -bo ERR884056_1_ecoli_b0842_1to5.bam
 
