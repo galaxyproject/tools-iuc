036feef4f46b5aedabc5080c8fa4bc00d9c30d95
Marius van den Beek <m.vandenbeek@gmail.com>
2020-02-13 00:07:09 +0100

    Update and fix snpeff data managers (older builds are missing python) (#2847)

--- data_managers/data_manager_snpeff/data_manager/data_manager_snpEff_databases.py
+++ data_managers/data_manager_snpeff/data_manager/data_manager_snpEff_databases.py
@@ -9,15 +9,15 @@ import sys
 def fetch_databases(data_manager_dict, target_directory):
     if not os.path.exists(target_directory):
         os.makedirs(target_directory)
-    databases_path = os.path.join( target_directory, 'databases.out' )
+    databases_path = os.path.join(target_directory, 'databases.out')
     databases_output = open(databases_path, 'w')
     args = ['snpEff', 'databases']
     return_code = subprocess.call(args=args, shell=False, stdout=databases_output.fileno())
     if return_code:
-        sys.exit( return_code )
+        sys.exit(return_code)
     databases_output.close()
-    data_manager_dict['data_tables'] = data_manager_dict.get( 'data_tables', {} )
-    data_manager_dict['data_tables']['snpeffv_databases'] = data_manager_dict['data_tables'].get( 'snpeffv_databases', [] )
+    data_manager_dict['data_tables'] = data_manager_dict.get('data_tables', {})
+    data_manager_dict['data_tables']['snpeffv_databases'] = data_manager_dict['data_tables'].get('snpeffv_databases', [])
     data_table_entries = []
     with open(databases_path, 'r') as fh:
         for i, line in enumerate(fh):
@@ -41,16 +41,16 @@ def main():
 
     filename = args[0]
 
-    params = json.loads( open( filename ).read() )
-    target_directory = params[ 'output_data' ][0]['extra_files_path']
-    os.mkdir( target_directory )
+    params = json.loads(open(filename).read())
+    target_directory = params['output_data'][0]['extra_files_path']
+    os.mkdir(target_directory)
     data_manager_dict = {}
 
     # Create Defuse Reference Data
-    data_manager_dict = fetch_databases( data_manager_dict, target_directory)
+    data_manager_dict = fetch_databases(data_manager_dict, target_directory)
 
     # save info to json file
-    open( filename, 'wb' ).write( json.dumps( data_manager_dict ) )
+    open(filename, 'w').write(json.dumps(data_manager_dict, sort_keys=True))
 
 
 if __name__ == "__main__":
