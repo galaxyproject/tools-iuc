3a2663e8a2d8b056e041c3c9e550d8e522f44583
Dan Fornika <dfornika@gmail.com>
2020-02-24 13:10:29 -0800

    Add individual_sequences parameter

--- data_managers/data_manager_mash_sketch_builder/data_manager/mash_sketch_builder.py
+++ data_managers/data_manager_mash_sketch_builder/data_manager/mash_sketch_builder.py
@@ -27,6 +27,9 @@ def mash_sketch(mash_sketch_args, sketch_name, target_directory, data_table_name
         '-p', str(mash_sketch_args["threads"]),
         str(mash_sketch_args["fasta"]),
     ]
+
+    if mash_sketch_args["individual_sequences"]:
+        args = args + ["-i"]
     
     subprocess.check_call(['mash', 'sketch'] + args, cwd=target_directory)
 
@@ -50,6 +53,7 @@ def main():
     parser.add_argument('data_manager_json')
     parser.add_argument('--kmer-size', dest='kmer_size', type=int, default=35, help='kmer length')
     parser.add_argument('--sketch-size', dest='sketch_size', type=int, default=31, help='minimizer length')
+    parser.add_argument('--individual-sequences', dest='individual_sequences', type=boolean, action='store_true' help='Sketch individual sequences (for multi-fasta files)')
     parser.add_argument('--fasta', dest='fasta', help='Fasta file to sketch')
     parser.add_argument('--threads', dest='threads', default=1, help='threads')
     parser.add_argument('--sketch-name', dest='sketch_name', help='Name for sketch')
@@ -58,8 +62,6 @@ def main():
     data_manager_input = json.loads(open(args.data_manager_json).read())
     
     target_directory = data_manager_input['output_data'][0]['extra_files_path']
-    
-    pprint("***" + target_directory + "***" )
     
     try:
         os.mkdir( target_directory )
@@ -75,6 +77,7 @@ def main():
         "kmer_size": args.kmer_size,
         "sketch_size": args.sketch_size,
         "fasta": args.fasta,
+        "individual_sequences": args.individual_sequences,
         "threads": args.threads,
     }
 
