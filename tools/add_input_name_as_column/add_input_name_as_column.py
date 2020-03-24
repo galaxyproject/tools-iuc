169082542f576482e946002e159ca06224eea92b
Matthias Bernt <m.bernt@ufz.de>
2020-03-24 18:16:21 +0100

    add the possibility to prepend the column
    useful for txt input and ill-formatted tabular inputs
    that have a varying number of columns

--- tools/add_input_name_as_column/add_input_name_as_column.py
+++ tools/add_input_name_as_column/add_input_name_as_column.py
@@ -8,6 +8,8 @@ def Parser():
     the_parser.add_argument('--output', required=True, action="store", type=str, help="output file path")
     the_parser.add_argument('--label', required=True, action="store", type=str, help="label to add in last column")
     the_parser.add_argument('--header', action="store", type=str, help="column label for last column")
+    the_parser.add_argument('--prepend', action='store_true', default=False, help='Prepend column instead of appending' )
+
     args = the_parser.parse_args()
     return args
 
@@ -19,7 +21,11 @@ with io.open(args.input, encoding="utf-8") as input, io.open(args.output, 'w', e
     for i, line in enumerate(input):
         line = line.strip('\n')
         if (i == 0) and args.header:
-            line = "%s\t%s\n" % (line, args.header)
+            new_entry = args.header
         else:
-            line = "%s\t%s\n" % (line, args.label)
+            new_entry = args.label
+        if args.prepend:
+            line = "%s\t%s\n" % (new_entry, line)
+        else:
+            line = "%s\t%s\n" % (line, new_entry)
         output.write(line)
