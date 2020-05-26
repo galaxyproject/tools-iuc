eeff23f45898b0abd4af039ef9558b5a7fb11356
RomainDallet <romain.dallet@sb-roscoff.fr>
2020-05-26 10:50:30 +0200

    Add lastdb composite datatype

---
+++ tools/last/datatypes/last.py
@@ -0,0 +1,16 @@
+class LastDb(Text):
+    """Class for LAST database files."""
+    file_ext = 'lastdb'
+    allow_datatype_change = False
+    composite_type = 'basic'
+
+    def __init__(self, **kwd):
+        Text.__init__(self, **kwd)
+        self.add_composite_file('lastdb.bck', is_binary=True)
+        self.add_composite_file('lastdb.des', is_binary=False)  # description file
+        self.add_composite_file('lastdb.prj', is_binary=False)  # project resume
+        self.add_composite_file('lastdb.sds', is_binary=True)
+        self.add_composite_file('lastdb.ssp', is_binary=True)
+        self.add_composite_file('lastdb.suf', is_binary=True)
+        self.add_composite_file('lastdb.tis', is_binary=True)
+
