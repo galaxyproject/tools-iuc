262f0bc39c871d1ffef60c12b4f0b6a612980bd4
npinter <npinter@protonmail.com>
2020-08-13 12:35:54 +0200

    Fix failed linting

--- tools/freec/ratio2circos.py
+++ tools/freec/ratio2circos.py
@@ -1,6 +1,6 @@
 import math
-import os
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=True, default='./output/sample.bam_ratio.BedGraph', type=str)
@@ -15,6 +15,6 @@ with open(args.input) as file:
     for line in file.readlines():
         ls = line.split()
         if ls[0] != "track" and float(ls[3]) > 0:
-            log2_ratio = math.log2(float(ls[3])/args.ploidy)
+            log2_ratio = math.log2(float(ls[3]) / args.ploidy)
             with open(output, "a") as out:
                 out.write("{}\t{}\t{}\t{}\n".format(ls[0], ls[1], ls[2], log2_ratio))
