b6f2c8342d0a200d3ffcb1c4fb425923d7fe8e82
npinter <npinter@protonmail.com>
2020-08-17 16:21:08 +0200

    Add karyotype output for Circos

--- tools/freec/ratio2circos.py
+++ tools/freec/ratio2circos.py
@@ -1,20 +1,18 @@
-import argparse
 import math
-import os
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', required=True, default='./output/sample.bam_ratio.BedGraph', type=str)
-parser.add_argument('-o', '--output', required=True, default='./output/sample.bam_ratio_log2_circos.txt', type=str)
-parser.add_argument('-p', '--ploidy', required=True, default=2, type=int)
-args = parser.parse_args()
+ploidy = int(sys.argv[1])
 
-path = os.path.dirname(args.input)
-output = os.path.join(path, args.output)
-
-with open(args.input) as file:
-    for line in file.readlines():
+with open("./output/sample.bam_ratio.BedGraph") as bed:
+    for line in bed.readlines():
         ls = line.split()
         if ls[0] != "track" and float(ls[3]) > 0:
-            log2_ratio = math.log2(float(ls[3]) / args.ploidy)
-            with open(output, "a") as out:
-                out.write("{}\t{}\t{}\t{}\n".format(ls[0], ls[1], ls[2], log2_ratio))
+            log2_ratio = math.log2(float(ls[3]) / ploidy)
+            with open("./output/sample.bam_ratio_log2_circos.txt", "a") as olog2r:
+                olog2r.write("{}\t{}\t{}\t{}\n".format(ls[0], ls[1], ls[2], log2_ratio))
+
+with open("./genome.fa.fai") as fai:
+    for line in fai.readlines():
+        ls = line.split()
+        with open("./output/karyotype_circos.txt", "a") as ochr:
+            ochr.write("chr - {}\t{}\t0\t{}\t{}\n".format(ls[0], ls[0].strip("chr").lower(), ls[1], ls[0]))
