afe526d0193ee82d74e41acac26baacdc1aabc62
Björn Grüning <bjoern@gruenings.eu>
2020-08-18 14:21:17 +0200

    Update ratio2circos.py

--- tools/freec/ratio2circos.py
+++ tools/freec/ratio2circos.py
@@ -4,7 +4,7 @@ import sys
 ploidy = int(sys.argv[1])
 
 with open("./output/sample.bam_ratio.BedGraph") as bed:
-    with open("./output/sample.bam_ratio_log2_circos.txt", "a") as olog2r:
+    with open("./output/sample.bam_ratio_log2_circos.txt", "w+") as olog2r:
         for line in bed.readlines():
             ls = line.split()
             if ls[0] != "track" and float(ls[3]) > 0:
@@ -12,7 +12,7 @@ with open("./output/sample.bam_ratio.BedGraph") as bed:
                 olog2r.write("{}\t{}\t{}\t{}\n".format(ls[0], ls[1], ls[2], log2_ratio))
 
 with open("./genome.fa.fai") as fai:
-    with open("./output/karyotype_circos.txt", "a") as ochr:
+    with open("./output/karyotype_circos.txt", "w+") as ochr:
         for line in fai.readlines():
             ls = line.split()
             ochr.write("chr - {}\t{}\t0\t{}\t{}\n".format(ls[0], ls[0].strip("chr").lower(), ls[1], ls[0]))
