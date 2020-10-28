b8bd946a1c909a31368051e694342d5f62f12547
Nathan Roach <nroach2@jhu.edu>
2020-10-28 16:09:41 -0400

    updating convert_VCF_info_fields.py to address code review, exposing --pad parameter of medaka tools annotate, addressing DP vs DPSP issue by calculating other info fields off of DPSP instead of DP

--- tools/medaka/convert_VCF_info_fields.py
+++ tools/medaka/convert_VCF_info_fields.py
@@ -9,102 +9,101 @@
 
 import sys
 from math import log10
+from collections import OrderedDict
 
 from scipy.stats import fisher_exact
 
 
 def pval_to_phredqual(pval):
-    return int(round(-10. * log10(pval)))
+    return round(-10 * log10(pval))
 
 
 def parseInfoField(info):
     info_fields = info.split(';')
-    info_dict = {}
-    info_list = []  # Want to keep fields in order they appear in original VCF
+    info_dict = OrderedDict()
     for info_field in info_fields:
         code, val = info_field.split('=')
         info_dict[code] = val
-        info_list.append(code)
-    return info_dict, info_list
+    return info_dict
 
 
 def annotateVCF(in_vcf_filepath, out_vcf_filepath):
     in_vcf = open(in_vcf_filepath, 'r')
     out_vcf = open(out_vcf_filepath, 'w')
-    to_skip = set()
+    to_skip = set(['SC', 'SR'])
     for i, line in enumerate(in_vcf):
         if i == 1:
             out_vcf.write("##convert_VCF_info_fields=0.1\n")
         if line[0:2] == "##":
-            if "Number=." in line:
-                assert line[0:11] == "##INFO=<ID="
-                line = line[11:]
-                id_ = line.split(',')[0]
-                to_skip.add(id_)
-                continue
+            if line[0:11] == "##INFO=<ID=":
+                id_ = line[11:].split(',')[0]
+                if id_ in to_skip:
+                    continue
             out_vcf.write(line)
         elif line[0] == "#":
-            # out_vcf.write("##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Raw Depth\">\n") # Already annotated by medaka tools annotate
-            out_vcf.write("##INFO=<ID=AF,Number=1,Type=Float,Description=\"Allele Frequency\">\n")
-            out_vcf.write("##INFO=<ID=FAF,Number=1,Type=Float,Description=\"Forward Allele Frequency\">\n")
-            out_vcf.write("##INFO=<ID=RAF,Number=1,Type=Float,Description=\"Reverse Allele Frequency\">\n")
-            out_vcf.write("##INFO=<ID=SB,Number=1,Type=Integer,Description=\"Phred-scaled strand bias at this position\">\n")
-            out_vcf.write("##INFO=<ID=DP4,Number=4,Type=Integer,Description=\"Counts for ref-forward bases, ref-reverse, alt-forward and alt-reverse bases\">\n")
-            out_vcf.write("##INFO=<ID=AS,Number=4,Type=Integer,Description=\"Total alignment score to ref and alt allele of spanning reads by strand (ref fwd, ref rev, alt fwd, alt rev) aligned with parasail match 5, mismatch -4, open 5, extend 3\">\n")
+            out_vcf.write('##INFO=<ID=DPSPS,Number=2,Type=Integer,Description="Spanning Reads Allele Frequency By Strand">\n')
+            out_vcf.write('##INFO=<ID=AF,Number=1,Type=Float,Description="Spanning Reads Allele Frequency">\n')
+            out_vcf.write('##INFO=<ID=FAF,Number=1,Type=Float,Description="Forward Spanning Reads Allele Frequency">\n')
+            out_vcf.write('##INFO=<ID=RAF,Number=1,Type=Float,Description="Reverse Spanning Reads Allele Frequency">\n')
+            out_vcf.write('##INFO=<ID=SB,Number=1,Type=Integer,Description="Phred-scaled strand bias of spanning reads at this position">\n')
+            out_vcf.write('##INFO=<ID=DP4,Number=4,Type=Integer,Description="Counts for ref-forward bases, ref-reverse, alt-forward and alt-reverse bases in spanning reads">\n')
+            out_vcf.write('##INFO=<ID=AS,Number=4,Type=Integer,Description="Total alignment score to ref and alt allele of spanning reads by strand (ref fwd, ref rev, alt fwd, alt rev) aligned with parasail match 5, mismatch -4, open 5, extend 3">\n')
             out_vcf.write(line)
         else:
-            fields = line.strip().split('\t')
-            info_dict, info_list = parseInfoField(fields[7])
+            fields = line.split('\t')
+            info_dict = parseInfoField(fields[7])
             sr_list = [int(x) for x in info_dict["SR"].split(',')]
             sc_list = [int(x) for x in info_dict["SC"].split(',')]
             if len(sr_list) == len(sc_list):
-                print(len(sr_list))
-                variant_count = int((len(sr_list) / 2) - 1)
-                dp = int(info_dict["DP"])
-                dpf = int(info_dict["DPS"].split(',')[0])
-                dpr = int(info_dict["DPS"].split(',')[1])
-                for i in range(variant_count):
-                    ref_fwd = 0
-                    ref_rev = 1
-                    var_fwd = 2 * (i + 1)
-                    var_rev = 2 * (i + 1) + 1
-
-                    dp4 = [sr_list[ref_fwd], sr_list[ref_rev], sr_list[var_fwd], sr_list[var_rev]]
+                variant_list = fields[4].split(',')
+                dpsp = int(info_dict["DPSP"])
+                ref_fwd, ref_rev = 0, 1
+                dpspf, dpspr = (int(x) for x in info_dict["AR"].split(','))
+                for i in range(0, len(sr_list), 2):
+                    dpspf += sr_list[i]
+                    dpspr += sr_list[i + 1]
+                for j, i in enumerate(range(2, len(sr_list), 2)):
+                    dp4 = (sr_list[ref_fwd], sr_list[ref_rev], sr_list[i], sr_list[i + 1])
                     dp2x2 = [[dp4[0], dp4[1]], [dp4[2], dp4[3]]]
                     _, p_val = fisher_exact(dp2x2)
                     sb = pval_to_phredqual(p_val)
 
-                    as_ = [sc_list[ref_fwd], sc_list[ref_rev], sc_list[var_fwd], sc_list[var_rev]]
+                    as_ = (sc_list[ref_fwd], sc_list[ref_rev], sc_list[i], sc_list[i + 1])
 
                     info = []
-                    for code in info_list:
+                    for code in info_dict:
+                        if code in to_skip:
+                            continue
                         val = info_dict[code]
                         info.append("%s=%s" % (code, val))
 
-                    if dp == 0:
+                    info.append("DPSPS=%d,%d" % (dpspf, dpspr))
+
+                    if dpsp == 0:
                         info.append("AF=NaN")
                     else:
-                        af = float(dp4[2] + dp4[3]) / float(dp)
+                        af = dp4[2] + dp4[3] / dpsp
                         info.append("AF=%.6f" % (af))
-                    if dpf == 0:
+                    if dpspf == 0:
                         info.append("FAF=NaN")
                     else:
-                        faf = float(dp4[2]) / float(dpf)
+                        faf = dp4[2] / dpspf
                         info.append("FAF=%.6f" % (faf))
-                    if dpr == 0:
+                    if dpspr == 0:
                         info.append("RAF=NaN")
                     else:
-                        raf = float(dp4[3]) / float(dpr)
+                        raf = dp4[3] / dpspr
                         info.append("RAF=%.6f" % (raf))
                     info.append("SB=%d" % (sb))
-                    info.append("DP4=%s" % (','.join([str(x) for x in dp4])))
-                    info.append("AS=%s" % (','.join([str(x) for x in as_])))
+                    info.append("DP4=%d,%d,%d,%d" % (dp4))
+                    info.append("AS=%d,%d,%d,%d" % (as_))
                     new_info = ';'.join(info)
+                    fields[4] = variant_list[j]
                     fields[7] = new_info
-                    out_vcf.write("%s\n" % ("\t".join(fields)))
+                    out_vcf.write("%s" % ("\t".join(fields)))
             else:
                 print("WARNING - SR and SC are different lengths, skipping variant")
-                continue
+                print(line.strip())  # Print the line for debugging purposes
     in_vcf.close()
     out_vcf.close()
 
