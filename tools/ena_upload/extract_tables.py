12719350aa4590da8e8a9712c912dbb6c7df93bf
ieguinoa <ignacio.eguinoa@gmail.com>
2020-11-17 19:41:22 +0100

    linting flake8

--- tools/ena_upload/extract_tables.py
+++ tools/ena_upload/extract_tables.py
@@ -23,21 +23,21 @@ experiments_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'stu
                                    'library_strategy', 'library_source', 'library_selection',
                                    'library_layout', 'insert_size',
                                    'library_construction_protocol', 'platform', 'instrument_model',
-                                   'submission_date'])+ '\n')
+                                   'submission_date']) + '\n')
 runs_table = open(pathlib.Path(args.out_path) / 'runs.tsv', 'w')
 runs_table.write('\t'.join(['alias', 'status', 'accession', 'experiment_alias', 'file_name',
-                            'file_format', 'file_checksum', 'submission_date'])+ '\n')
+                            'file_format', 'file_checksum', 'submission_date']) + '\n')
 
 action = args.action
 
 dt_oobj = datetime.now(tz=None)
 timestamp = dt_oobj.strftime("%Y%m%d_%H:%M:%S")
 for study_index, study in enumerate(studies_dict):
-    study_alias = 'study_'+str(study_index)+'_'+timestamp
+    study_alias = 'study_' + str(study_index) + '_' + timestamp
     studies_table.write('\t'.join([study_alias, action, 'ENA_accession', study['title'],
                                    study['type'], study['abstract'], study['pubmed_id'],
                                    'ENA_submission_data']))
-    if "geo_location" in study['samples'][0].keys(): # sample belongs to a viral sample
+    if "geo_location" in study['samples'][0].keys():           # sample belongs to a viral sample
         samples_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'scientific_name',
                                        'taxon_id', 'sample_description', 'collection_date',
                                        'geographic_location', 'host_common_name', 'host_subject_id',
@@ -47,9 +47,9 @@ for study_index, study in enumerate(studies_dict):
     else:
         samples_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'scientific_name',
                                        'taxon_id', 'sample_description', 'submission_date']) + '\n')
-    for sample_index,sample in enumerate(study['samples']):
-        sample_alias = 'sample_'+str(sample_index)+'_'+timestamp
-        if "geo_location" in sample.keys(): # sample belongs to a viral sample
+    for sample_index, sample in enumerate(study['samples']):
+        sample_alias = 'sample_' + str(sample_index) + '_' + timestamp
+        if "geo_location" in sample.keys():  # sample belongs to a viral sample
             if sample['collector_name'] == '':
                 sample['collector_name'] = 'unknown'
             samples_table.write('\t'.join([sample_alias, action, 'ena_accession', sample['title'],
@@ -60,13 +60,13 @@ for study_index, study in enumerate(studies_dict):
                                            sample['host_sex'], sample['host_scientific_name'],
                                            sample['collector_name'],
                                            sample['collecting_institution'], sample['isolate'],
-                                           'ENA_submission_date'])+ '\n')
+                                           'ENA_submission_date']) + '\n')
         else:
             samples_table.write('\t'.join([sample_alias, action, 'ena_accession', sample['title'],
                                            sample['tax_name'], sample['tax_id'],
-                                           sample['description'], 'ENA_submission_date'])+ '\n')
-        for exp_index,exp in enumerate(sample['experiments']):
-            exp_alias = 'experiment_'+str(exp_index) + '.' + str(sample_index) + '_' + timestamp
+                                           sample['description'], 'ENA_submission_date']) + '\n')
+        for exp_index, exp in enumerate(sample['experiments']):
+            exp_alias = 'experiment_' + str(exp_index) + '.' + str(sample_index) + '_' + timestamp
             lib_alias = 'library_' + str(exp_index) + '_' + str(sample_index)
             experiments_table.write('\t'.join([exp_alias, action, 'accession_ena', exp['title'],
                                                study_alias, sample_alias, exp['experiment_design'],
@@ -81,7 +81,7 @@ for study_index, study in enumerate(studies_dict):
             for run in exp['runs']:
                 run_index += 1
                 run_alias = '.'.join(['run_' + str(run_index), str(exp_index), str(sample_index)]) \
-                        + '_' + timestamp
+                            + '_' + timestamp
                 for file_entry in run:
                     runs_table.write('\t'.join([run_alias, action, 'ena_run_accession', exp_alias,
                                                 file_entry, FILE_FORMAT, 'file_checksum',
--- tools/ena_upload/process_xlsx.py
+++ tools/ena_upload/process_xlsx.py
@@ -1,10 +1,12 @@
 import argparse
 import pathlib
 import sys
+
 import xlrd
 
 
 FILE_FORMAT = 'fastq'
+
 
 def extract_data(xl_sheet, expected_columns):
     """
@@ -20,41 +22,43 @@ def extract_data(xl_sheet, expected_columns):
             else:
                 sheet_columns[xl_sheet.cell(0, sh_col).value] = sh_col
     for col in range(len(expected_columns)):
-        assert expected_columns[col] in sheet_columns.keys(), "Expected column %s not found" %expected_columns[col]
+        assert expected_columns[col] in sheet_columns.keys(), \
+            "Expected column %s not found" % expected_columns[col]
 
     # fetch rows in a dict
     data_dict = {}
     # the first of the expected columns will be the index
     index_col = sheet_columns[expected_columns[0]]
-    ## skip first 2 rows: column names + comments rows
-    for row_id in range(2,xl_sheet.nrows):
+    # skip first 2 rows: column names + comments rows
+    for row_id in range(2, xl_sheet.nrows):
         row_dict = {}
-        for col in range(1,len(expected_columns)):
+        for col in range(1, len(expected_columns)):
             sheet_col_index = sheet_columns[expected_columns[col]]
-            row_dict[expected_columns[col]] = xl_sheet.cell(row_id,sheet_col_index).value
+            row_dict[expected_columns[col]] = xl_sheet.cell(row_id, sheet_col_index).value
         # should check for duplicate alias/ids?
         data_dict[xl_sheet.cell(row_id, index_col).value] = row_dict
     return data_dict
 
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--form',dest='xlsx_path', required=True)
-parser.add_argument('--out_dir',dest='out_path', required=True)
-parser.add_argument('--action',dest='action', required=True)
-parser.add_argument('--vir',dest='viral_submission',required=False,action='store_true')
+parser.add_argument('--form', dest='xlsx_path', required=True)
+parser.add_argument('--out_dir', dest='out_path', required=True)
+parser.add_argument('--action', dest='action', required=True)
+parser.add_argument('--vir', dest='viral_submission', required=False, action='store_true')
 args = parser.parse_args()
 
 xl_workbook = xlrd.open_workbook(args.xlsx_path)
 
-## PARSE STUDIES
+# PARSE STUDIES
 #################
 xl_sheet = xl_workbook.sheet_by_name('ENA_study')
 if xl_sheet.nrows < 3:
     raise ValueError('No entries found in studies sheet')
 studies_dict = {}
-studies_col = ['alias','title','study_type','study_abstract']
+studies_col = ['alias', 'title', 'study_type', 'study_abstract']
 studies_dict = extract_data(xl_sheet, studies_col)
 
-## PARSE SAMPLES
+# PARSE SAMPLES
 #################
 xl_sheet = xl_workbook.sheet_by_name('ENA_sample')
 if xl_sheet.nrows < 3:
@@ -68,22 +72,22 @@ else:
     samples_cols = ['alias', 'title', 'scientific_name', 'sample_description']
 samples_dict = extract_data(xl_sheet, samples_cols)
 
-## PARSE EXPERIMENTS
+# PARSE EXPERIMENTS
 #################
 xl_sheet = xl_workbook.sheet_by_name('ENA_experiment')
 if xl_sheet.nrows < 3:
     raise ValueError('No experiments found in experiments sheet')
-exp_columns = ['alias','title', 'study_alias', 'sample_alias', 'design_description', 'library_name',
+exp_columns = ['alias', 'title', 'study_alias', 'sample_alias', 'design_description', 'library_name',
                'library_strategy', 'library_source', 'library_selection', 'library_layout',
                'insert_size', 'library_construction_protocol', 'platform', 'instrument_model']
 experiments_dict = extract_data(xl_sheet, exp_columns)
 
-## PARSE RUNS SHEET
+# PARSE RUNS SHEET
 #################
 xl_sheet = xl_workbook.sheet_by_name('ENA_run')
 if xl_sheet.nrows < 3:
     raise ValueError('No entries found in runs sheet')
-run_cols = ['alias','experiment_alias','file_name','file_format']
+run_cols = ['alias', 'experiment_alias', 'file_name', 'file_format']
 runs_dict = extract_data(xl_sheet, run_cols)
 # WRITE HEADERS TO TABLES
 studies_table = open(pathlib.Path(args.out_path) / 'studies.tsv', 'w')
@@ -91,15 +95,15 @@ studies_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'study_t
                                'study_abstract', 'pubmed_id', 'submission_date']) + '\n')
 samples_table = open(pathlib.Path(args.out_path) / 'samples.tsv', 'w')
 if args.viral_submission:
-    samples_table.write('\t'.join(['alias','status','accession','title','scientific_name',
+    samples_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'scientific_name',
                                    'taxon_id', 'sample_description', 'collection date',
                                    'geographic_location', 'host_common_name', 'host_subject_id',
                                    'host_health_state', 'host_sex', 'host_scientific_name',
                                    'collector_name', 'collecting_institution', 'isolate',
                                    'submission_date']) + '\n')
 else:
-    samples_table.write('\t'.join(['alias','status','accession','title','scientific_name',
-                                   'taxon_id','sample_description','submission_date'])+ '\n')
+    samples_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'scientific_name',
+                                   'taxon_id', 'sample_description', 'submission_date']) + '\n')
 
 experiments_table = open(pathlib.Path(args.out_path) / 'experiments.tsv', 'w')
 experiments_table.write('\t'.join(['alias', 'status', 'accession', 'title', 'study_alias',
@@ -113,9 +117,7 @@ runs_table.write('\t'.join(['alias', 'status', 'accession', 'experiment_alias', 
                             'file_format', 'file_checksum', 'submission_date']) + '\n')
 action = args.action
 
-
-
-## WRITE  DICTIONARIES TO TABLE FILES
+# WRITE  DICTIONARIES TO TABLE FILES
 
 # ADD A TIMESTAMP TO THE ALIAS? SEEMS LIKE ENA REQUIRES ALL ENTRIES FOR A WEBIN TO HAVE UNIQUE IDS?
 # dt_oobj = datetime.now(tz=None)
@@ -124,7 +126,7 @@ for study_alias, study in studies_dict.items():
     # study_alias = study_alias + '_' + timestamp
     studies_table.write('\t'.join([study_alias, action, 'ENA_accession', study['title'],
                                    study['study_type'], study['study_abstract'], '',
-                                   'ENA_submission_data'])+ '\n')  ## assuming no pubmed_id
+                                   'ENA_submission_data']) + '\n')  # assuming no pubmed_id
 for sample_alias, sample in samples_dict.items():
     # sample_alias = sample_alias + '_' + timestamp
     if args.viral_submission:
@@ -138,7 +140,7 @@ for sample_alias, sample in samples_dict.items():
                                        sample['host health state'], sample['host sex'],
                                        sample['host scientific name'], sample['collector name'],
                                        sample['collecting institution'], sample['isolate'],
-                                       'ENA_submission_date'])+ '\n')
+                                       'ENA_submission_date']) + '\n')
     else:
         samples_table.write('\t'.join([sample_alias, action, 'ena_accession', sample['title'],
                                        sample['scientific_name'], 'tax_id_updated_by_ENA',
@@ -148,7 +150,7 @@ for sample_alias, sample in samples_dict.items():
         # (not listed in the samples or study dict)
         # process the experiments for this sample
         if exp['sample_alias'] == sample_alias:
-            lib_alias = 'library_'+exp_alias +'_'+ exp['sample_alias']
+            lib_alias = 'library_' + exp_alias + '_' + exp['sample_alias']
             experiments_table.write('\t'.join([exp_alias, action, 'accession_ena', exp['title'],
                                                exp['study_alias'], sample_alias,
                                                exp['design_description'], lib_alias,
@@ -157,7 +159,7 @@ for sample_alias, sample in samples_dict.items():
                                                exp['library_layout'].lower(),
                                                str(exp['insert_size']),
                                                exp['library_construction_protocol'],
-                                               exp['platform'],exp['instrument_model'],
+                                               exp['platform'], exp['instrument_model'],
                                                'submission_date_ENA']) + '\n')
             for run_alias, run in runs_dict.items():
                 if run['experiment_alias'] == exp_alias:
