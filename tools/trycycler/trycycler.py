e0f71a2681d91b20916147d2967f681e07a949e9
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2021-01-24 23:21:05 +0100

    Trycycler suite fix file size and python style

--- tools/trycycler/trycycler.py
+++ tools/trycycler/trycycler.py
@@ -1,21 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from os import listdir, path, walk
+from os import path, walk
 from sys import argv
 
 
 def cluster(output_folder):
     counter = 1
     for root, dir, files in walk(output_folder):
-        print(root)
         if root.endswith('1_contigs'):
-            output_path = path.join(output_folder, f"cluster_0{counter}.fasta")
+            output_path = path.join(output_folder , f"cluster_0{counter}.fasta")
             with open(output_path, "a") as out_cluster:
                 for fasta in files:
                     fasta_path = path.join(root, fasta)
                     fasta = open(fasta_path).read()
                     out_cluster.write(fasta)
             counter += 1
+
 
 def reconcile(input_file):
     number_cluster = [x for x in input_file[-1:0:-1] if x.isdigit()][0]
@@ -25,7 +25,7 @@ def reconcile(input_file):
             if ">" in line:
                 filename = line[1:].strip()
                 output_fasta = f"{full_path}{filename}.fasta"
-            open(output_fasta ,"a").write(line)
+            open(output_fasta , "a").write(line)
 
 
 def main():
