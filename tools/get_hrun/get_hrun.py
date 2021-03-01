2ad62d2baa77c95fbd45ea972181de84f41a810e
Nathan Roach <nroach2@jhu.edu>
2021-03-01 15:07:57 -0500

    Addressing bjoern comments on PR

--- tools/get_hrun/get_hrun.py
+++ tools/get_hrun/get_hrun.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
 import argparse
 from collections import OrderedDict
 
+import vcf
 from pyfaidx import Fasta
+from vcf.parser import _Info as VcfInfo
+
 
 parser = argparse.ArgumentParser(description='Generate report tables')
 parser.add_argument("--reference",
@@ -33,60 +37,41 @@ args = parser.parse_args()
 ref_path = args.reference
 reference = Fasta(ref_path, sequence_always_upper=True, read_ahead=1000)
 in_vcf_path = args.in_vcf
-in_vcf = open(in_vcf_path)
+in_vcf = vcf.Reader(open(in_vcf_path))
+in_vcf.infos['HRUN'] = VcfInfo(
+    'HRUN', 1, 'Integer',
+    'Homopolymer length to the right of report indel position',
+    "get_hrun", "1.0")
 out_vcf_path = args.out_vcf
-out_vcf = open(out_vcf_path, 'w')
-
-first_info = True
-for line in in_vcf:
-    if line[0] == '#':
-        if len(line) >= 2:
-            if line[0:2] == "##":
-                if len(line) >= 6:
-                    if first_info and line[0:6] == "##INFO":
-                        out_vcf.write("##INFO=<ID=HRUN,Number=1,Type=Integer,Description=\"Homopolymer length to the right of report indel position\">\n")
-                        first_info = False
-            elif first_info:  # There were no INFO lines; add one before fields header
-                out_vcf.write("##INFO=<ID=HRUN,Number=1,Type=Integer,Description=\"Homopolymer length to the right of report indel position\">\n")
-        out_vcf.write(line)
-    else:
-        fields = line.split('\t')
-        chrom = fields[0]
-        pos = int(fields[1]) - 1
-        ref = fields[3]
-        alt = fields[4]
-        if len(ref) != len(alt):  # Indel
-            info_dict = parseInfoField(fields[7])
-
-            window = 50
-            hrun = 1
-            start = pos + 2
-            end = start + window
-            base = reference[chrom][pos + 1]
-            seq_len = len(reference[chrom])
-            for i in range(start, len(reference)):
-                base2 = reference[chrom][i]
-                if base == base2:
-                    hrun += 1
-                else:
-                    break
-            # Extend to left in case not left aligned
-            for i in range(pos, -1, -1):
-                if reference[chrom][i] == base:
-                    hrun += 1
-                else:
-                    break
-            info = ["HRUN=%d" % (hrun)]
-            for code in info_dict:
-                val = info_dict[code]
-                if val == "":
-                    info.append("%s" % (code))
-                else:
-                    info.append("%s=%s" % (code, val))
-            new_info = ';'.join(info)
-            fields[7] = new_info
-            out_vcf.write("%s" % ("\t".join(fields)))
-        else:
-            out_vcf.write(line)
-in_vcf.close()
+out_vcf = vcf.Writer(open(out_vcf_path, 'w'), in_vcf)
+for record in in_vcf:
+    chrom = record.CHROM
+    pos = record.POS - 1
+    ref = record.REF
+    calc_hrun = False
+    for alt in record.ALT:
+        if len(ref) != len(alt):
+            calc_hrun = True
+    if calc_hrun:
+        window = 50
+        hrun = 1
+        start = pos + 2
+        end = start + window
+        base = reference[chrom][pos + 1]
+        seq_len = len(reference[chrom])
+        for i in range(start, len(reference)):
+            base2 = reference[chrom][i]
+            if base == base2:
+                hrun += 1
+            else:
+                break
+        # Extend to left in case not left aligned
+        for i in range(pos, -1, -1):
+            if reference[chrom][i] == base:
+                hrun += 1
+            else:
+                break
+        record.add_info('HRUN', [hrun])
+    out_vcf.write_record(record)
+# in_vcf.close()
 out_vcf.close()
