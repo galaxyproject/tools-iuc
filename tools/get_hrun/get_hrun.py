08ddbca924494cf0db5d9c25a617561cab9949b3
Nathan Roach <nroach2@jhu.edu>
2021-03-02 08:33:02 -0500

    Addressing additional comments

--- tools/get_hrun/get_hrun.py
+++ tools/get_hrun/get_hrun.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import argparse
-from collections import OrderedDict
 
 import vcf
 from pyfaidx import Fasta
@@ -18,32 +17,19 @@ parser.add_argument("--out-vcf",
                     required=True,
                     help="Filepath to vcf file to be output")
 
-
-def parseInfoField(info):
-    info_fields = info.split(';')
-    info_dict = OrderedDict()
-    for info_field in info_fields:
-        info_field_fields = info_field.split('=')
-        if len(info_field_fields) == 2:
-            code, val = info_field_fields
-        else:
-            code = info_field_fields[0]
-            val = ""
-        info_dict[code] = val
-    return info_dict
-
-
 args = parser.parse_args()
 ref_path = args.reference
 reference = Fasta(ref_path, sequence_always_upper=True, read_ahead=1000)
 in_vcf_path = args.in_vcf
-in_vcf = vcf.Reader(open(in_vcf_path))
+in_vcf_handle = open(in_vcf_path)
+in_vcf = vcf.Reader(in_vcf_handle)
 in_vcf.infos['HRUN'] = VcfInfo(
     'HRUN', 1, 'Integer',
     'Homopolymer length to the right of report indel position',
     "get_hrun", "1.0")
 out_vcf_path = args.out_vcf
-out_vcf = vcf.Writer(open(out_vcf_path, 'w'), in_vcf)
+out_vcf_handle = open(out_vcf_path, 'w')
+out_vcf = vcf.Writer(out_vcf_handle, in_vcf)
 for record in in_vcf:
     chrom = record.CHROM
     pos = record.POS - 1
@@ -73,5 +59,6 @@ for record in in_vcf:
                 break
         record.add_info('HRUN', [hrun])
     out_vcf.write_record(record)
-# in_vcf.close()
+in_vcf_handle.close()
 out_vcf.close()
+out_vcf_handle.close()
