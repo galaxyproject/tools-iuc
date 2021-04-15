607f602e4631e7b3d3b08049d200f91f66f1ba3d
Mehmet Tekman <mtekman89@gmail.com>
2021-04-15 11:46:58 +0200

    custerinspect lintr fixes except snake_case

--- tools/raceid/scripts/cluster.R
+++ tools/raceid/scripts/cluster.R
@@ -17,33 +17,35 @@ do.filter <- function(sc) {
     if (!is.null(filt.lbatch.regexes)) {
         lar <- filt.lbatch.regexes
         nn <- colnames(sc@expdata)
-        filt$LBatch <- lapply(1:length(lar), function(m) { return( nn[grep(lar[[m]], nn)] ) })
+        filt$LBatch <- lapply(seq_len(lar), function(m) {
+            return(nn[grep(lar[[m]], nn)])})
     }
 
     sc <- do.call(filterdata, c(sc, filt))
 
     ## Get histogram metrics for library size and number of features
     raw.lib <- log10(colSums(as.matrix(sc@expdata)))
-    raw.feat <- log10(colSums(as.matrix(sc@expdata)>0))
+    raw.feat <- log10(colSums(as.matrix(sc@expdata) > 0))
     filt.lib <- log10(colSums(as.matrix(getfdata(sc))))
-    filt.feat <- log10(colSums(as.matrix(getfdata(sc)>0)))
+    filt.feat <- log10(colSums(as.matrix(getfdata(sc) > 0)))
 
     if (filt.geqone) {
-        filt.feat <- log10(colSums(as.matrix(getfdata(sc)> = 1)))
+        filt.feat <- log10(colSums(as.matrix(getfdata(sc) >= 1)))
     }
 
     br <- 50
     par(mfrow = c(2, 2))
-    print(hist(raw.lib, breaks = br, main = "RawData Log10 LibSize")) # , xlim = lib.x_lim, ylim = lib.y_lim)
-    print(hist(raw.feat, breaks = br, main = "RawData Log10 NumFeat")) #, xlim = feat.x_lim, ylim = feat.y_lim)
-    print(hist(filt.lib, breaks = br, main = "FiltData Log10 LibSize")) # , xlim = lib.x_lim, ylim = lib.y_lim)
-    tmp <- hist(filt.feat, breaks = br, main = "FiltData Log10 NumFeat") # , xlim = feat.x_lim, ylim = feat.y_lim)
+    print(hist(raw.lib, breaks = br, main = "RawData Log10 LibSize"))
+    print(hist(raw.feat, breaks = br, main = "RawData Log10 NumFeat"))
+    print(hist(filt.lib, breaks = br, main = "FiltData Log10 LibSize"))
+    tmp <- hist(filt.feat, breaks = br, main = "FiltData Log10 NumFeat")
     print(tmp)
     ## required, for extracting midpoint
     unq <- unique(filt.feat)
-    if (length(unq) = = 1) {
+    if (length(unq) == 1) {
         abline(v = unq, col = "red", lw = 2)
-        text(tmp$mids, table(filt.feat)[[1]] - 100, pos = 1, paste(10^unq, "\nFeatures\nin remaining\nCells", sep = ""), cex = 0.8)
+        text(tmp$mids, table(filt.feat)[[1]] - 100, pos = 1,
+             paste(10^unq, "\nFeatures\nin remaining\nCells", sep = ""), cex = 0.8)
     }
 
     if (filt.use.ccorrect) {
@@ -76,13 +78,13 @@ do.outlier <- function(sc) {
     print(plotoutlierprobs(sc))
     ## Heatmaps
     test1 <- list()
-    test1$side = 3
-    test1$line = 0  #1 #3
+    test1$side <- 3
+    test1$line <- 0  #1 #3
 
     x <- clustheatmap(sc, final = FALSE)
-    print(do.call(mtext, c(paste("(Initial)"), test1)))  ## spacing is a hack
+    print(do.call(mtext, c(paste("(Initial)"), test1)))
     x <- clustheatmap(sc, final = TRUE)
-    print(do.call(mtext, c(paste("(Final)"), test1)))  ## spacing is a hack
+    print(do.call(mtext, c(paste("(Final)"), test1)))
     return(sc)
 }
 
@@ -97,9 +99,9 @@ do.clustmap <- function(sc) {
 mkgenelist <- function(sc) {
     ## Layout
     test <- list()
-    test$side = 4
-    test$line = -2
-    test$cex = 0.8
+    test$side <- 4
+    test$line <- -2
+    test$cex <- 0.8
 
     df <- c()
     options(cex = 1)
@@ -115,11 +117,12 @@ mkgenelist <- function(sc) {
 
         print(plotmarkergenes(sc, goi))
         buffer <- paste(rep("", 36), collapse = " ")
-        print(do.call(mtext, c(paste(buffer, "Cluster ", n), test)))  ## spacing is a hack
-        test$line = -1
-        print(do.call(mtext, c(paste(buffer, "Sig. Genes"), test)))  ## spacing is a hack
-        test$line = 0
-        print(do.call(mtext, c(paste(buffer, "(fc > ", genelist.foldchange, ")"), test)))  ## spacing is a hack
+        print(do.call(mtext, c(paste(buffer, "Cluster ", n), test)))
+        test$line <- -1
+        print(do.call(mtext, c(paste(buffer, "Sig. Genes"), test)))
+        test$line <- 0
+        print(do.call(mtext, c(paste(buffer, "(fc > ",
+                                     genelist.foldchange, ")"), test)))
     })
     write.table(df, file = out.genelist, sep = "\t", quote = F)
 }
@@ -127,13 +130,14 @@ mkgenelist <- function(sc) {
 
 writecellassignments <- function(sc) {
     dat <- sc@cluster$kpart
-    tab <- data.frame(row.names = NULL, 
-                      cells = names(dat), 
-                      cluster.initial = dat, 
-                      cluster.final = sc@cpart, 
+    tab <- data.frame(row.names = NULL,
+                      cells = names(dat),
+                      cluster.initial = dat,
+                      cluster.final = sc@cpart,
                       is.outlier = names(dat) %in% sc@out$out)
 
-    write.table(tab, file = out.assignments, sep = "\t", quote = F, row.names = F)
+    write.table(tab, file = out.assignments, sep = "\t",
+                quote = F, row.names = F)
 }
 
 
@@ -141,12 +145,19 @@ pdf(out.pdf)
 
 if (use.filtnormconf) {
     sc <- do.filter(sc)
-    message(paste(" - Source:: genes:", nrow(sc@expdata), ", cells:", ncol(sc@expdata)))
-    message(paste(" - Filter:: genes:", nrow(as.matrix(getfdata(sc))), ", cells:", ncol(as.matrix(getfdata(sc)))))
-    message(paste("         :: ", 
-                  sprintf("%.1f", 100 * nrow(as.matrix(getfdata(sc)))/nrow(sc@expdata)), "% of genes remain, ", 
-                  sprintf("%.1f", 100 * ncol(as.matrix(getfdata(sc)))/ncol(sc@expdata)), "% of cells remain"))
-    write.table(as.matrix(sc@ndata), file = out.table, col.names = NA, row.names = T, sep = "\t", quote = F)
+    message(paste(" - Source:: genes:", nrow(sc@expdata),
+                  ", cells:", ncol(sc@expdata)))
+    message(paste(" - Filter:: genes:", nrow(as.matrix(getfdata(sc))),
+                  ", cells:", ncol(as.matrix(getfdata(sc)))))
+    message(paste("         :: ",
+                  sprintf("%.1f", 100 * nrow(as.matrix(
+                                            getfdata(sc))) / nrow(sc@expdata)),
+                  "% of genes remain, ",
+                  sprintf("%.1f", 100 * ncol(as.matrix(
+                                            getfdata(sc))) / ncol(sc@expdata)),
+                  "% of cells remain"))
+    write.table(as.matrix(sc@ndata), file = out.table, col.names = NA,
+                row.names = T, sep = "\t", quote = F)
 }
 
 if (use.cluster) {
