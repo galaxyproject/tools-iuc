9a7026521c04fb85b5394a73fb640a005d324f5e
Peter van Heusden <pvh@sanbi.ac.za>
2021-04-22 21:08:49 +0200

    Fix flake8 error

--- tools/pangolin/fetch_latest_pangolearn.py
+++ tools/pangolin/fetch_latest_pangolearn.py
@@ -7,7 +7,9 @@ import tarfile
 # rely on the fact that pangolin itself uses the requests module
 import requests
 
-response = requests.get("https://api.github.com/repos/cov-lineages/pangoLEARN/releases/latest")
+response = requests.get(
+    "https://api.github.com/repos/cov-lineages/pangoLEARN/releases/latest"
+)
 if response.status_code == 200:
     details = json.loads(response.text)
     response = requests.get(details["tarball_url"])
@@ -16,10 +18,8 @@ if response.status_code == 200:
         tf = tarfile.open("pangolearn.tgz")
         pl_path = tf.next().name
         tf.extractall()
-        os.rename(pl_path + '/pangoLEARN', 'datadir')
+        os.rename(pl_path + "/pangoLEARN", "datadir")
     else:
         response.raise_for_status()
 else:
     response.raise_for_status()
-
-
