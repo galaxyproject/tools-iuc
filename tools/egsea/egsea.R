a668494149ee90ec542f12dd450ed438baf67417
Maria Doyle <maria.doyle@petermac.org>
2021-06-27 22:00:28 +0100

    Fix linting

--- tools/egsea/egsea.R
+++ tools/egsea/egsea.R
@@ -1,8 +1,8 @@
 # Code based on (and inspired by) the Galaxy limma-voom/edgeR/DESeq2 wrappers
 
-options(show.error.messages = F, error = function() { 
-    cat(geterrmessage(), file = stderr()); q("no", 1, F) 
-    })
+options(show.error.messages = F, error = function() {
+    cat(geterrmessage(), file = stderr()); q("no", 1, F)
+})
 
 # we need that to not crash galaxy with an UTF8 error on German LC settings.
 loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
