39977e50037c166723c7edc5b1e44205c67d7000
Mehmet Tekman <mtekman89@gmail.com>
2021-08-05 17:16:41 +0200

    parameterized dendrogram script, can now represent any cluster group

--- tools/music_deconvolution/scripts/dendrogram.R
+++ tools/music_deconvolution/scripts/dendrogram.R
@@ -1,34 +1,53 @@
-## 
+##
 suppressWarnings(suppressPackageStartupMessages(library(xbioc)))
 suppressWarnings(suppressPackageStartupMessages(library(MuSiC)))
 suppressWarnings(suppressPackageStartupMessages(library(reshape2)))
 suppressWarnings(suppressPackageStartupMessages(library(cowplot)))
 ## We use this script to generate a clustering dendrogram of cell types, using the prior labelling from scRNA.
 
+read_list <- function(lfile){
+    read.table(file=lfile, header=F, stringsAsFactors=F)$V1
+}
+
+
 bulk_eset = readRDS('../test-data/Mousebulkeset.rds')
 scrna_eset = readRDS('../test-data/Mousesubeset.rds')
 
-print(bulk_eset)
-print(scrna_eset)
-# Download EMTAB single cell dataset from Github
-
 ## The first user inspects the scrna dataset to find these types
-celltypes = c('Endo', 'Podo', 'PT', 'LOH', 'DCT',
-              'CD-PC', 'CD-IC', 'Fib', 'Macro',
-              'Neutro','B lymph', 'T lymph', 'NK')
-cell_type_label = 'cellType'
-cluster_type_label = 'clusterType'
+celltypes = c('Endo', 'Podo', 'PT', 'LOH', 'DCT', 'CD-PC', 'CD-IC', 'Fib', 'Macro', 'Neutro','B lymph', 'T lymph', 'NK')
+celltypes_label = 'cellType'
+clustertype_label = 'clusterType'
 samples_label = 'sampleID'
 ## We then perform bulk tissue cell type estimation with pre-grouping of cell types.
-tab <- read.table('../test-data/cluster_groups.tsv', sep="\t", header=F, row.names=1, stringsAsFactors=FALSE)
-pre_grouped_cell_types = lapply(split(tab, row.names(tab)),
-                       function(x){
-                           unlist(strsplit(x$V2, split=","))
-                       })
+## CN, list_of_cell_types, marker genes name, marker genes list
+## e.g.
+##   C1, c("Neutro"), NULL, NULL
+##   C2, c("Podo"), NULL, NULL
+##   C3, c("Endo","CD-PC","LOH","CD-IC","DCT","PT"), Epith.marker, <data>
+##   C4, c("Macro","Fib","B lymph","NK","T lymph"), Immune.marker, <data>
+data.to.use = list(
+    "C1" = list(cell.types = c("Neutro"),
+                marker.names=NULL,
+                marker.list=NULL),
+    "C2" = list(cell.types = c("Podo"),
+                marker.names=NULL,
+                marker.list=NULL),
+    "C3" = list(cell.types = c("Endo","CD-PC","LOH","CD-IC","DCT","PT"),
+                marker.names = "Epithelial",
+                marker.list = read_list("../test-data/epith.markers")),
+    "C4" = list(cell.types = c("Macro","Fib","B lymph","NK","T lymph"),
+                marker.names = "Immune",
+                marker.list = read_list("../test-data/immune.markers"))
+)
+grouped_celltypes = lapply(data.to.use, function(x){ x$cell.types })
+marker_groups = lapply(data.to.use, function(x){ x$marker.list })
+names(marker_groups) = names(data.to.use) 
+
+
 ## Perform the estimation
 ## Produce the first step information
-sub.basis = music_basis(scrna_eset, clusters = cell_type_label,
-                        samples = samples_label, 
+sub.basis = music_basis(scrna_eset, clusters = celltypes_label,
+                        samples = samples_label,
                         select.ct = celltypes)
 
 # Plot the dendrogram of design matrix and cross-subject mean of realtive abundance
@@ -45,36 +64,21 @@ hc2 <- hclust(d, method = "complete")
 # Plot the obtained dendrogram
 plot(hc2, cex = 0.6, hang = -1, main = 'Cluster log(Mean of RA)')
 
-cl.type = as.character(scrna_eset[[cell_type_label]])
+cl.type = as.character(scrna_eset[[celltypes_label]])
 
-for(cl in 1:length(pre_grouped_cell_types)){
-  cl.type[cl.type %in% pre_grouped_cell_types[[cl]]] = names(pre_grouped_cell_types)[cl]
+for(cl in 1:length(grouped_celltypes)){
+  cl.type[cl.type %in% grouped_celltypes[[cl]]] = names(grouped_celltypes)[cl]
 }
-pData(scrna_eset)[[cluster_type_label]] = factor(cl.type, levels = c(names(pre_grouped_cell_types), 'CD-Trans', 'Novel1', 'Novel2'))
+pData(scrna_eset)[[clustertype_label]] = factor(cl.type, levels = c(names(grouped_celltypes), 'CD-Trans', 'Novel1', 'Novel2'))
 
 # 13 selected cell types
-##cell_types_flattened = unlist(pre_grouped_cell_types)
+##cell_types_flattened = unlist(grouped_celltypes)
 ##cell_types_flattened
-#       C1        C2       C31       C32       C33       C34       C35       C36       C41       C42 
-# "Neutro"    "Podo"    "Endo"   "CD-PC"     "LOH"   "CD-IC"     "DCT"      "PT"   "Macro"     "Fib" 
-#      C43       C44       C45 
+#       C1        C2       C31       C32       C33       C34       C35       C36       C41       C42
+# "Neutro"    "Podo"    "Endo"   "CD-PC"     "LOH"   "CD-IC"     "DCT"      "PT"   "Macro"     "Fib"
+#      C43       C44       C45
 ##"B lymph"      "NK" "T lymph"
-
-
-
-## We load cell markers
-## This RData file provides two vectors of gene names Epith.marker and Immune.marker
-load('../test-data/IEmarkers.RData')
-##
-
-
-# We now construct the list of group marker
-IEmarkers = list(NULL, NULL, Epith.marker, Immune.marker)
-names(IEmarkers) = c('C1', 'C2', 'C3', 'C4')
-# The name of group markers should be the same as the cluster names
-
 Est.mouse.bulk = music_prop.cluster(bulk.eset = bulk_eset, sc.eset = scrna_eset,
-                                    group.markers = IEmarkers, clusters = cell_type_label,
-                                    groups = cluster_type_label, samples = samples_label,
-                                    clusters.type = pre_grouped_cell_types)
-
+                                    group.markers = marker_groups, clusters = celltypes_label,
+                                    groups = clustertype_label, samples = samples_label,
+                                    clusters.type = grouped_celltypes)
