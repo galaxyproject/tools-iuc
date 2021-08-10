e186fb05db169c3b7326757ec6e3069df283e188
Mehmet Tekman <mtekman89@gmail.com>
2021-08-10 14:01:52 +0200

    added inspect-eset tool for independent inspection (forgotton in previous commit)

--- tools/music_deconvolution/scripts/dendrogram.R
+++ tools/music_deconvolution/scripts/dendrogram.R
@@ -3,35 +3,23 @@ suppressWarnings(suppressPackageStartupMessages(library(xbioc)))
 suppressWarnings(suppressPackageStartupMessages(library(MuSiC)))
 suppressWarnings(suppressPackageStartupMessages(library(reshape2)))
 suppressWarnings(suppressPackageStartupMessages(library(cowplot)))
-## We use this script to generate a clustering dendrogram of cell types, using the prior labelling from scRNA.
+## We use this script to generate a clustering dendrogram of cell
+## types, using the prior labelling from scRNA.
 
-read_list <- function(lfile){
-    if (lfile=="None"){
+read_list <- function(lfile) {
+    if (lfile == "None") {
         return(NULL)
     }
-    return(read.table(file=lfile, header=F, stringsAsFactors=F)$V1)
+    return(read.table(file = lfile, header = FALSE,
+                      stringsAsFactors = FALSE)$V1)
 }
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 source(args[1])
 
-## bulk_eset = readRDS('../test-data/Mousebulkeset.rds')
-## scrna_eset = readRDS('../test-data/Mousesubeset.rds')
-
-## ## The first user inspects the scrna dataset to find these types
-## celltypes = c('Endo', 'Podo', 'PT', 'LOH', 'DCT', 'CD-PC', 'CD-IC', 'Fib', 'Macro', 'Neutro','B lymph', 'T lymph', 'NK')
-## ## "Endo,Podo,PT,LOH,DCT,CD-PC,CD-IC,Fib,Macro,Neutro,B lymph,T lymph,NK"
-## celltypes_label = 'cellType'
-## clustertype_label = 'clusterType'
-## samples_label = 'sampleID'
-## ## We then perform bulk tissue cell type estimation with pre-grouping of cell types.
-## ## CN, list_of_cell_types, marker genes name, marker genes list
-## ## e.g.
-## ##   C1, c("Neutro"), NULL, NULL
-## ##   C2, c("Podo"), NULL, NULL
-## ##   C3, c("Endo","CD-PC","LOH","CD-IC","DCT","PT"), Epith.marker, <data>
-## ##   C4, c("Macro","Fib","B lymph","NK","T lymph"), Immune.marker, <data>
-
+## We then perform bulk tissue cell type estimation with pre-grouping
+## of cell types: C, list_of_cell_types, marker genes name, marker
+## genes list.
 ## data.to.use = list(
 ##     "C1" = list(cell.types = c("Neutro"),
 ##                 marker.names=NULL,
@@ -46,44 +34,50 @@ source(args[1])
 ##                 marker.names = "Immune",
 ##                 marker.list = read_list("../test-data/immune.markers"))
 ## )
-grouped_celltypes = lapply(data.to.use, function(x){ x$cell.types })
-marker_groups = lapply(data.to.use, function(x){ x$marker.list })
-names(marker_groups) = names(data.to.use) 
+grouped_celltypes <- lapply(data.to.use, function(x) {
+    x$cell.types
+})
+marker_groups <- lapply(data.to.use, function(x) {
+    x$marker.list
+})
+names(marker_groups) <- names(data.to.use)
 
 
 ## Perform the estimation
 ## Produce the first step information
-sub.basis = music_basis(scrna_eset, clusters = celltypes_label,
-                        samples = samples_label,
-                        select.ct = celltypes)
+sub.basis <- music_basis(scrna_eset, clusters = celltypes_label,
+                         samples = samples_label,
+                         select.ct = celltypes)
 
-## Plot the dendrogram of design matrix and cross-subject mean of realtive abundance
+## Plot the dendrogram of design matrix and cross-subject mean of
+## realtive abundance
 par(mfrow = c(1, 2))
 d <- dist(t(log(sub.basis$Disgn.mtx + 1e-6)), method = "euclidean")
 ## Hierarchical clustering using Complete Linkage
-hc1 <- hclust(d, method = "complete" )
+hc1 <- hclust(d, method = "complete")
 ## Plot the obtained dendrogram
-plot(hc1, cex = 0.6, hang = -1, main = 'Cluster log(Design Matrix)')
+plot(hc1, cex = 0.6, hang = -1, main = "Cluster log(Design Matrix)")
 d <- dist(t(log(sub.basis$M.theta + 1e-8)), method = "euclidean")
 ## Hierarchical clustering using Complete Linkage
 hc2 <- hclust(d, method = "complete")
 ## Plot the obtained dendrogram
-pdf(file=outfile_pdf, width=8, height=8)
-plot(hc2, cex = 0.6, hang = -1, main = 'Cluster log(Mean of RA)')
+pdf(file = outfile_pdf, width = 8, height = 8)
+plot(hc2, cex = 0.6, hang = -1, main = "Cluster log(Mean of RA)")
 
-cl.type = as.character(scrna_eset[[celltypes_label]])
+cl_type <- as.character(scrna_eset[[celltypes_label]])
 
-for(cl in 1:length(grouped_celltypes)){
-  cl.type[cl.type %in% grouped_celltypes[[cl]]] = names(grouped_celltypes)[cl]
+for (cl in seq_len(length(grouped_celltypes))) {
+  cl_type[cl_type %in% grouped_celltypes[[cl]]] <- names(grouped_celltypes)[cl]
 }
-pData(scrna_eset)[[clustertype_label]] = factor(cl.type, levels = c(names(grouped_celltypes), 'CD-Trans', 'Novel1', 'Novel2'))
+pData(scrna_eset)[[clustertype_label]] <- factor(
+    cl_type, levels = c(names(grouped_celltypes),
+                        "CD-Trans", "Novel1", "Novel2"))
 
-Est.bulk = music_prop.cluster(bulk.eset = bulk_eset, sc.eset = scrna_eset,
-                              group.markers = marker_groups, clusters = celltypes_label,
-                              groups = clustertype_label, samples = samples_label,
-                              clusters.type = grouped_celltypes)
+est_bulk <- music_prop.cluster(
+    bulk.eset = bulk_eset, sc.eset = scrna_eset,
+    group.markers = marker_groups, clusters = celltypes_label,
+    groups = clustertype_label, samples = samples_label,
+    clusters.type = grouped_celltypes)
 
-write.table(Est.bulk, file=outfile_tab, quote=F, col.names=NA, sep="\t")
-##print(Est.bulk)
-
+write.table(est_bulk, file = outfile_tab, quote = F, col.names = NA, sep = "\t")
 dev.off()
--- tools/music_deconvolution/scripts/estimateprops.R
+++ tools/music_deconvolution/scripts/estimateprops.R
@@ -5,92 +5,89 @@ suppressWarnings(suppressPackageStartupMessages(library(cowplot)))
 ## We use this script to estimate the effectiveness of proportion methods
 
 ## Load Conf
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 source(args[1])
 
-##bulk_eset = readRDS('test-data/GSE50244bulkeset.rds')
-##scrna_eset = readRDS('test-data/EMTABesethealthy.rds')
-
-## celltypes_label = 'cellType'
-## samples_label = 'sampleID'
-## phenotype_factors = c('age', 'bmi', 'hba1c', 'gender')
-## celltypes = c('alpha', 'beta', 'delta', 'gamma', 'acinar', 'ductal')
-## methods = c('MuSiC', 'NNLS')
-## phenotype_gene = 'hba1c' ## *must* be in list of phenotype_factors
-## sample_groups = c('Normal', 'T2D')
-## sample_disease_group = "T2D"
-## sample_disease_group_scale = 5
-## healthy_phenotype = "Normal"
-## compare_title=""
-
 ## Estimate cell type proportions
-est.prop = music_prop(bulk.eset = bulk_eset, sc.eset = scrna_eset,
-                      clusters = celltypes_label,
-                      samples = samples_label, select.ct = celltypes, verbose = T)
+est_prop <- music_prop(
+    bulk.eset = bulk_eset, sc.eset = scrna_eset,
+    clusters = celltypes_label,
+    samples = samples_label, select.ct = celltypes, verbose = T)
 
 
 ## Show different in estimation methods
 ## Jitter plot of estimated cell type proportions
-jitter.fig = Jitter_Est(list(data.matrix(est.prop$Est.prop.weighted),
-                             data.matrix(est.prop$Est.prop.allgene)),
-                        method.name = methods, title = 'Jitter plot of Est Proportions')
+jitter.fig <- Jitter_Est(
+    list(data.matrix(est_prop$Est.prop.weighted),
+         data.matrix(est_prop$Est.prop.allgene)),
+    method.name = methods, title = "Jitter plot of Est Proportions")
 
 
 ## Make a Plot
-## A more sophisticated jitter plot is provided as below. We seperated the T2D subjects and normal
-#subjects by their HbA1c levels.
-m.prop = rbind(melt(est.prop$Est.prop.weighted),
-                        melt(est.prop$Est.prop.allgene))
+## A more sophisticated jitter plot is provided as below. We separated
+## the T2D subjects and normal subjects by their HbA1c levels.
+m_prop <- rbind(melt(est_prop$Est.prop.weighted),
+               melt(est_prop$Est.prop.allgene))
 
-colnames(m.prop) = c('Sub', 'CellType', 'Prop')
+colnames(m_prop) <- c("Sub", "CellType", "Prop")
 
-m.prop$CellType = factor(m.prop$CellType, levels = celltypes)
+m_prop$CellType <- factor(m_prop$CellType, levels = celltypes) # nolint
+m_prop$Method <- factor(rep(methods, each = 89 * 6), levels = methods) # nolint
+m_prop$HbA1c <- rep(bulk_eset$hba1c, 2 * 6) # nolint
+m_prop <- m_prop[!is.na(m_prop$HbA1c), ]
+m_prop$Disease <- factor(sample_groups[(m_prop$HbA1c > 6.5) + 1], # nolint
+                         levels = sample_groups)
 
-m.prop$Method = factor(rep(methods, each = 89*6), levels = methods)
-m.prop$HbA1c = rep(bulk_eset$hba1c, 2*6)
-m.prop = m.prop[!is.na(m.prop$HbA1c), ]
-m.prop$Disease = factor(sample_groups[(m.prop$HbA1c > 6.5)+1], levels = sample_groups)
+m_prop$D <- (m_prop$Disease ==   # nolint
+             sample_disease_group) / sample_disease_group_scale
+m_prop <- rbind(subset(m_prop, Disease == healthy_phenotype),
+               subset(m_prop, Disease != healthy_phenotype))
 
-m.prop$D = (m.prop$Disease == sample_disease_group)/sample_disease_group_scale
-m.prop = rbind(subset(m.prop, Disease == healthy_phenotype),
-               subset(m.prop, Disease != healthy_phenotype))
-
-jitter.new = ggplot(m.prop, aes(Method, Prop)) +
-  geom_point(aes(fill = Method, color = Disease, stroke = D, shape = Disease),
-             size = 2, alpha = 0.7, position = position_jitter(width=0.25, height=0)) +
-  facet_wrap(~ CellType, scales = 'free') + scale_colour_manual( values = c('white', "gray20")) +
-  scale_shape_manual(values = c(21, 24))+ theme_minimal()
+jitter.new <- ggplot(m_prop, aes(Method, Prop)) +
+    geom_point(aes(fill = Method, color = Disease, stroke = D, shape = Disease),
+               size = 2, alpha = 0.7,
+               position = position_jitter(width = 0.25, height = 0)) +
+    facet_wrap(~ CellType, scales = "free") +
+    scale_colour_manual(values = c("white", "gray20")) +
+    scale_shape_manual(values = c(21, 24)) + theme_minimal()
 
 ## Plot to compare method effectiveness
 ## Create dataframe for beta cell proportions and HbA1c levels
-m.prop.ana = data.frame(pData(bulk_eset)[rep(1:89, 2), phenotype_factors],
-                        ct.prop = c(est.prop$Est.prop.weighted[, 2],
-                                    est.prop$Est.prop.allgene[, 2]),
+m_prop_ana <- data.frame(pData(bulk_eset)[rep(1:89, 2), phenotype_factors],
+                        ct.prop = c(est_prop$Est.prop.weighted[, 2],
+                                    est_prop$Est.prop.allgene[, 2]),
                         Method = factor(rep(methods, each = 89),
                                         levels = methods))
-colnames(m.prop.ana)[1:4] = phenotype_factors
-m.prop.ana = subset(m.prop.ana, !is.na(m.prop.ana[phenotype_gene]))
-m.prop.ana$Disease = factor(sample_groups[(m.prop.ana[phenotype_gene] > 6.5) + 1], sample_groups)
-m.prop.ana$D = (m.prop.ana$Disease == sample_disease_group)/sample_disease_group_scale
+colnames(m_prop_ana)[1:4] <- phenotype_factors
+m_prop_ana <- subset(m_prop_ana, !is.na(m_prop_ana[phenotype_gene]))
+m_prop_ana$Disease <- factor(sample_groups[(  # nolint
+    m_prop_ana[phenotype_gene] > 6.5) + 1], sample_groups)
+m_prop_ana$D <- (m_prop_ana$Disease ==        # nolint
+                 sample_disease_group) / sample_disease_group_scale
 
-jitt.compare <- ggplot(m.prop.ana, aes_string(phenotype_gene, "ct.prop")) +
-    geom_smooth(method = 'lm',  se = FALSE, col = 'black', lwd = 0.25) +
-    geom_point(aes(fill = Method, color = Disease, stroke = D, shape = Disease), size = 2, alpha = 0.7) +  facet_wrap(~ Method) +
+jitt_compare <- ggplot(m_prop_ana, aes_string(phenotype_gene, "ct.prop")) +
+    geom_smooth(method = "lm",  se = FALSE, col = "black", lwd = 0.25) +
+    geom_point(aes(fill = Method, color = Disease, stroke = D, shape = Disease),
+               size = 2, alpha = 0.7) +  facet_wrap(~ Method) +
     ggtitle(compare_title) + theme_minimal() +
-    scale_colour_manual( values = c('white', "gray20")) +
+    scale_colour_manual(values = c("white", "gray20")) +
     scale_shape_manual(values = c(21, 24))
 
 
-pdf(file=outfile_pdf, width=8, height=8)
-plot_grid(jitter.fig, jitter.new, labels = 'auto', ncol=1, nrow=2)
-jitt.compare
+pdf(file = outfile_pdf, width = 8, height = 8)
+plot_grid(jitter.fig, jitter.new, labels = "auto", ncol = 1, nrow = 2)
+jitt_compare
 dev.off()
 
 ## Summary table
-for (meth in methods){
-    ##lm.beta.meth = lm(ct.prop ~ age + bmi + hba1c + gender, data = subset(m.prop.ana, Method == meth))
-    lm.beta.meth = lm(as.formula(paste("ct.prop", paste(phenotype_factors, collapse=" + "), sep=" ~ ")),
-                      data = subset(m.prop.ana, Method == meth))
+for (meth in methods) {
+    ##lm_beta_meth = lm(ct.prop ~ age + bmi + hba1c + gender, data =
+    ##subset(m_prop_ana, Method == meth))
+    lm_beta_meth <- lm(as.formula(
+        paste("ct.prop", paste(phenotype_factors, collapse = " + "),
+              sep = " ~ ")),
+        data = subset(m_prop_ana, Method == meth))
     print(paste0("Summary: ", meth))
-    capture.output(summary(lm.beta.meth), file = paste0("report_data/summ_", meth, ".txt"))
+    capture.output(summary(lm_beta_meth),
+                   file = paste0("report_data/summ_", meth, ".txt"))
 }
