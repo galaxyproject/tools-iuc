cc3e43f5bd806c41aa74b6d4489b54894a771128
Mehmet Tekman <mtekman89@gmail.com>
2021-10-07 13:59:14 +0200

    added protocol data as output, added better text detection

--- tools/music_deconvolution/scripts/inspect.R
+++ tools/music_deconvolution/scripts/inspect.R
@@ -6,17 +6,19 @@ args <- commandArgs(trailingOnly = TRUE)
 source(args[1])
 
 printout <- function(text) {
-    if (typeof(text) %in% c("list", "vector")) {
+    if (typeof(text) %in% c("list", "vector", "integer", "double", "numeric")) {
         write.table(text, file = outfile_tab, quote = F, sep = "\t",
                     col.names = NA)
     } else {
         ## text
+        print(typeof(text))
         capture.output(text, file = outfile_tab)  # nolint
     }
 }
 
-if (inspector %in% c("print", "pData", "fData", "dims", "experimentData",
-                     "exprs", "signature", "annotation", "abstract")) {
+if (inspector %in% c("print", "pData", "fData", "dims",
+                     "experimentData", "protocolData", "exprs",
+                     "signature", "annotation", "abstract")) {
     op <- get(inspector)
     tab <- op(rds_eset)
     printout(tab)
