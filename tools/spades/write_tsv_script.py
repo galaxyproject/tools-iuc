4a09024b14a20bd54ce3f84aa09b1304aa8c998a
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2021-12-19 13:42:46 +0100

    Commit suggestions Bern-Matthias

---
+++ tools/spades/write_tsv_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys, re
+
+search_str = r"^>(NODE|\S+)_(\d+)(?:_|\s)length_(\d+)_cov_(\d+\.*\d*).*\$"
+
+replace_str = r"\1_\2\t\3\t\4"
+
+cmd = re.compile(search_str)
+
+sys.stdout.write("#name\tlength\tcoverage\n")
+
+for i, line in enumerate(sys.stdin):
+    if cmd.match(line):
+        sys.stdout.write(cmd.sub(replace_str, line))
