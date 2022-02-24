ca172cb90ff36bfadfc641c7bfd49dea5673d466
Daniel Blankenberg <dan.blankenberg@gmail.com>
2022-02-24 13:04:55 -0500

    Fix bracken datamanager

--- data_managers/data_manager_build_bracken_database/data_manager/bracken_build_database.py
+++ data_managers/data_manager_build_bracken_database/data_manager/bracken_build_database.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import argparse
 import errno
 import json
 import os
 import subprocess
 import uuid
+from sys import stderr, stdout
 
 
 DATA_TABLE_NAME = "bracken_databases"
@@ -17,19 +16,34 @@ def bracken_build_database(target_directory, bracken_build_args, database_name, 
 
     database_value = str(uuid.uuid4())
 
-    database_name = database_name
+    database_kmer_name = 'database' + str(bracken_build_args['read_len']) + 'mers.kmer_distrib'
 
-    database_path = os.path.join(bracken_build_args['kraken_database'], 'database' + str(bracken_build_args['read_len']) + 'mers.kmer_distrib')
+    database_path_name = bracken_build_args['kraken_database']
+
+    database_path = target_directory
+
+    database_kmer_path = os.path.join(target_directory, database_kmer_name)
 
     if not prebuilt:
         bracken_build_args_list = [
             '-t', bracken_build_args['threads'],
             '-k', bracken_build_args['kmer_len'],
             '-l', bracken_build_args['read_len'],
-            '-d', bracken_build_args['kraken_database'],
+            '-d', database_path
         ]
 
-        subprocess.check_call(['bracken-build'] + bracken_build_args_list)
+        cp = subprocess.run(['bracken-build'] + bracken_build_args_list,
+                            capture_output=True, text=True)
+        if cp.stdout:
+            stdout.write(cp.stdout)
+        if cp.stderr:
+            stderr.write(cp.stderr)
+        cp.check_returncode()
+        if cp.stdout and 'ERROR:' in cp.stdout:
+            raise Exception("Error in calling bracken-build, but returncode is '%i'." % cp.returncode)
+    else:
+        if not os.path.exists(database_kmer_path):
+            raise Exception("Requested prebuilt database does not include .kmer_distrib file: '%s'" % database_kmer_path)
 
     data_table_entry = {
         "data_tables": {
@@ -37,7 +51,8 @@ def bracken_build_database(target_directory, bracken_build_args, database_name, 
                 {
                     "value": database_value,
                     "name": database_name,
-                    "path": database_path,
+                    "path": database_kmer_name,
+                    "db_dir": database_path_name
                 }
             ]
         }
@@ -54,13 +69,9 @@ def main():
     parser.add_argument('--read-len', dest='read_len', help='Read length')
     parser.add_argument('--kraken-db', dest='kraken_database', help='Kraken Database')
     parser.add_argument('--database-name', dest='database_name', help='Database Name')
+    parser.add_argument('--root-dir', dest='root_dir', help='Root directory for building')
     parser.add_argument('--prebuilt', action='store_true', dest='prebuilt', help='Use pre-built DB')
     args = parser.parse_args()
-
-    with open(args.data_manager_json) as fh:
-        data_manager_input = json.load(fh)
-
-    target_directory = data_manager_input['output_data'][0]['extra_files_path']
 
     if args.prebuilt:
         bracken_build_args = {
@@ -77,9 +88,9 @@ def main():
         }
 
     try:
-        os.mkdir(target_directory)
+        os.mkdir(args.root_dir)
     except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(target_directory):
+        if exc.errno == errno.EEXIST and os.path.isdir(args.root_dir):
             pass
         else:
             raise
@@ -87,7 +98,7 @@ def main():
     data_manager_output = {}
 
     data_manager_output = bracken_build_database(
-        target_directory,
+        args.root_dir,
         bracken_build_args,
         args.database_name,
         args.prebuilt,
