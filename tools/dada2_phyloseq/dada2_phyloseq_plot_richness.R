934d5329efc672e3227db05531080488da8221f1
greg <ghv2@psu.edu>
2022-02-28 10:30:49 -0500

    Attempt 1 to pass lintr

--- tools/dada2_phyloseq/dada2_phyloseq_plot_richness.R
+++ tools/dada2_phyloseq/dada2_phyloseq_plot_richness.R
@@ -1,22 +1,22 @@
 #!/usr/bin/env Rscript
 
 suppressPackageStartupMessages(library("optparse"))
-suppressPackageStartupMessages(library('phyloseq'))
-suppressPackageStartupMessages(library('tidyverse'))
+suppressPackageStartupMessages(library("phyloseq"))
+suppressPackageStartupMessages(library("tidyverse"))
 
 option_list <- list(
-    make_option(c("--sequence_table"), action="store", dest="sequence_table", help="Input sequence table"),
-    make_option(c("--taxonomy_table"), action="store", dest="taxonomy_table", help="Input taxonomy table"),
-    make_option(c("--output_richness_plot"), action="store", dest="output_richness_plot", help="Output richness plot")
+    make_option(c("--sequence_table"), action = "store", dest = "sequence_table", help = "Input sequence table"),
+    make_option(c("--taxonomy_table"), action = "store", dest = "taxonomy_table", help = "Input taxonomy table"),
+    make_option(c("--output_richness_plot"), action = "store", dest = "output_richness_plot", help = "Output richness plot")
 )
 
-parser <- OptionParser(usage="%prog [options] file", option_list=option_list);
-args <- parse_args(parser, positional_arguments=TRUE);
+parser <- OptionParser(usage = "%prog [options] file", option_list = option_list);
+args <- parse_args(parser, positional_arguments = TRUE);
 opt <- args$options;
 
 # The input sequence_table is an integer matrix
-# stored as tabular (rows=samples, columns=ASVs).
-seq_table_numeric_matrix <- data.matrix(read.table(opt$sequence_table, sep="\t"));
+# stored as tabular (rows = samples, columns = ASVs).
+seq_table_numeric_matrix <- data.matrix(read.table(opt$sequence_table, sep = "\t"));
 
 # The input taxonomy_table is a table containing
 # the assigned taxonomies exceeding the minBoot
@@ -24,7 +24,7 @@ seq_table_numeric_matrix <- data.matrix(read.table(opt$sequence_table, sep="\t")
 # to sequences, columns to taxonomic levels. NA
 # indicates that the sequence was not consistently
 # classified at that level at the minBoot threshold.
-tax_table_matrix <- as.matrix(read.table(opt$taxonomy_table, header=FALSE, sep='\t'));
+tax_table_matrix <- as.matrix(read.table(opt$taxonomy_table, header = FALSE, sep = "\t"));
 
 # Construct a tax_table object.  The rownames of
 # tax_tab must match the OTU names (taxa_names)
@@ -32,14 +32,13 @@ tax_table_matrix <- as.matrix(read.table(opt$taxonomy_table, header=FALSE, sep='
 tax_tab <- tax_table(tax_table_matrix);
 
 # Construct an otu_table object.
-otu_tab <- otu_table(seq_table_numeric_matrix, taxa_are_rows=TRUE);
+otu_tab <- otu_table(seq_table_numeric_matrix, taxa_are_rows = TRUE);
 
 # Construct a phyloseq object.
 phyloseq_obj <- phyloseq(otu_tab, tax_tab);
 
 # Start PDF device driver and plot species richness.
 dev.new();
-pdf(file=opt$output_richness_plot);
-plot_richness(phyloseq_obj, x="samples", color="samples");
+pdf(file = opt$output_richness_plot);
+plot_richness(phyloseq_obj, x = "samples", color = "samples");
 dev.off()
-
