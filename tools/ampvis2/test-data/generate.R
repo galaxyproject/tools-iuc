ec844431ebbff66930f0d093b2a23d1c5a61a740
Matthias Bernt <m.bernt@ufz.de>
2022-04-06 15:10:37 +0200

    fix R linter issues

--- tools/ampvis2/test-data/generate.R
+++ tools/ampvis2/test-data/generate.R
@@ -10,13 +10,13 @@ write.table(aalborgwwtps$tax, file = "AalborgWWTPs.tax", quote = F, sep = "\t", 
 write.table(aalborgwwtps$metadata, file = "AalborgWWTPs.tsv", quote = F, sep = "\t")
 
 # construct data for merge_ampvis2
-t<-amp_load(otutable="AalborgWWTPs.otu.csv", metadata = "AalborgWWTPs.tsv", taxonomy = "AalborgWWTPs.tax.tsv", fasta = "AalborgWWTPs.fa", tree = "AalborgWWTPs.nwk")
-> d_2010 <- amp_subset_samples(t, Period %in% "2010")
-# 60 samples and 109 OTUs have been filtered 
+t <- amp_load(otutable = "AalborgWWTPs.otu.csv", metadata = "AalborgWWTPs.tsv", taxonomy = "AalborgWWTPs.tax.tsv", fasta = "AalborgWWTPs.fa", tree = "AalborgWWTPs.nwk")
+d_2010 <- amp_subset_samples(t, Period %in% "2010")
+# 60 samples and 109 OTUs have been filtered
 # Before: 67 samples and 200 OTUs
 # After: 7 samples and 91 OTUs
-> d_2011 <- amp_subset_samples(t, Period %in% "2011")
-# 61 samples and 94 OTUs have been filtered 
+d_2011 <- amp_subset_samples(t, Period %in% "2011")
+# 61 samples and 94 OTUs have been filtered
 # Before: 67 samples and 200 OTUs
 # After: 6 samples and 106 OTUs
 
