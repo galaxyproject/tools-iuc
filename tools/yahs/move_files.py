6a4b867606ccddf0c5855f4027129fbdc313309d
Alexander OSTROVSKY <alexanderostrovsky@Alexanders-MacBook-Pro.local>
2022-06-13 07:07:28 -0700

    small fix

--- tools/yahs/move_files.py
+++ tools/yahs/move_files.py
@@ -5,7 +5,8 @@ files = os.listdir()
 for file in files:
     if "yahs_out" in file and "final" in file:
         shutil.move(file, "final_outs/"+file)
-conda info         shutil.move(file, "initial_break/"+file)
+    elif "yahs_out_init" in file:yconda 
+        shutil.move(file, "initial_break/"+file)
     elif "_break.agp" in file:
         shutil.move(file, "agp_break/"+file)
     elif ".agp" in file:
