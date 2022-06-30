d1e45bbdaca6ef2c870b857587a7fbe9eebf3a6c
Peter van Heusden <pvh@sanbi.ac.za>
2022-06-30 20:01:24 +0200

    Add support for pangolin_constellations data table

--- data_managers/data_manager_pangolin_data/data_manager/pangolin_data_dm.py
+++ data_managers/data_manager_pangolin_data/data_manager/pangolin_data_dm.py
@@ -9,12 +9,13 @@ import shutil
 import subprocess
 import sys
 import tempfile
+from typing import Generator, List
 
 import requests
 from packaging import version
 
 
-def parse_date(d):
+def parse_date(d: str) -> datetime.datetime:
     # Parses the publication date from the GitHub API or user input into a datetime object.
     date = None
     try:
@@ -25,11 +26,12 @@ def parse_date(d):
 
 
 def get_model_list(
-    existing_release_tags,
-    url="https://api.github.com/repos/cov-lineages/pangolin-data/releases"
-):
+    existing_release_tags: List[str],
+    package: str
+) -> Generator[dict, None, None]:
     page_num = 0
     while True:
+        url = f"https://api.github.com/repos/cov-lineages/{package}/releases"
         page_num += 1
         response = requests.get(url + f'?page={page_num}')
         if response.status_code == 200:
@@ -52,9 +54,10 @@ def get_model_list(
             response.raise_for_status()
 
 
-def filter_by_date(existing_release_tags, start_date=None, end_date=None):
+def filter_by_date(existing_release_tags: List[str], package_name: str,
+                   start_date: datetime.datetime=None, end_date: datetime.datetime=None) -> List[dict]:
     ret = []
-    for release in get_model_list(existing_release_tags):
+    for release in get_model_list(existing_release_tags, package_name):
         if start_date and release["date"] < start_date:
             break
         if not end_date or release["date"] <= end_date:
@@ -63,9 +66,10 @@ def filter_by_date(existing_release_tags, start_date=None, end_date=None):
     return ret
 
 
-def filter_by_version(existing_release_tags, start_version, end_version):
+def filter_by_version(existing_release_tags: List[str],
+                      package_name: str, start_version: str, end_version: str) -> List[dict]:
     ret = []
-    for release in get_model_list(existing_release_tags):
+    for release in get_model_list(existing_release_tags, package_name):
         if start_version is not None and version.parse(release["tag_name"]) < version.parse(start_version):
             # we can stop looking because releases are sorted by version, from newest to oldest
             break
@@ -74,9 +78,10 @@ def filter_by_version(existing_release_tags, start_version, end_version):
     return ret
 
 
-def download_and_unpack(dependency, release, output_directory):
+def download_and_unpack(dependency: str, release: str, output_directory: str) -> pathlib.Path:
     url = f"git+https://github.com/cov-lineages/{dependency}.git@{release}"
     dependency_package_name = dependency.replace('-', '_')
+    print(dependency, dependency_package_name, "release", release, "output_directory", output_directory)
     output_path = pathlib.Path(output_directory) / dependency_package_name / release
     print("output path is", output_path, "release is", release)
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -90,7 +95,7 @@ def download_and_unpack(dependency, release, output_directory):
     return output_path
 
 
-def comma_split(args):
+def comma_split(args: str) -> List[str]:
     return args.split(",")
 
 
@@ -106,45 +111,72 @@ if __name__ == "__main__":
     parser.add_argument("--overwrite", default=False, action="store_true")
     parser.add_argument('--known_revisions', type=comma_split)
     parser.add_argument("datatable_name")
-    parser.add_argument("galaxy_datamanager_filename")
+    parser.add_argument("datatable_cache_filename")
+    parser.add_argument("galaxy_config")
     args = parser.parse_args()
+
+    if args.datatable_name == 'pangolin_data':
+        package_name = 'pangolin-data'
+        min_version_key = 'min_pangolin_version'
+        min_version = '4'
+    elif args.datatable_name == 'pangolin_constellations':
+        package_name = 'constellations'
+        min_version_key = 'min_scorpio_version'
+        min_version = '0'
+    else:
+        sys.exit(f"Unknown data table {args.datatable_name}")
 
     if args.testmode:
         if args.start_version is not None:
-            releases = filter_by_version([], args.start_version, args.end_version)
+            releases = filter_by_version([], package_name, args.start_version, args.end_version)
         else:
-            releases = filter_by_date([], start_date=args.start_date, end_date=args.end_date)
+            releases = filter_by_date([], package_name, start_date=args.start_date, end_date=args.end_date)
         for release in releases:
             print(release["tag_name"], release["tarball_url"].split("/")[-1], release["date"])
         sys.exit(0)
 
-    with open(args.galaxy_datamanager_filename) as fh:
+    with open(args.galaxy_config) as fh:
         config = json.load(fh)
 
     output_directory = config.get("output_data", [{}])[0].get("extra_files_path", None)
-    data_manager_dict = {}
-    data_manager_dict["data_tables"] = config.get("data_tables", {})
-    data_manager_dict["data_tables"][args.datatable_name] = data_manager_dict[
-        "data_tables"
-    ].get(args.datatable_name, [])
 
+    try:
+        with open(args.datatable_cache_filename) as fh:
+            data_manager_dict = json.load(fh)
+    except IOError:
+        # on the first run this file doesn't exist
+        data_manager_dict = {}
+
+    if 'data_tables' in data_manager_dict:
+        if args.datatable_name not in data_manager_dict['data_tables']:
+            # got a data_tables entry, probably from a previous run of this script,
+            # but no entry for this specific data table
+            data_manager_dict['data_tables'][args.datatable_name] = []
+    else:
+        # got no entry for data tables, start from scratch
+        data_manager_dict = {"data_tables": {args.datatable_name: []}}
+
+    print("DT now", data_manager_dict)
     # known-revisions is populated from the Galaxy `pangolin_data` data table by the wrapper
     if args.known_revisions is not None:
         existing_release_tags = set(args.known_revisions)
     else:
         existing_release_tags = set()
     if args.latest:
-        latest_release = next(get_model_list([]))
+        latest_release = next(get_model_list([], package_name))
         if latest_release["tag_name"] in existing_release_tags:
             releases = []
         else:
             releases = [latest_release]
     elif args.start_version is not None or args.end_version is not None:
         releases = filter_by_version(existing_release_tags,
+                                     package_name,
                                      args.start_version, args.end_version)
     else:
         releases = filter_by_date(
-            existing_release_tags, start_date=args.start_date, end_date=args.end_date
+            existing_release_tags,
+            package_name,
+            start_date=args.start_date, end_date=args.end_date
         )
     releases_to_download = [
         release
@@ -154,22 +186,21 @@ if __name__ == "__main__":
     for release in releases_to_download:
 
         fname = download_and_unpack(
-            "pangolin-data",
+            package_name,
             release['tag_name'],
             output_directory
         )
         if fname is not None:
             data_manager_dict["data_tables"][args.datatable_name].append(
-                dict(
-                    value=release["tag_name"],
-                    description=release["name"],
-                    # NOTE: this needs to be updated if there is a new version of pangolin-data that breaks pangolin
-                    min_pangolin_version="4",
-                    path=str(output_directory / fname)
-                )
+                {
+                    'value': release["tag_name"],
+                    'description': release["name"],
+                    min_version_key: min_version,
+                    'path': str(output_directory / fname)
+                }
             )
     data_manager_dict["data_tables"][args.datatable_name].sort(
         key=operator.itemgetter("value"), reverse=True
     )
-    with open(args.galaxy_datamanager_filename, "w") as fh:
+    with open(args.datatable_cache_filename, "w") as fh:
         json.dump(data_manager_dict, fh, indent=2, sort_keys=True)
