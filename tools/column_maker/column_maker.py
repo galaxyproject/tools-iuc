0edf8c24b34ec897b4f7d537b6d01b7fe7acb9a4
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2022-07-20 17:24:14 +0200

    Put column_maker on steroids
    Enables multiple expressions to be computed sequentially on every input
    data row. In addition, the resulting columns can now not only be
    appended to existing columns, but also be inserted at arbitrary
    positions or be used to replace any existing column.

--- tools/column_maker/column_maker.py
+++ tools/column_maker/column_maker.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 """
-This tool takes a tab-delimited textfile as input and creates another column in
-the file which is the result of a computation performed on every row in the
+This tool takes a tab-delimited textfile as input and creates new columns in
+the file which are the result of a computation performed on every row in the
 original file. The tool will skip over invalid lines within the file,
-informing the user about the number of lines skipped.
+informing the user about the number of lines skipped. It does not change the
+formatting of any original, retained columns.
 """
 
 import argparse
-import json
+import enum
 import re
 import sys
 # Functions that may be used in the compute expression
@@ -20,143 +21,237 @@ from math import (  # noqa: F401
     sqrt,
 )
 
-from numpy import format_float_positional  # noqa: F401
+from numpy import format_float_positional
+
+
+class Mode(enum.Enum):
+    APPEND = ''
+    INSERT = 'I'
+    REPLACE ='R'
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input', type=argparse.FileType('r'), help="input file")
-parser.add_argument('output', type=argparse.FileType('wt'), help="output file")
-parser.add_argument('cond', nargs='?', type=str, help="expression")
-parser.add_argument('columns', nargs='?', type=int, help="number of columns")
-parser.add_argument('column_types', nargs='?', type=str, help="comma separated list of column types")
-parser.add_argument('--round', action="store_true",
-                    help="round result")
-parser.add_argument('--avoid_scientific_notation', action="store_true",
-                    help="avoid scientific notation")
-parser.add_argument('--header_new_column_name', default=None, type=str,
-                    help="First line of input is a header line with column "
-                         "names and this should become the name of the new "
-                         "column")
-parser.add_argument('--load_json', default=None, type=argparse.FileType('r'),
-                    help="overwrite parsed arguments from json file")
+parser.add_argument('input', type=str, help='input file')
+parser.add_argument('output', type=str, help='output file')
+parser.add_argument(
+    '-t', '--column-types', nargs='?', required=True,
+    help='A comma-separated list of column types in the input file'
+)
+parser.add_argument(
+    '--avoid-scientific-notation', action='store_true',
+    help='avoid scientific notation'
+)
+parser.add_argument(
+    '--header', action='store_true',
+    help='The input has a header line with column names. '
+         'Expressions must specify names of newly calculated columns.'
+)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-e', '--expr', nargs='*', type=str, help='Expression(s)')
+group.add_argument(
+    '-f', '--file', type=str,
+    help='File to read expressions from (mutually exclusive with -e)'
+)
 args = parser.parse_args()
 
-argparse_dict = vars(args)
-if args.load_json:
-    json_dict = json.load(args.load_json)
-    argparse_dict.update(json_dict)
-
-fh = argparse_dict['input']
-out = argparse_dict['output']
-expr = argparse_dict['cond']
-round_result = argparse_dict['round']
-avoid_scientific_notation = argparse_dict['avoid_scientific_notation']
-
-if argparse_dict['header_new_column_name'] is not None:
-    header_line = fh.readline().strip('\n')
-    out.write(
-        '{0}\t{1}\n'.format(
-            header_line, argparse_dict['header_new_column_name']
+if not args.column_types:
+    with open(args.input) as fh:
+        if not fh.readline():
+            # Generally, the input must have at least one column to be
+            # considered tabular, but empty files are ok and should produce
+            # empty output.
+            with open(args.output, 'w') as out:
+                pass
+            sys.exit()
+        sys.exit(
+            "Missing column types. "
+            "In Galaxy, click the pencil icon on the history item and "
+            "select the Auto-detect option to correct it.  "
+            "This tool can only be used with tab-delimited data."
         )
-    )
-try:
-    in_columns = int(argparse_dict['columns'])
-    if in_columns < 1:
-        # To be considered tabular, data must have at least one column.
-        raise ValueError
-except Exception:
-    if not fh.readline():
-        # empty file content is ok and should produce empty output
-        out.close()
-        sys.exit()
-    sys.exit("Missing or invalid 'columns' metadata value, click the pencil icon in the history item and select the Auto-detect option to correct it.  This tool can only be used with tab-delimited data.")
-try:
-    in_column_types = argparse_dict['column_types'].split(',')
-except Exception:
-    sys.exit("Missing or invalid 'column_types' metadata value, click the pencil icon in the history item and select the Auto-detect option to correct it.  This tool can only be used with tab-delimited data.")
-if len(in_column_types) != in_columns:
-    sys.exit("The 'columns' metadata setting does not conform to the 'column_types' metadata setting, click the pencil icon in the history item and select the Auto-detect option to correct it.  This tool can only be used with tab-delimited data.")
 
-operators = 'is|not|or|and'
-builtin_and_math_functions = 'abs|all|any|bin|chr|cmp|complex|divmod|float|bool|hex|int|len|long|max|min|oct|ord|pow|range|reversed|round|sorted|str|sum|type|unichr|unicode|log|log10|exp|sqrt|ceil|floor'
-string_and_list_methods = [name for name in dir('') + dir([]) if not name.startswith('_')]
-whitelist = r"^([c0-9\+\-\*\/\(\)\.\'\"><=,:! ]|%s|%s|%s)*$" % (operators, builtin_and_math_functions, '|'.join(string_and_list_methods))
-if not re.compile(whitelist).match(expr):
-    sys.exit("Invalid expression")
-if avoid_scientific_notation:
-    expr = "format_float_positional(%s)" % expr
+in_column_types = [t.strip() for t in args.column_types.split(',')]
+in_columns = len(in_column_types)
 
-# Prepare the column variable names and wrappers for column data types
+# Prepare initial column variable names and type cast representations
+# for column data types
 cols, type_casts = [], []
-for col in range(1, in_columns + 1):
-    col_name = "c%d" % col
+for n, col_type in enumerate(in_column_types, start=1):
+    col_name = "c%d" % n
     cols.append(col_name)
-    col_type = in_column_types[col - 1].strip()
-    if not round_result and col_type == 'int':
-        col_type = 'float'
     type_cast = "%s(%s)" % (col_type, col_name)
     type_casts.append(type_cast)
 
 col_str = ', '.join(cols)    # 'c1, c2, c3, c4'
 type_cast_str = ', '.join(type_casts)  # 'str(c1), int(c2), int(c3), str(c4)'
-assign = "%s = line.split('\\t')" % col_str
-if len(cols) == 1:
-    # Single column, unpacking by assignment won't work
-    assign += '[0]'
-wrap = "%s = %s" % (col_str, type_cast_str)
+# Define lambda for type-casting of original row fields
+try:
+    cast_types = eval('lambda %s: [%s]' % (col_str, type_cast_str))
+except Exception as e:
+    sys.exit(
+        'While parsing column types, the following problem occured: "%s"'
+        % e
+    )
+
+# Get and parse expressions
+if args.file:
+    expr = []
+    with open(args.file) as i:
+        for line in i:
+            line = line.strip()
+            if line:
+                expr.append(line)
+else:
+    expr = args.expr
+
+
+operators = 'is|not|or|and'
+builtin_and_math_functions = 'abs|all|any|bin|chr|cmp|complex|divmod|float|bool|hex|int|len|long|max|min|oct|ord|pow|range|reversed|round|sorted|str|sum|type|unichr|unicode|log|log10|exp|sqrt|ceil|floor'
+string_and_list_methods = [name for name in dir('') + dir([]) if not name.startswith('_')]
+whitelist = r"^([c0-9\+\-\*\/\(\)\.\'\"><=,:! ]|%s|%s|%s)*$" % (operators, builtin_and_math_functions, '|'.join(string_and_list_methods))
+valid_pat = re.compile(whitelist)
+ops = []
+num_cols = in_columns
+for e in expr:
+    parts = e.split(';')
+    if len(parts) != 3:
+        sys.exit(
+            'Invalid compute instruction: "%s".  '
+            'Required format: EXPR;[COL_ADD_SPEC];[COL_NAME]' % e
+        )
+    if not valid_pat.match(parts[0]):
+        sys.exit('Invalid expression: "%s"' % parts[0])
+    try:
+        exp_lambda = eval('lambda %s: %s' % (col_str, parts[0]))
+    except Exception as e:
+        if str(e).startswith('invalid syntax'):
+            sys.exit(
+                'Expression "%s" caused a syntax error during parsing.'
+                % parts[0]
+            )
+        else:
+            sys.exit(
+                'While parsing expression "%s" the following problem occured: '
+                '"%s"' % (parts[0], str(e))
+            )
+    try:
+        new_col_idx = int(parts[1][:-1] or '0') - 1
+    except ValueError:
+        sys.exit(
+            'COL_ADD_SPECS need to start with a (1-based) column index. '
+            'Could not parse a column index from "%s"' % parts[1]
+        )
+    try:
+        mode = Mode(parts[1][-1:])
+    except ValueError:
+        sys.exit(
+            'COL_ADD_SPECS need to end in a single-character mode identifier '
+            '("A", "I", or "R").  '
+            'Could not parse a valid identifier from "%s"' % parts[1]
+        )
+    if mode is Mode.APPEND and new_col_idx != -1:
+        sys.exit(
+            '"%s" combines Append mode with a column index.  '
+            'Please specify mode "A" without a leading column index.'
+            % parts[1]
+        )
+    if mode is Mode.REPLACE:
+        if new_col_idx < 0 or new_col_idx >= num_cols:
+            sys.exit(
+                'Cannot replace the contents of column %d as specified by '
+                'instruction "%s".  No such column at this point of the '
+                'computation' % (new_col_idx + 1, e)
+            )
+    new_col_name = parts[2] if len(parts) == 3 else None
+    if not new_col_name and args.header:
+        sys.exit(
+            'A name is required for any new columns when using an existing '
+            'header line (--header option), but found none in instruction: '
+            '"%s"' % e
+        )
+    ops.append([exp_lambda, new_col_idx, mode, new_col_name])
+    if mode is Mode.APPEND or mode is Mode.INSERT:
+        # If the current expression results in an additional column,
+        # we need to handle the new field in subsequent lambda functions.
+        num_cols += 1
+        col_str += ', c%d' % num_cols
+
+
+# ready to start parsing the input file
+print(
+    'Computing %d columns with instructions %s' % (num_cols - in_columns, expr)
+)
 skipped_lines = 0
 first_invalid_line = 0
 invalid_line = None
 lines_kept = 0
 total_lines = 0
 
-# Read input file, skipping invalid lines, and perform computation that will result in a new column
-code = '''
-for i, line in enumerate(fh):
-    total_lines += 1
-    line = line.rstrip('\\r\\n')
-    if not line or line.startswith('#'):
-        skipped_lines += 1
-        if not invalid_line:
-            first_invalid_line = i + 1
-            invalid_line = line
-        continue
-    try:
-        %s
-        %s
-        new_val = %s
-        if round_result:
-            new_val = int(round(new_val))
-        new_line = line + '\\t' + str(new_val) + "\\n"
-        out.write(new_line)
-        lines_kept += 1
-    except Exception:
-        skipped_lines += 1
-        if not invalid_line:
-            first_invalid_line = i + 1
-            invalid_line = line
-fh.close()
-''' % (assign, wrap, expr)
+with open(args.input) as fh, open(args.output, 'w') as out:
+    if args.header:
+        # compute new header line from original
+        header_cols = fh.readline().strip('\n').split('\t')
+        for _, col_idx, mode, col_name in ops:
+            if mode is Mode.INSERT:
+                header_cols.insert(col_idx, col_name)
+            elif mode is Mode.REPLACE:
+                header_cols[col_idx] = col_name
+            else:
+                header_cols.append(col_name)
+        out.write('\t'.join(header_cols) + '\n')
 
-valid_expr = True
-try:
-    exec(code)
-except Exception as e:
-    if str(e).startswith('invalid syntax'):
-        valid_expr = False
-        sys.exit('Expression "%s" likely invalid. See tool tips, syntax and examples.' % expr)
-    else:
-        sys.exit(str(e))
-finally:
-    out.close()
+    # read data, skipping invalid lines, and perform computations
+    # that will result in new columns
+    for i, line in enumerate(fh):
+        total_lines += 1
+        line = line.rstrip('\n')
+        if not line or line.startswith('#'):
+            skipped_lines += 1
+            if not invalid_line:
+                first_invalid_line = i + 1
+                invalid_line = line
+            continue
+        fields = line.split('\t')
+        typed_fields = cast_types(*fields)
+        try:
+            for fun, col_idx, mode, col_name in ops:
+                new_val = fun(*typed_fields)
+                if mode is Mode.INSERT:
+                    fields.insert(col_idx, new_val)
+                    typed_fields.insert(col_idx, new_val)
+                elif mode is Mode.REPLACE:
+                    fields[col_idx] = new_val
+                    typed_fields[col_idx] = new_val
+                else:
+                    fields.append(new_val)
+                    typed_fields.append(new_val)
+            fields = [
+                format_float_positional(field)
+                if args.avoid_scientific_notation and type(field) is float
+                else str(field)
+                for field in fields
+            ]
+            out.write('\t'.join(fields) + '\n')
+            lines_kept += 1
+        except Exception as e:
+            print(e)
+            skipped_lines += 1
+            if not invalid_line:
+                first_invalid_line = i + 1
+                invalid_line = line
 
-if valid_expr:
-    valid_lines = total_lines - skipped_lines
-    print('Creating column %d with expression %s' % (in_columns + 1, expr))
-    if valid_lines > 0:
-        print('kept %4.2f%% of %d lines.' % (100.0 * lines_kept / valid_lines,
-                                             total_lines))
-    else:
-        print('Possible invalid expression "%s" or non-existent column referenced. See tool tips, syntax and examples.' % expr)
-    if skipped_lines > 0:
-        print('Skipped %d invalid lines starting at line #%d: "%s"' %
-              (skipped_lines, first_invalid_line, invalid_line))
+
+valid_lines = total_lines - skipped_lines
+if valid_lines > 0:
+    print('kept %4.2f%% of %d lines.' % (100.0 * lines_kept / valid_lines,
+                                         total_lines))
+else:
+    print(
+        'Could not compute a new column for any input row!  '
+        'Please check your expression(s) "%s" for references to non-existent '
+        'columns or other problems.'
+        % expr
+    )
+if skipped_lines > 0:
+    print('Skipped %d invalid lines starting at line #%d: "%s"' %
+          (skipped_lines, first_invalid_line, invalid_line))
