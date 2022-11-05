c94c542bca7424cf7d17efa6d56d4900cfc72be1
Björn Grüning <bjoern@gruenings.eu>
2022-11-05 17:46:29 +0100

    fix more linting errors

--- data_managers/data_manager_qiime_database_downloader/data_manager/data_manager_qiime_download.py
+++ data_managers/data_manager_qiime_database_downloader/data_manager/data_manager_qiime_download.py
@@ -184,24 +184,18 @@ def extract_archive(filepath, ext, db):
     if ext == "tar.gz" or ext == "tgz":
         with tarfile.open(filepath) as tar:
             def is_within_directory(directory, target):
-                
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-            
                 prefix = os.path.commonprefix([abs_directory, abs_target])
-                
                 return prefix == abs_directory
             
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
                 for member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-            
                 tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
+
             safe_extract(tar, path=archive_content_path)
         archive_content_path = find_archive_content_path(archive_content_path)
     elif ext == "zip":
