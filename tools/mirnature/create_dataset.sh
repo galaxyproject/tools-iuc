9f38962f8e87a7c86b36c775ac8f1096e633b7ab
cavelandiah <cavelandiah@unal.edu.co>
2022-12-01 14:03:22 +0100

    included some suggestions

--- tools/mirnature/create_dataset.sh
+++ tools/mirnature/create_dataset.sh
@@ -3,7 +3,9 @@
 input=$1
 dir=$2
 
-cp $input $dir/data.gz
-Dir_name=`tar -tf $dir/data.gz | head -1 | cut -f1 -d"/" | sort | uniq`
+ln -s $input $dir/data.gz
+# Get name of compressed folder into $Dir_name
+Dir_name=$(tar -tf $dir/data.gz | head -1 | cut -f1 -d"/")
 tar -xf $dir/data.gz --directory $dir/
+# Change name of user folder to Dataset
 mv $dir/${Dir_name} $dir/Dataset
--- tools/mirnature/generate_blast_folder.sh
+++ tools/mirnature/generate_blast_folder.sh
@@ -3,4 +3,4 @@
 fasta_file=$1
 # Anolis_carolinensis.fa  miRNA   Anolis carolinensis
 base_name=$(basename $fasta_file)
-echo "$base_name miRNA Unknown specie"
+echo "$base_name miRNA Unknown species"
