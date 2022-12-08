59bb47a91e6d11febfc7b79a4b2063580c090c49
cavelandiah <cavelandiah@unal.edu.co>
2022-12-08 02:37:40 +0100

    Improved bash script

--- tools/mirnature/create_dataset.sh
+++ tools/mirnature/create_dataset.sh
@@ -4,8 +4,7 @@ input=$1
 dir=$2
 
 ln -s $input $dir/data.gz
-# Get name of compressed folder into $Dir_name
-Dir_name=$(tar -tf $dir/data.gz | head -1 | cut -f1 -d"/")
 tar -xf $dir/data.gz --directory $dir/
+rm $dir/data.gz
 # Change name of user folder to Dataset
-mv $dir/${Dir_name} $dir/Dataset
+mv $dir/* $dir/Dataset
