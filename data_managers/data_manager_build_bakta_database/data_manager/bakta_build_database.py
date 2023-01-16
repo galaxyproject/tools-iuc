02c1440c755820f63d70a857669b0e2ee66fe211
pimarin <pierre.marin@live.fr>
2023-01-16 17:15:30 +0100

    change value by name

--- data_managers/data_manager_build_bakta_database/data_manager/bakta_build_database.py
+++ data_managers/data_manager_build_bakta_database/data_manager/bakta_build_database.py
@@ -78,12 +78,12 @@ class GetBaktaDatabaseInfo:
 
     def get_data_manager(self, bakta_database_info):
         self.bakta_table_list = self.get_data_table_format()
-        bakta_value = f"V{bakta_database_info['major']}." \
+        bakta_name = f"V{bakta_database_info['major']}." \
                       f"{bakta_database_info['minor']}_" \
                       f"{bakta_database_info['date']}"
         tool_version = str(f"{bakta_database_info['software-min']['major']}."
                            f"{bakta_database_info['software-min']['minor']}")
-        data_info = dict(value=bakta_value,
+        data_info = dict(name=bakta_name,
                          dbkey=bakta_database_info['record'],
                          bakta_version=tool_version,
                          path="db")
