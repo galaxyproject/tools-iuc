be9d53eda8954065ebbf5ab0dd51f17c017a532a
Cristóbal Gallardo <gallardo@informatik.uni-freiburg.de>
2023-04-04 08:44:08 +0200

    Last changes

--- data_managers/data_manager_busco_database_downloader/data_manager/data_manager_busco_download.py
+++ data_managers/data_manager_busco_database_downloader/data_manager/data_manager_busco_download.py
@@ -7,45 +7,6 @@ import subprocess
 from datetime import date
 from pathlib import Path
 
-HUMANN_REFERENCE_DATA = {
-    "chocophlan": {
-        "full": "Full ChocoPhlAn for HUManN",
-        "DEMO": "Demo ChocoPhlAn for HUManN"
-    },
-    "uniref": {
-        "uniref50_diamond": "Full UniRef50 for HUManN",
-        "uniref50_ec_filtered_diamond": "EC-filtered UniRef50 for HUManN",
-        "uniref90_diamond": "Full UniRef90 for HUManN",
-        "uniref90_ec_filtered_diamond": "EC-filtered UniRef90 for HUManN",
-        "DEMO_diamond": "Demo UniRef for HUManN"
-    },
-    "utility_mapping": {
-        "full": {
-            "map_uniref50_uniref90": "Mapping (full) for UniRef50 from UniRef90",
-            "map_ko_uniref90": "Mapping (full) for KEGG Orthogroups (KOs) from UniRef90",
-            "map_eggnog_name": "Mapping (full) between EggNOG (including COGs) ids and names",
-            "map_uniref90_name": "Mapping (full) between UniRef90 ids and names",
-            "map_go_uniref90": "Mapping (full) for Gene Ontology (GO) from UniRef90",
-            "uniref90-tol-lca": "Mapping (full) for LCA for UniRef90",
-            "uniref50-tol-lca": "Mapping (full) for LCA for UniRef50",
-            "map_eggnog_uniref50": "Mapping (full) for EggNOG (including COGs) from UniRef50",
-            "map_pfam_uniref90": "Mapping (full) for Pfam domains from UniRef90",
-            "map_go_uniref50": "Mapping (full) for Gene Ontology (GO) from UniRef50",
-            "map_ko_name": "Mapping (full) between KEGG Orthogroups (KOs) ids and names",
-            "map_level4ec_uniref90": "Mapping (full) for Level-4 enzyme commission (EC) categories from UniRef90",
-            "map_go_name": "Mapping (full) between Gene Ontology (GO) ids and names",
-            "map_ko_uniref50": "Mapping (full) for KEGG Orthogroups (KOs) from UniRef50",
-            "map_level4ec_uniref50": "Mapping (full) for Level-4 enzyme commission (EC) categories from UniRef90",
-            "map_pfam_uniref50": "Mapping (full) for Pfam domains from UniRef50",
-            "map_eggnog_uniref90": "Mapping (full) for EggNOG (including COGs) from UniRef90",
-            "map_uniref50_name": "Mapping (full) between UniRef50 ids and names",
-            "map_ec_name": "Mapping (full) between Level-4 enzyme commission (EC) categories ids and names",
-            "map_pfam_name": "Mapping (full) between Pfam domains ids and names"
-        }
-    }
-}
-
-
 # Utility functions for interacting with Galaxy JSON
 def read_input_json(json_fp):
     """Read the JSON supplied from the data manager tool
@@ -113,8 +74,8 @@ def add_data_table_entry(d, table, entry):
         raise Exception("add_data_table_entry: no table '%s'" % table)
 
 
-def download_humann_db(data_tables, table_name, database, build, version, target_dp):
-    """Download HUMAnN database
+def download_busco_db(data_tables, table_name, database, index, version, target_dp):
+    """Download BUSCO database
 
     Creates references to the specified file(s) on the Galaxy
     server in the appropriate data table (determined from the
@@ -125,54 +86,40 @@ def download_humann_db(data_tables, table_name, database, build, version, target
 
     Arguments:
       data_tables: a dictionary containing the data table info
-      table_name: name of the table
-      database: database to download (chocophlan or uniref)
-      build: build of the database to download
+      database: database to download (all, eukaryota, prokaryota or virus)
+      index: build of the database to download
       version: tool version
       target_dp: directory to put copy or link to the data file
     """
-    db_target_dp = target_dp / Path(database)
-    db_dp = db_target_dp / Path(database)
-    build_target_dp = db_target_dp / Path(build)
+
     # launch tool to get db
-    cmd = "humann_databases --download %s %s %s --update-config no" % (
-        database,
-        build,
-        db_target_dp)
+    db_dp = target_dp / Path(database)
+    index_target_dp = db_dp / Path(index)
+    cmd = "busco --download %s" % (
+        database)
     subprocess.check_call(cmd, shell=True)
     # move db
     db_dp.rename(build_target_dp)
     # add details to data table
-    if database != "utility_mapping":
-        add_data_table_entry(
-            data_tables,
-            table_name,
-            dict(
-                value="%s-%s-%s-%s" % (database, build, version, date.today().strftime("%d%m%Y")),
-                name=HUMANN_REFERENCE_DATA[database][build],
-                dbkey=version,
-                path=str(build_target_dp)))
-    elif args.database == "utility_mapping":
-        for x in build_target_dp.iterdir():
-            name = str(x.stem).split('.')[0]
-            add_data_table_entry(
-                data_tables,
-                table_name,
-                dict(
-                    value="%s-%s-%s-%s-%s%s" % (database, build, name, version, date.today().strftime("%d%m%Y"), x.suffix),
-                    name=HUMANN_REFERENCE_DATA["utility_mapping"][build][name],
-                    dbkey=version,
-                    path=str(x)))
+    add_data_table_entry(
+        data_tables,
+        table_name,
+        dict(
+            dbdate="%s" % (date.today().strftime("%d%m%Y")),
+            dbname="BUSCO database %s" % database,
+            dbversion="db_version_%s_%s" % (version,index),
+            path=str(index_target_dp)))
+
 
 
 if __name__ == "__main__":
     print("Starting...")
 
     # Read command line
-    parser = argparse.ArgumentParser(description='Download HUMAnN database')
+    parser = argparse.ArgumentParser(description='Download BUSCO database')
     parser.add_argument('--database', help="Database name")
     parser.add_argument('--build', help="Build of the database")
-    parser.add_argument('--version', help="HUMAnN version")
+    parser.add_argument('--version', help="BUSCO version")
     parser.add_argument('--json', help="Path to JSON file")
     args = parser.parse_args()
     print("args   : %s" % args)
