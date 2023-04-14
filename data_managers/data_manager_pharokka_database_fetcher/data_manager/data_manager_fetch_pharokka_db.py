803b61e92a50da0f34042e8eb043f45cd8b08e1f
paulzierep <paul.zierep@googlemail.com>
2023-04-14 14:34:33 +0200

    flake8 linting

--- data_managers/data_manager_pharokka_database_fetcher/data_manager/data_manager_fetch_pharokka_db.py
+++ data_managers/data_manager_pharokka_database_fetcher/data_manager/data_manager_fetch_pharokka_db.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
 import argparse
-import uuid
 import os
 from datetime import datetime
 import json
 import subprocess
 import sys
 
+
 def main():
-    #Parse Command Line
-    parser = argparse.ArgumentParser(description='Create data manager JSON.')
-    parser.add_argument('--out', dest='output', action='store', help='JSON filename')
-    parser.add_argument("--test", action='store_true', help="option to test the script with an lighted database")
+    # Parse Command Line
+    parser = argparse.ArgumentParser(description="Create data manager JSON.")
+    parser.add_argument("--out", dest="output", action="store", help="JSON filename")
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="option to test the script with an lighted database",
+    )
 
     args = parser.parse_args()
 
@@ -22,17 +26,16 @@ def main():
     with open(args.output) as fh:
         params = json.load(fh)
 
-
-    workdir = params['output_data'][0]['extra_files_path']
+    workdir = params["output_data"][0]["extra_files_path"]
     os.mkdir(workdir)
 
     time = datetime.utcnow().strftime("%Y-%m-%dT%H%M%SZ")
     db_value = "db_from_{0}".format(time)
     db_path = os.path.join(workdir, db_value)
 
-    #create DB
-    if args.test: #the test just checks that the pharokka download script is available
-        command_args = ["install_databases.py", "-h"]   
+    # create DB
+    if args.test:  # the test just checks that the pharokka download script is available
+        command_args = ["install_databases.py", "-h"]
     else:
         command_args = ["install_databases.py", "-o", db_path]
 
@@ -45,18 +48,19 @@ def main():
 
     # Update Data Manager JSON and write to file
     data_manager_entry = {
-        'data_tables': {
-            'pharokka_db': {
-                'value': db_value,
-                'dbkey': db_value,
-                'name': 'Pharokka DB downloaded at {0}'.format(datetime.now()),
-                'path': db_path
+        "data_tables": {
+            "pharokka_db": {
+                "value": db_value,
+                "dbkey": db_value,
+                "name": "Pharokka DB downloaded at {0}".format(datetime.now()),
+                "path": db_path,
             }
         }
     }
 
-    with open(os.path.join(args.output), 'w+') as fh:
+    with open(os.path.join(args.output), "w+") as fh:
         json.dump(data_manager_entry, fh, sort_keys=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
