4b3120eb37e388780948695b1726b3227821c236
Cristóbal Gallardo <gallardoalbac@gmail.com>
2023-06-05 19:32:20 +0200

    Update tools/sleuth/sleuth.R

--- tools/sleuth/sleuth.R
+++ tools/sleuth/sleuth.R
@@ -53,13 +53,13 @@ if (args$experiment_design == "complex") {
                sep = "\t")
   paths <- c()
   for (x in s2c$data_filename) {
-    paths <- c(paths, paste("./kallisto_outputs/", x, sep = ""))
+    paths <- c(paths, paste("./kallisto_outputs/",  x, sep = ""))
   }
   for (f in paths) {
     file.rename(f, gsub(".fastq.*", "", f))
-        file.rename(f, paste(gsub(".fastq.*", "", f),".h5",sep=""))
+        file.rename(f, paste(gsub(".fastq.*", "", f), ".h5",sep = ""))
   }
-  s2c$path <- paste(gsub(".fastq.*", ".h5", paths),".h5",sep="")
+  s2c$path <- paste(gsub(".fastq.*", ".h5", paths), ".h5",sep = "")
 
   so <- sleuth_prep(s2c, full_model = ~ condition, num_cores = 1)
   so <- sleuth_fit(so)
