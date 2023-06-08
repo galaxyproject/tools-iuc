b685cbe5113235134ab99813c081d1ba4adefd4c
Peter van Heusden <pvanheusden@uwc.ac.za>
2023-06-08 22:39:16 +0200

    Fix flake8 errors in ungzip.py

--- tools/meningotype/ungzip.py
+++ tools/meningotype/ungzip.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
     parser.add_argument('dataset_input')
     parser.add_argument('dataset_name')
     args = parser.parse_args()
-    
-    blocksize = 1024*1000*100;  # 100 MB buffer
+
+    blocksize = 1024 * 1000 * 100  # 100 MB buffer
     input_file = gzip.open(args.dataset_input, "rb")
-    output_filename = args.dataset_name.replace('.gz', '')
-    output_file = open(output_filename, "wb")
+    output_file = open(args.dataset_name, "wb")
 
     data = input_file.read(blocksize)
     while len(data) != 0:
