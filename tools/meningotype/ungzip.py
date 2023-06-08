c1b31311bc2452f659d423e4009b1f443a96782b
Peter van Heusden <pvanheusden@uwc.ac.za>
2023-06-08 21:15:40 +0200

    Fix more uncompression bugs

--- tools/meningotype/ungzip.py
+++ tools/meningotype/ungzip.py
@@ -6,12 +6,15 @@ import gzip
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset_input')
-    parser.add_argument('dataset_output', type=argparse.FileType('wb'))
+    parser.add_argument('dataset_name')
     args = parser.parse_args()
     
     blocksize = 1024*1000*100;  # 100 MB buffer
     input_file = gzip.open(args.dataset_input, "rb")
+    output_filename = args.dataset_name.replace('.gz', '')
+    output_file = open(output_filename, "wb")
+
     data = input_file.read(blocksize)
     while len(data) != 0:
-        args.dataset_output.write(data)
+        output_file.write(data)
         data = input_file.read(blocksize)
