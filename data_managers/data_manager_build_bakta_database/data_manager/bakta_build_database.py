d61f63dc84ac1501447f8d535a58e6c207931262
pimarin <pierre.marin@live.fr>
2023-06-08 16:18:25 +0200

    update bakta code

--- data_managers/data_manager_build_bakta_database/data_manager/bakta_build_database.py
+++ data_managers/data_manager_build_bakta_database/data_manager/bakta_build_database.py
@@ -2,8 +2,8 @@ import argparse
 import hashlib
 import json
 import os
-import sys
 import re
+import sys
 import tarfile
 from datetime import datetime
 from pathlib import Path
@@ -21,20 +21,29 @@ class GetBaktaDatabaseInfo:
                  data_table_name="bakta_database",
                  db_name=Path.cwd().joinpath("db"),
                  db_version="latest",
+                 tarball_name="db.tar.gz",
                  test_mode=False):
         self.bakta_table_list = None
         self.db_url = None
+        self.db_type = ""
         self.data_table_entry = None
         self.data_table_name = data_table_name
         self.db_name = db_name
+        self.tar_name = tarball_name
         self.db_version = db_version
-        self.light_db = bool(re.search("light", self.db_version))
-        self.db_version.split(sep="_")
-        
-        
         self.DB_VERSIONS_URL = 'https://raw.githubusercontent.com/oschwengers/bakta/master/db-versions.json'
-        self.DB_TEST_URL = 'https://zenodo.org/record/7360542/files/db-versions.json'
+        self.DB_TEST_URL = 'https://zenodo.org/record/8017774/files/db-versions.json'
         self.test_mode = test_mode
+
+    def get_database_type(self):
+        self.light_db = bool(re.search(pattern="light", string=self.db_version))
+        self.db_version = self.db_version.split(sep="_")[0]
+        if self.light_db:
+            self.db_type = "light"
+            self.tar_name = "db-light.tar.gz"
+            self.md5 = self.fetch_db_versions()["md5-light"]
+        else:
+            self.md5 = self.fetch_db_versions()["md5"]
 
     def get_data_table_format(self):
         """
@@ -52,7 +61,7 @@ class GetBaktaDatabaseInfo:
         """
         List bakta database info related to the db_version selected
         """
-        if self.test_mode is True:
+        if self.test_mode == "True":
             self.DB_VERSIONS_URL = self.DB_TEST_URL
         try:
             with requests.get(self.DB_VERSIONS_URL) as resp:
@@ -60,30 +69,30 @@ class GetBaktaDatabaseInfo:
         except IOError as e:
             print(e, file=sys.stderr)
             raise e
+        
+        if self.db_version == "latest":
+            db_date_list = []
+            for db_dic in versions:
+                db_date_list.append(datetime.strptime(db_dic["date"],
+                                                        '%Y-%m-%d').date())
+            filtered_version = max(versions, key=lambda x: x['date'])
         else:
-            if self.db_version == "latest":
-                db_date_list = []
-                for db_dic in versions:
-                    db_date_list.append(datetime.strptime(db_dic["date"],
-                                                          '%Y-%m-%d').date())
-                filtered_version = max(versions, key=lambda x: x['date'])
+            filtered_version = None
+            for item in versions:
+                if '{0}.{1}'.format(item["major"], item["minor"]) == self.db_version:
+                    filtered_version = item
+                    break
+            if filtered_version is None:
+                print("No matching version detected in the list")
             else:
-                filtered_version = None
-                for item in versions:
-                    if '{0}.{1}'.format(item["major"], item["minor"]) == self.db_version:
-                        filtered_version = item
-                        break
-                if filtered_version is None:
-                    print("No matching version detected in the list")
-            if filtered_version is not None:
                 self.db_url = f"https://zenodo.org/record/" \
-                              f"{filtered_version['record']}/files/db.tar.gz"
+                                f"{filtered_version['record']}/files/{self.tar_name}"
                 return filtered_version
 
     def get_data_manager(self, bakta_database_info):
         self.bakta_table_list = self.get_data_table_format()
         bakta_name = f"V{bakta_database_info['major']}." \
-                     f"{bakta_database_info['minor']}_" \
+                     f"{bakta_database_info['minor']}{self.db_type}_" \
                      f"{bakta_database_info['date']}"
         tool_version = str(f"{bakta_database_info['software-min']['major']}."
                            f"{bakta_database_info['software-min']['minor']}")
@@ -105,21 +114,20 @@ class InstallBaktaDatabase(GetBaktaDatabaseInfo):
     def __init__(self,
                  db_dir=Path.cwd(),
                  db_name="bakta",
-                 tarball_name="db.tar.gz",
                  db_version="latest",
                  test_mode=False):
         super().__init__()
         self.md5 = None
+        self.db_version = db_version
         self.db_dir = db_dir
         self.db_name = db_name
-        self.tarball_name = tarball_name
-        self.tarball_path = None
+        self.tarball_path = ""
         self.test_mode = test_mode
-        self.db_version = db_version
+        self.get_database_type()
 
     def download(self):
-        self.db_name = f'{self.db_name}_{self.db_version}'
-        bakta_path = Path(self.db_dir).joinpath(self.tarball_name)
+        self.db_name = f'{self.db_name}_{self.db_version}{self.db_type}'
+        bakta_path = Path(self.db_dir).joinpath(self.tar_name)
         try:
             with bakta_path.open('wb') as fh_out, \
                     requests.get(self.db_url, stream=True) as resp:
@@ -134,7 +142,7 @@ class InstallBaktaDatabase(GetBaktaDatabaseInfo):
             self.tarball_path = bakta_path
         except IOError:
             print(f'ERROR: Could not download file from Zenodo!'
-                  f' url={self.db_url}, path={self.tarball_name}')
+                  f' url={self.db_url}, path={self.tar_name}')
 
     def untar(self):
         db_path = Path(self.db_dir).as_posix()
@@ -145,12 +153,11 @@ class InstallBaktaDatabase(GetBaktaDatabaseInfo):
                 print(f'Untar the database in {db_path}')
                 return db_path
         except OSError:
-            sys.exit(f'ERROR: Could not extract {self.tarball_name} '
+            sys.exit(f'ERROR: Could not extract {self.tar_name} '
                      f'to {self.db_name}')
 
     def calc_md5_sum(self, buffer_size=1048576):
-        tarball_path = Path(self.db_dir).joinpath(self.tarball_name)
-        self.md5 = self.fetch_db_versions(db_version=self.db_version)["md5"]
+        tarball_path = Path(self.db_dir).joinpath(self.tar_name)
         md5 = hashlib.md5()
         with tarball_path.open('rb') as fh:
             data = fh.read(buffer_size)
@@ -211,8 +218,8 @@ def main():
     target_dir = params['output_data'][0]['extra_files_path']
     os.makedirs(target_dir)
     # init the class to download bakta db
-    bakta_upload = InstallBaktaDatabase(test_mode=all_args.test)
-    bakta_db = bakta_upload.fetch_db_versions(db_version=all_args.database_version)
+    bakta_upload = InstallBaktaDatabase(test_mode=all_args.test, db_version=all_args.database_version)
+    bakta_db = bakta_upload.fetch_db_versions()
     # update the path for galaxy
     bakta_upload.db_dir = target_dir
     # download the database
@@ -231,17 +238,9 @@ if __name__ == '__main__':
     main()
 
 
-    self = InstallBaktaDatabase(db_version="5.0")
-    bakta_db = self.fetch_db_versions()
-    # update the path for galaxy
-    bakta_upload.db_dir = target_dir
-    # download the database
-    bakta_upload.download()
-    # check md5 sum
-    bakta_upload.calc_md5_sum()
-    # untar db
-    bakta_upload.untar()
-    # make the data_manager metadata
-    bakta_data_manager = bakta_upload.get_data_manager(bakta_database_info=bakta_db)
-    with open(all_args.data_manager_json, 'w') as fh:
-        json.dump(bakta_data_manager, fh, sort_keys=True)
+self = InstallBaktaDatabase(db_version="5.0_light",test_mode="True")
+bakta_db = self.fetch_db_versions()
+self.download()
+self.untar()
+bakta_data_manager = self.get_data_manager(bakta_database_info=bakta_db)
+
