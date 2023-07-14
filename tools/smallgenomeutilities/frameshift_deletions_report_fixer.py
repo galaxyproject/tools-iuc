558f1a67c627965a3021c2dc6b7db31cee7a7377
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2023-07-14 13:37:10 +0200

    Add missing script

---
+++ tools/smallgenomeutilities/frameshift_deletions_report_fixer.py
@@ -0,0 +1,32 @@
+"""Polish the output of the frameshift_deletions_check command.
+
+- Drops the first index column, which is rather pointless to include
+- Turns ref bases printed as literal bytes strings into plain output
+- Removes [] around pos lists and spaces after comma separating list elements
+- Turns None and empty list values into . as a cell placeholder
+"""
+
+import re
+import sys
+
+
+def matchrepl(matchobj):
+    print(matchobj.groups())
+    bytes_string_content = matchobj.group(1)
+    if bytes_string_content is not None:
+        return bytes_string_content
+    list_content = matchobj.group(2)
+    if list_content is not None:
+        if list_content == '':
+            return '.'
+        return list_content.replace(', ', ',')
+    none_cell = matchobj.group(3)
+    return '\t.\t'
+
+if __name__ == '__main__':
+    regex = re.compile(r"b'(.+)'|\[([^\]]*)\]|\t(None)\t")
+    with open(sys.argv[1]) as i:
+        with open(sys.argv[2], 'w') as o:
+            for line in i:
+                line = line[line.index('\t') + 1:]
+                o.write(regex.sub(matchrepl, line))
