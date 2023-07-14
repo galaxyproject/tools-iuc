584cfa8c0bac098f9310eb455bf0d528a4658428
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2023-07-14 15:23:56 +0200

    Fix linting

--- tools/smallgenomeutilities/frameshift_deletions_report_fixer.py
+++ tools/smallgenomeutilities/frameshift_deletions_report_fixer.py
@@ -21,7 +21,11 @@ def matchrepl(matchobj):
             return '.'
         return list_content.replace(', ', ',')
     none_cell = matchobj.group(3)
-    return '\t.\t'
+    if none_cell is not None:
+        return '\t.\t'
+        
+    raise ValueError('Error in regex parsing code')
+
 
 if __name__ == '__main__':
     regex = re.compile(r"b'(.+)'|\[([^\]]*)\]|\t(None)\t")
