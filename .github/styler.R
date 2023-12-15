242dbd96b755cccba744442c8ff6e4f1d1e50806
Matthias Bernt <m.bernt@ufz.de>
2023-12-15 18:09:29 +0100

    replace lintr by styler
    styler the R code linter/styler from the tidyverse.
    in contrast to lintr it can also fix the found problems
    (added a script that can be used for checking and fixing)

---
+++ .github/styler.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+library("argparse")
+library("styler")
+
+parser <- ArgumentParser(description = "Call styler")
+parser$add_argument("dir",
+  metavar = "DIR", type = "character",
+  help = "File to parse"
+)
+parser$add_argument("--dry",
+  choices = c("off", "on"), default = "on"
+)
+args <- parser$parse_args()
+
+file_info <- file.info(args$dir)
+is_directory <- file_info$isdir
+
+if(is_directory){
+    captured_output <- capture.output({
+        result <- style_dir(args$dir, indent_by = 4, dry = args$dry, recursive = TRUE)
+    })
+}else{
+    captured_output <- capture.output({
+        result <- style_file(args$dir, indent_by = 4, dry = args$dry)
+    })
+}
+
+n <- nrow(subset(result, changed == TRUE))
+if(n > 0){
+    if(args$dry == "off"){
+        print(paste("Changed", n, "files"))
+    }else{
+        stop(paste("Linting failed for", n, "files"))
+    }
+}
