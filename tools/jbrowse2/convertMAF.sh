b5db796fdd2682fc871c8360ea089c9ca00d38f6
fubar2 <ross.lazarus@gmail.com>
2024-01-02 17:46:42 +1100

    fixes to restore maf functionality. Now have blast, maf, bed, bam, gff and vcf test data showing. references and HiC files are served as urls now so size of files directory is constrained a little.. not completely.

--- tools/jbrowse2/convertMAF.sh
+++ tools/jbrowse2/convertMAF.sh
@@ -2,7 +2,6 @@
 # https://github.com/cmdcolin/jbrowse-plugin-mafviewer/blob/master/bin/convert.sh
 #  MAF file must contain the species name and chromosome name
 #  e.g. hg38.chr1 in the sequence identifiers.
-perl $3/maf2bed.pl $2 < $1 > $1.bed
-sort -k1,1 -k2,2n $1.bed > $1.sorted.bed
-bgzip -c $1.sorted.bed > $1.sorted.bed.gz
-tabix -p bed $1.sorted.bed.gz
+perl $3/maf2bed.pl $2 < $1 | sort -k1,1 -k2,2n > $4.sorted.bed
+bgzip $4.sorted.bed
+tabix -p bed $4.sorted.bed.gz
