4c644fd63ef3f23de6a9f546875fb177131013b6
rlibouban <romane.libouban@irisa.fr>
2024-01-02 15:41:20 +0100

    error flake8

--- data_managers/data_manager_omamer/data_manager/omamer.py
+++ data_managers/data_manager_omamer/data_manager/omamer.py
@@ -11,7 +11,6 @@ import requests
 # URL for downloading OMAmer datasets
 OMAMER_DATASETS_URL = "https://omabrowser.org/All/{dataset}"
 
-
 # List of OMAmer data sets
 OMAMER_DATASETS = [
     "Primates-v2.0.0.h5",
@@ -19,7 +18,6 @@ OMAMER_DATASETS = [
     "Metazoa-v2.0.0.h5",
     "LUCA-v0.2.5.h5"
 ]
-
 
 def download_file(url, dest):
     try:
@@ -35,12 +33,10 @@ def download_file(url, dest):
         print(f"Error downloading {url}: {e}")
         sys.exit(1)
 
-
 def main(args):
     # Create output directory if none exists
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
-
 
     # Download each specified OMAmer data set
     for dataset in OMAMER_DATASETS:
@@ -48,7 +44,6 @@ def main(args):
         base_name = os.path.splitext(dataset)[0]
         destination_path = os.path.join(args.output_dir, base_name)
         download_file(url, destination_path)
-
 
     # Utiliser le nom du fichier sans extension comme identifiant unique
     data_manager_entry = {
@@ -60,11 +55,9 @@ def main(args):
     # Creates a JSON dictionary representing the Data Manager configuration
     data_manager_json = {"data_tables": {"omamer_data": [data_manager_entry]}}
 
-
     # Writes this JSON dictionary to the specified output file
     with open(args.json, "w") as fh:
         json.dump(data_manager_json, fh, indent=2, sort_keys=True)
-
 
 if __name__ == "__main__":
     # Set up argparse to specify expected command line arguments
@@ -73,7 +66,6 @@ if __name__ == "__main__":
     parser.add_argument('--name', default='default_name', help='Data table entry unique ID')
     parser.add_argument('--json', help='Path to JSON file')
     args = parser.parse_args()
-
 
     # Call the main function with the analyzed arguments
     main(args)
