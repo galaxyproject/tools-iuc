0fcfa3a008173aca1f53f5d9aa794cf2f33a9ec7
Matthias Bernt <m.bernt@ufz.de>
2024-03-14 15:44:26 +0100

    start working on biom import

---
+++ tools/phyloseq/phyloseq_from_biom.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library("optparse"))
+suppressPackageStartupMessages(library("phyloseq"))
+suppressPackageStartupMessages(library("tidyverse"))
+
+option_list <- list(
+    make_option(c("--BIOMfilename"), action = "store", dest = "biom", help = "Input BIOM file"),
+    make_option(c("--treefilename"), action = "store", dest = "tree", default = NULL, help = "Input Tree newick/nexus file"),
+    make_option(c("--parseFunction"), action = "store", dest = "tree", default = "parse_taxonomy_default", help = "Parse function parse_taxonomy_default/read_tree_greengenes"),
+    make_option(c("--refseqfilename"), action = "store", dest = "sequences", default = NULL, help = "Input Sequence fasta file"),
+    make_option(c("--output"), action = "store", dest = "output", help = "RDS output")
+)
+
+parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
+args <- parse_args(parser, positional_arguments = TRUE)
+opt <- args$options
+
+parseFunction <- eval(opt$parseFunction)
+phyloseq_obj <- import_biom(opt$biom, opt$treefilename, opt$refseqfilename, parseFunction=parseFunction)
+
+# save R object to file
+saveRDS(phyloseq_obj, file = opt$output, compress = TRUE)
