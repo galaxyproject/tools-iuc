e099acda89ab9af3683d6198976110ad2d05f424
fubar2 <ross.lazarus@gmail.com>
2024-03-26 17:04:52 +1100

    Revert "updates to current code on EU"
    This reverts commit db857c231abb28e24f749c87bf8763f065a07cd8.

--- tools/jbrowse2/convertMAF.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# https://github.com/cmdcolin/jbrowse-plugin-mafviewer/blob/master/bin/convert.sh
-# maf2bed modified to work right as a python script by ross lazarus in desperation
-#  MAF file must contain the species name and chromosome name
-#  e.g. hg38.chr1 in the sequence identifiers with hg38 passed in as $2
-python $3/maf2bed.py $2 < $1 | sort -k1,1 -k2,2n > $4.sorted.bed
-bgzip -c $4.sorted.bed > $4.sorted.bed.gz
-tabix -p bed $4.sorted.bed.gz
