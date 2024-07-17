4063b65243f9ca9ead8ccd80a6040651ef6fae4b
fubar2 <ross.lazarus@gmail.com>
2024-07-18 08:47:13 +1000

    add option for windowed density bigwig output with selectable window size for size selected and specified motifs not so easy for native pytrf operation

--- tools/microsatbed/find_str.py
+++ tools/microsatbed/find_str.py
@@ -1,4 +1,7 @@
 import argparse
+import shutil
+
+import pybigtools
 
 import pytrf  # 1.3.0
 from pyfastx import Fastx  # 0.5.2
@@ -8,6 +11,15 @@ Allows all STR or those for a subset of motifs to be written to a bed file
 Designed to build some of the microsatellite tracks from https://github.com/arangrhie/T2T-Polish/tree/master/pattern for the VGP.
 """
 
+def getDensity(name, bed, len, winwidth):
+    nwin = int(len / winwidth)
+    d = [0.0 for x in range(nwin+1)]
+    for b in bed:
+        nt = b[5]
+        bin = int(b[1]/winwidth)
+        d[bin] += nt
+    dw = [(name,x*winwidth,(x+1)*winwidth,float(d[x])) for x in range(nwin+1) if (x+1)*winwidth <= len]
+    return dw
 
 def write_ssrs(args):
     """
@@ -18,13 +30,15 @@ def write_ssrs(args):
     Sequence read bias might be influenced by GC density or some other specific motif.
     """
     bed = []
+    wig = []
+    chrlens = {}
     specific = None
     if args.specific:
         specific = args.specific.upper().split(",")
     fa = Fastx(args.fasta, uppercase=True)
     for name, seq in fa:
-        if args.specific:
-            ssrs = pytrf.STRFinder(
+        cbed = []
+        for ssr in pytrf.STRFinder(
                 name,
                 seq,
                 args.monomin,
@@ -33,22 +47,10 @@ def write_ssrs(args):
                 args.tetramin,
                 args.pentamin,
                 args.hexamin,
-            )
-        else:
-            ssrs = pytrf.STRFinder(
-                name,
-                seq,
-                args.monomin,
-                args.dimin,
-                args.trimin,
-                args.tetramin,
-                args.pentamin,
-                args.hexamin,
-            )
-        for ssr in ssrs:
+            ):
             row = (
                 ssr.chrom,
-                ssr.start - 1,
+                ssr.start,
                 ssr.end,
                 ssr.motif,
                 ssr.repeat,
@@ -56,24 +58,35 @@ def write_ssrs(args):
             )
             # pytrf reports a 1 based start position so start-1 fixes the bed interval lengths
             if args.specific and ssr.motif in specific:
-                bed.append(row)
+                cbed.append(row)
             elif args.mono and len(ssr.motif) == 1:
-                bed.append(row)
+                cbed.append(row)
             elif args.di and len(ssr.motif) == 2:
-                bed.append(row)
+                cbed.append(row)
             elif args.tri and len(ssr.motif) == 3:
-                bed.append(row)
+                cbed.append(row)
             elif args.tetra and len(ssr.motif) == 4:
-                bed.append(row)
+                cbed.append(row)
             elif args.penta and len(ssr.motif) == 5:
-                bed.append(row)
+                cbed.append(row)
             elif args.hexa and len(ssr.motif) == 6:
-                bed.append(row)
-    bed.sort()
-    obed = ["%s\t%d\t%d\t%s_%d\t%d" % x for x in bed]
-    with open(args.bed, "w") as outbed:
-        outbed.write("\n".join(obed))
-        outbed.write("\n")
+                cbed.append(row)
+        bed += cbed
+        if args.bigwig:
+            chrlens[name] = len(seq)
+            w = getDensity(name, cbed, len(seq), args.winwidth)
+            wig += w
+    if args.bigwig:
+        wig.sort()
+        bw = pybigtools.open("temp.bw", 'w')
+        bw.write(chrlens,wig)
+        shutil.move("temp.bw", args.bed)
+    else:
+        bed.sort()
+        obed = ["%s\t%d\t%d\t%s_%d\t%d" % x for x in bed]
+        with open(args.bed, "w") as outbed:
+            outbed.write("\n".join(obed))
+            outbed.write("\n")
 
 
 if __name__ == "__main__":
@@ -93,6 +106,8 @@ if __name__ == "__main__":
     a("--monomin", default=2, type=int)
     a("-f", "--fasta", default="humsamp.fa")
     a("-b", "--bed", default="humsamp.bed")
+    a("--bigwig", action="store_true")
+    a("--winwidth", default=128, type=int)
     a("--specific", default=None)
     a("--minreps", default=2, type=int)
     args = parser.parse_args()
