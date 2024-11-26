7aec94118dd44d8359c17501621ac94fdcd38a8b
MaraBesemer <deine.email@beispiel.com>
2024-11-26 15:47:22 +0100

    change testinputs

--- tools/phyloseq/phyloseq_plot_bar.R
+++ tools/phyloseq/phyloseq_plot_bar.R
@@ -30,21 +30,33 @@ option_list <- list(
 )
 
 # Parse arguments
-parser <- OptionParser(
-    usage = "%prog [options] file",
-    option_list = option_list
-)
-args <- parse_args(parser)
+parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
+args <- parse_args(parser, positional_arguments = TRUE)
 opt <- args$options
 
 # Load phyloseq object
+print(paste("Trying to read:", opt$input))
 physeq <- readRDS(opt$input)
+
+# Check if the 'x' and 'fill' variables are valid
+if (!opt$x %in% colnames(sample_data(physeq))) {
+  stop(paste("Error: x variable", opt$x, "does not exist in the sample data."))
+}
+
+if (!opt$fill %in% colnames(sample_data(physeq))) {
+  stop(paste("Error: fill variable", opt$fill, "does not exist in the sample data."))
+}
 
 # Generate bar plot
 p <- plot_bar(physeq, x = opt$x, fill = opt$fill)
 
+# Only facet if the facet variable is provided and exists in the sample data
 if (!is.null(opt$facet)) {
-    p <- p + facet_wrap(as.formula(paste("~", opt$facet)))
+    if (opt$facet %in% colnames(sample_data(physeq))) {
+        p <- p + facet_wrap(as.formula(paste("~", opt$facet)))
+    } else {
+        warning(paste("Facet variable", opt$facet, "does not exist in the sample data. Faceting will be skipped."))
+    }
 }
 
 # Save to output file
