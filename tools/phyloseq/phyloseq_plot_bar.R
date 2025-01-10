82825683f587a7b64a64c1f90ec67e0c541081e4
MaraBesemer <deine.email@beispiel.com>
2025-01-10 15:11:23 +0100

    add: variables for width, height and device and fix R linting

--- tools/phyloseq/phyloseq_plot_bar.R
+++ tools/phyloseq/phyloseq_plot_bar.R
@@ -7,42 +7,54 @@ suppressPackageStartupMessages(library("ggplot2"))
 
 # Define options
 option_list <- list(
-  make_option(c("--input"),
-              action = "store", dest = "input",
-              help = "Input file containing a phyloseq object"
-  ),
-  make_option(c("--x"),
-              action = "store", dest = "x",
-              help = "Variable for x-axis (e.g., 'Sample', 'Phylum')"
-  ),
-  make_option(c("--fill"),
-              action = "store", dest = "fill", default = NULL,
-              help = "Variable for fill color (e.g., 'Genus', 'Order') (optional)"
-  ),
-  make_option(c("--facet"),
-              action = "store", dest = "facet", default = NULL,
-              help = "Facet by variable (optional)"
-  ),
-  make_option(c("--output"),
-              action = "store", dest = "output",
-              help = "Output file (PDF)"
-  ),
-  make_option(c("--topX"),
-              action = "store", dest = "topX", default = NULL,
-              help = "Show only the top X taxa based on abundance (e.g., '10') (optional)"
-  ),
-  make_option(c("--keepOthers"),
-              action = "store_true", dest = "keepOthers", default = FALSE,
-              help = "Keep taxa not in topX and label them as 'Others' (optional)"
-  ),
-  make_option(c("--keepNonAssigned"),
-              action = "store_true", dest = "keepNonAssigned", default = FALSE,
-              help = "Keep taxa labeled as 'Not Assigned' (optional)"
-  ),
-  make_option(c("--normalize"),
-              action = "store_true", dest = "normalize", default = FALSE,
-              help = "Normalize abundances to sum to 100% (optional)"
-  )
+    make_option(c("--input"),
+        action = "store", dest = "input",
+        help = "Input file containing a phyloseq object"
+    ),
+    make_option(c("--x"),
+        action = "store", dest = "x",
+        help = "Variable for x-axis (e.g., 'Sample', 'Phylum')"
+    ),
+    make_option(c("--fill"),
+        action = "store", dest = "fill", default = NULL,
+        help = "Variable for fill color (e.g., 'Genus', 'Order') (optional)"
+    ),
+    make_option(c("--facet"),
+        action = "store", dest = "facet", default = NULL,
+        help = "Facet by variable (optional)"
+    ),
+    make_option(c("--output"),
+        action = "store", dest = "output",
+        help = "Output file (PDF)"
+    ),
+    make_option(c("--topX"),
+        action = "store", dest = "topX", default = NULL,
+        help = "Show only the top X taxa based on abundance (e.g., '10') (optional)"
+    ),
+    make_option(c("--keepOthers"),
+        action = "store_true", dest = "keepOthers", default = FALSE,
+        help = "Keep taxa not in topX and label them as 'Others' (optional)"
+    ),
+    make_option(c("--keepNonAssigned"),
+        action = "store_true", dest = "keepNonAssigned", default = FALSE,
+        help = "Keep taxa labeled as 'Not Assigned' (optional)"
+    ),
+    make_option(c("--normalize"),
+        action = "store_true", dest = "normalize", default = FALSE,
+        help = "Normalize abundances to sum to 100% (optional)"
+    ),
+    make_option(c("--width"),
+        action = "store", dest = "width", default = 10,
+        type = "numeric", help = "Width of the output plot in inches"
+    ),
+    make_option(c("--height"),
+        action = "store", dest = "height", default = 8,
+        type = "numeric", help = "Height of the output plot in inches"
+    ),
+    make_option(c("--device"),
+        action = "store", dest = "device", default = "pdf",
+        help = "Output format (e.g., 'pdf', 'png', 'jpeg')"
+    )
 )
 
 # Parse arguments
@@ -52,10 +64,10 @@ opt <- args$options
 
 # Validate required options
 if (is.null(opt$input) || opt$input == "") {
-  stop("Error: Input file is required.")
+    stop("Error: Input file is required.")
 }
 if (is.null(opt$output) || opt$output == "") {
-  stop("Error: Output file is required.")
+    stop("Error: Output file is required.")
 }
 
 # Load phyloseq object
@@ -64,13 +76,13 @@ physeq <- readRDS(opt$input)
 
 # Normalize to relative abundances if requested
 if (opt$normalize) {
-  print("Normalizing abundances to sum to 100%...")
-  physeq <- transform_sample_counts(physeq, function(x) 100 * x / sum(x))
+    print("Normalizing abundances to sum to 100%...")
+    physeq <- transform_sample_counts(physeq, function(x) 100 * x / sum(x))
 }
 
 if (opt$keepNonAssigned) {
-# Add synthetic "Not Assigned" for missing/NA taxa
-tax_table(physeq) <- apply(tax_table(physeq), c(1, 2), function(x) ifelse(is.na(x) | x == "", "Not Assigned", x))
+    # Add synthetic "Not Assigned" for missing/NA taxa
+    tax_table(physeq) <- apply(tax_table(physeq), c(1, 2), function(x) ifelse(is.na(x) | x == "", "Not Assigned", x))
 }
 # Check if the 'x' and 'fill' variables are valid
 sample_vars <- colnames(sample_data(physeq))
@@ -83,12 +95,12 @@ if (!is.null(opt$topX) && opt$topX != "") {
     }
 
     # Aggregate the data at the selected rank (e.g., Phylum)
-    tax_rank <- opt$fill  # Adjust as necessary
+    tax_rank <- opt$fill # Adjust as necessary
     physeq_agg <- tax_glom(physeq, taxrank = tax_rank)
 
     # Get the abundance of each taxon at the selected rank
     taxa_abundance <- taxa_sums(physeq_agg)
-    
+
     # Summarize the abundance at each taxonomic rank (grouping by taxonomic name)
     tax_table_agg <- tax_table(physeq_agg)
     taxa_abundance_by_rank <- tapply(taxa_abundance, tax_table_agg[, tax_rank], sum)
@@ -115,24 +127,25 @@ if (!is.null(opt$topX) && opt$topX != "") {
 
 # Generate bar plot
 if (!is.null(opt$x) && opt$x != "") {
-  p <- plot_bar(physeq, x = opt$x, fill = opt$fill)
+    p <- plot_bar(physeq, x = opt$x, fill = opt$fill)
 } else {
-  p <- plot_bar(physeq, fill = opt$fill)  # If no x is provided, don't include x
+    p <- plot_bar(physeq, fill = opt$fill) # If no x is provided, don't include x
 }
 
 # Only facet if the facet variable is provided and exists in the sample data
 if (!is.null(opt$facet) && opt$facet != "") {
-  if (opt$facet %in% sample_vars) {
-    p <- p + facet_wrap(as.formula(paste("~", opt$facet)))
-  } else {
-    warning(paste("Facet variable", opt$facet, "does not exist in the sample data. Faceting will be skipped."))
-  }
+    if (opt$facet %in% sample_vars) {
+        p <- p + facet_wrap(as.formula(paste("~", opt$facet)))
+    } else {
+        warning(paste("Facet variable", opt$facet, "does not exist in the sample data. Faceting will be skipped."))
+    }
 }
 
-# Save to output file 
+# Save to output file
 ggsave(
-  opt$output, 
-  width = 10, 
-  height = 8,
-  device="png"
+    filename = opt$output,
+    plot = p,
+    width = opt$width,
+    height = opt$height,
+    device = opt$device
 )
