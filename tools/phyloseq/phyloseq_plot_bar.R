8ecc35b36ba563cbc726fa0c5d7b8f271990c60e
Mara Besemer <mara.besemer@wattfox.de>
2025-02-11 16:20:25 +0100

    Add normailze x option and fix galaxy wrapper input

--- tools/phyloseq/phyloseq_plot_bar.R
+++ tools/phyloseq/phyloseq_plot_bar.R
@@ -4,6 +4,7 @@
 suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("phyloseq"))
 suppressPackageStartupMessages(library("ggplot2"))
+suppressPackageStartupMessages(library("dplyr"))
 
 # Define options
 option_list <- list(
@@ -43,6 +44,10 @@ option_list <- list(
         action = "store_true", dest = "normalize", default = FALSE,
         help = "Normalize abundances to sum to 100% (optional)"
     ),
+    make_option(c("--normalize_x"),
+        action = "store_true", dest = "normalize_x", default = FALSE,
+        help = "Normalize each x category to 100% (optional)"
+    ),
     make_option(c("--width"),
         action = "store", dest = "width", default = 10,
         type = "numeric", help = "Width of the output plot in inches"
@@ -61,10 +66,12 @@ option_list <- list(
     )
 )
 
+
 # Parse arguments
 parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
 args <- parse_args(parser, positional_arguments = TRUE)
 opt <- args$options
+
 
 # Validate required options
 if (is.null(opt$input) || opt$input == "") {
@@ -84,12 +91,18 @@ if (opt$normalize) {
     physeq <- transform_sample_counts(physeq, function(x) 100 * x / sum(x))
 }
 
+if (opt$keepNonAssigned) {
+    tax_table(physeq) <- apply(tax_table(physeq), c(1, 2), function(x) ifelse(is.na(x) | x == "", "Not Assigned", x))
+}
+
+sample_vars <- colnames(sample_data(physeq))
+
 # Debug: Check available taxonomic ranks
 print("Available taxonomic ranks:")
 print(colnames(tax_table(physeq)))
 
 # Handle missing or unassigned taxa for all ranks
-if (opt$keepNonAssigned) {
+if (!is.null(opt$keepNonAssigned) && opt$keepNonAssigned != "") {
     # Replace NA or empty values with 'Not Assigned' for all ranks
     tax_ranks <- colnames(tax_table(physeq))
 
@@ -124,7 +137,7 @@ if (!is.null(opt$topX) && opt$topX != "") {
 
     otus_in_top_taxa <- rownames(tax_table_agg)[tax_table_agg[, tax_rank] %in% top_taxa]
 
-    if (opt$keepOthers) {
+    if (!is.null(opt$keepOthers) && opt$keepOthers) {
         tax_table(physeq_agg)[, tax_rank][!rownames(tax_table_agg) %in% otus_in_top_taxa] <- "Others"
         physeq <- physeq_agg
     } else {
@@ -147,6 +160,18 @@ if (!is.null(opt$x) && opt$x != "") {
         )
 }
 
+if (!is.null(opt$normalize_x) && opt$normalize_x) {
+    p_data <- p$data %>%
+        group_by(!!sym(opt$x), !!sym(opt$fill), !!sym(opt$facet)) %>%
+        summarise(Abundance = sum(Abundance), .groups = "drop") %>%
+        group_by(!!sym(opt$x), !!sym(opt$facet)) %>% # include facet
+        mutate(Abundance = 100 * Abundance / sum(Abundance))
+
+    p <- ggplot(p_data, aes(x = !!sym(opt$x), y = Abundance, fill = !!sym(opt$fill))) +
+        geom_bar(stat = "identity", position = "stack")
+}
+
+
 # Optional: Add faceting if specified
 if (!is.null(opt$facet) && opt$facet != "") {
     sample_vars <- colnames(sample_data(physeq))
@@ -156,6 +181,7 @@ if (!is.null(opt$facet) && opt$facet != "") {
         warning(paste("Facet variable", opt$facet, "not found in sample data. Skipping faceting."))
     }
 }
+
 
 # Save to output file
 ggsave(
