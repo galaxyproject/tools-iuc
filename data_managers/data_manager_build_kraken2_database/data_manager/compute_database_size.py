b74dbf63716bc0db295eab3e437b02261285db89
Peter van Heusden <pvanheusden@uwc.ac.za>
2026-02-13 19:51:15 +0200

    compute database size using a script

---
+++ data_managers/data_manager_build_kraken2_database/data_manager/compute_database_size.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pathlib
+import math
+import sys
+
+if __name__ == "__main__":
+    database_path = sys.argv[1]
+    all_files = [f for f in pathlib.Path(database_path).rglob('*') if f.is_file()]
+    db_size = math.ceil(sum(f.stat().st_size for f in all_files) / (1024 * 1024))
+    print(db_size, end="")
