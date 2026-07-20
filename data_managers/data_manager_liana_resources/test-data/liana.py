ad7d4b1b5cf21b11d22bdb0a1f0679a15078b03e
khaled196 <khalled.jooma@yahoo.com>
2026-07-20 11:59:04 +0100

    fix pandas pd therid party

--- data_managers/data_manager_liana_resources/test-data/liana.py
+++ data_managers/data_manager_liana_resources/test-data/liana.py
@@ -1,17 +1,44 @@
-from pathlib import Path
 import pandas as pd
+
 
 class Resource:
     @staticmethod
     def show_resources():
-        return ["consensus", "cellphonedb", "cellchatdb", "connectomedb2020", "mouseconsensus"]
+        return [
+            "consensus",
+            "cellphonedb",
+            "cellchatdb",
+            "connectomedb2020",
+            "mouseconsensus",
+        ]
+
     @staticmethod
     def select_resource(resource_name):
-        return pd.DataFrame({"ligand": ["L"], "receptor": ["R"], "resource": [resource_name]})
+        return pd.DataFrame(
+            {
+                "ligand": ["L"],
+                "receptor": ["R"],
+                "resource": [resource_name],
+            }
+        )
+
     @staticmethod
     def get_hcop_orthologs(target_organism="mouse", min_evidence=3, **kwargs):
-        return pd.DataFrame({"human_symbol": ["A"], f"{target_organism}_symbol": ["a"]})
+        return pd.DataFrame(
+            {
+                "human_symbol": ["A"],
+                f"{target_organism}_symbol": ["a"],
+            }
+        )
+
     @staticmethod
     def get_metalinks(biospecimen_location=None, **kwargs):
-        return pd.DataFrame({"type": ["lr"], "biospecimen_location": [str(biospecimen_location)]})
+        return pd.DataFrame(
+            {
+                "type": ["lr"],
+                "biospecimen_location": [str(biospecimen_location)],
+            }
+        )
+
+
 resource = Resource()
