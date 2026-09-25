88a92c0096c8d75d8c39176ef0c73b228368b50e
Matthew Nguyen <matthewnguyen.667@gmail.com>
2026-09-24 17:27:15 -0700

    Rewrote perseus setup as data manager

---
+++ data_managers/data_manager_perseus/data_manager/make_json.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--path", required=True)
+    args = parser.parse_args()
+
+    data_manager_json = {
+        "data_tables": {
+            "perseus_taxonomy_db": [
+                {
+                    "value": "ncbi_taxonomy",
+                    "name": "NCBI Taxonomy",
+                    "path": args.path,
+                }
+            ]
+        }
+    }
+
+    with open(args.output, "w") as handle:
+        json.dump(data_manager_json, handle)
+
+
+if __name__ == "__main__":
+    main()
